697e0c0851ba001d5c9224794e14a621298ef740
valbtlec <valerie.montusclat@btlec.fr>
2017-12-29 11:08:08 +0100

    modif lien google map et lien vers formulaire de contact principal

--- public/view/_footer.php
+++ public/view/_footer.php
@@ -15,13 +15,13 @@
         <div class="col l4 s12">
           <h5 class="white-text">Nous contacter</h5>
             <p><i class="fa fa-phone" aria-hidden="true"></i>&nbsp; &nbsp;&nbsp; 03 26 89 86 88<br></p>
-            <p><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp; &nbsp;&nbsp;<a class="white-text" href="">Envoyer un mail à BTlec</a>
+            <p><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp; &nbsp;&nbsp;<a class="white-text" href="<?= ROOT_PATH?>/public/mag/main-contact.php">Envoyer un mail à BTlec</a>
 
         </div>
         <div class="col l4 s12">
           <h5 class="white-text">Plus d'infos</h5>
           <p>
-            <i class="fa fa-globe" aria-hidden="true"></i>&nbsp; &nbsp;<a class="white-text" href="<?= $map ?>">Venir à BTlec</a>
+            <i class="fa fa-globe" aria-hidden="true"></i>&nbsp; &nbsp;<a class="white-text" href="<?=ROOT_PATH?>/public/mag/google-map.php">Venir à BTlec</a>
           </p>
 
 
