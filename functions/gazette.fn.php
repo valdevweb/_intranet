23a00b6b0941091983fd8bd4165148fef9f858cc
valbtlec <valerie.montusclat@btlec.fr>
2018-01-04 10:33:46 +0100

    correction bug mois (01 et non 1) : modif requete

--- functions/gazette.fn.php
+++ functions/gazette.fn.php
@@ -8,7 +8,8 @@ function showThisWeek($pdoBt)
 	$start=$start->format('Y-m-d');
 	$end=new DateTime('Friday this week');
 	$end=$end->format('Y-m-d');
-	$req=$pdoBt->prepare("SELECT * FROM gazette WHERE date BETWEEN :start AND :end ORDER BY date");
+	//SELECT file, month(date) as month, day(date) as day, date FROM `gazette`
+	$req=$pdoBt->prepare("SELECT file, month(date) as month, day(date) as day, year(date) as year, date FROM gazette WHERE date BETWEEN :start AND :end ORDER BY date");
 	$req->execute(array(
 		':start' 	=> $start,
 		':end'		=> $end
@@ -25,16 +26,15 @@ function createLinks($pdoBt,$gazettes,$version)
 	$months= array('','janvier', 'février', 'mars', 'avril', 'mai', 'juin','juillet', 'août', 'septembre', 'octobre','novembre','décembre');
 	$gazette=array();
 	foreach ($gazettes as $g) {
-		$date=explode('-',$g['date']);
-		$jour=$date[2];
-		$month=$date[1];
-		$month=$months[$month];
-		$year=$date[0];
-		$link="http://172.30.92.53/".$version."upload/gazette/" .$g['file'];
-		$html="<li><a href='".$link."' class='simple-link'>la gazette du ".$jour .' '. $month .' '.$year ."</a></li>";
-		array_push($gazette,$html);
-	}
-	return $gazette;
+	 	$jour=$g['day'];
+	 	$month=$g['month'];
+	 	$month=$months[$month];
+	 	$year=$g['year'];
+	 	$link="http://172.30.92.53/".$version."upload/gazette/" .$g['file'];
+	 	$html="<li><a href='".$link."' class='simple-link'>la gazette du ".$jour .' '. $month .' '.$year ."</a></li>";
+	 	array_push($gazette,$html);
+	 }
+	 return $gazette;
 }
 
 
