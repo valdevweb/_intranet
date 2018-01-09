45b927a7e482441c318422c17b72d9f03219f1d7
valbtlec <valerie.montusclat@btlec.fr>
2018-01-09 10:34:09 +0100

    ajout video

--- public/entrepot/discover.php
+++ public/entrepot/discover.php
@@ -1,8 +1,8 @@
 <?php
 require('../../config/autoload.php');
 if(!isset($_SESSION['id'])){
-  echo "pas de variable session";
-  header('Location:'. ROOT_PATH.'/index.php');
+	echo "pas de variable session";
+	header('Location:'. ROOT_PATH.'/index.php');
 }
 include('../view/_head.php');
 include('../view/_navbar.php');
@@ -23,24 +23,52 @@ addRecord($pdoStat,$page,$action, $descr);
 
 
 	<h1 class="light-blue-text text-darken-2">L'entrepôt</h1>
-<!-- 	<p><img src="../img/soon.png"></p> -->
-	<div class="row center">
+	<!-- 	<p><img src="../img/soon.png"></p> -->
+<!-- 	<div class="row center">
 		<div class="col l4"></div>
 		<div class="col l4">
 			<h4 class="light-blue-text text-darken-2">Visite virtuelle de l'entrepôt</h4>
 
-	<div class="down"></div>
+			<div class="down"></div>
 			<video width="400" height="300" controls poster="../img/entrepot/before-video.png">
-			<source src="" type="video/mp4">
-			<source src="" type="video/ogg">
-			</video>
-		</div>
-		<div class="col l4"></div>
+				<source src="" type="video/mp4">
+					<source src="" type="video/ogg">
+					</video>
+				</div>
+				<div class="col l4"></div>
 
+ -->
+	 <div class="row ">
+			<h4 class="light-blue-text text-darken-2">Visite virtuelle de l'entrepôt</h4>
 	</div>
+	 <div class="row center">
+	 	<div class="col l4">
+	 		<video width="400" height="300" controls poster="">
+	 			<source src="../video/entrepot02.mp4" type="video/mp4">
+					<source src="" type="video/ogg">
+			</video>
+			<p>quai de chargements</p>
+	 	</div>
+	 	<div class="col l4">
+	 		<video width="400" height="300" controls poster="">
+	 			<source src="../video/entrepot03.mp4" type="video/mp4">
+	 			<source src="" type="video/ogg">
+	 		</video>
+			<p>zone internet</p>
+
+	 	</div>
+	 	<div class="col l4">
+	 		<video width="400" height="300" controls poster="">
+	 			<source src="../video/entrepot04.mp4" type="video/mp4">
+	 			<source src="" type="video/ogg">
+	 		</video>
+			<p>"filmeuse"</p>
+
+	 	</div>
+	</div>
+
 </div>
+			<?php
+			include('../view/_footer.php');
 
-<?php
-include('../view/_footer.php');
-
-?>
+			?>
