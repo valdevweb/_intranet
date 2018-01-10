f03b4e9d288a047ba5c4037d0cd0a7beddddd22d
valbtlec <valerie.montusclat@btlec.fr>
2018-01-10 14:56:35 +0100

    refacto ok pour envoi mail avec fonction commune du fichier form.fn (à déplacer plus tard)

--- public/mag/contact.php
+++ public/mag/contact.php
@@ -42,46 +42,6 @@ $serviceName=getNames($pdoBt, $idGt);
 $nbName=sizeof($serviceName);
 
 
-//----------------------------------------------------------------
-//		éviter mulitple soumission du form->header ap traitement => plus utile
-//----------------------------------------------------------------
-// $here = $_SERVER['PHP_SELF'] ;
-// if(!empty($_SERVER['QUERY_STRING']))
-// {
-// 	$here .= '?' . $_SERVER['QUERY_STRING'] ;
-// }
-
-
-
-function sendMail($mailingList,$subject,$tplLocation,$contentOne,$contentTwo,$link)
-{
-	$tpl = file_get_contents($tplLocation);
-	$tpl=str_replace('{CONTENT1}',$contentOne,$tpl);
-	$tpl=str_replace('{CONTENT2}',$contentTwo,$tpl);
-	$tpl=str_replace('{LINK}',$link,$tpl);
-
-
-	$htmlContent=$tpl;
-// Set content-type header for sending HTML email
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// Additional headers
-	$headers .= 'From: ne_pas_repondre@btlec.fr>' . "\r\n";
-	$headers .= 'Cc: ' . "\r\n";
-	$headers .= 'Bcc:' . "\r\n";
-
-	if(mail($mailingList,$subject,$htmlContent,$headers))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-
-}
-
 // templates et sujet des mails envoyés à la soumission du formulaire
 // un mail informer le service qu'une demande à été posté
 // un mail au mag pour lui confirmer que sa demande a été envoyé
