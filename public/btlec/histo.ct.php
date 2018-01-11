857c40d1ca022441bcd7e33721a088650304922b
valbtlec <valerie.montusclat@btlec.fr>
2018-01-11 10:40:38 +0100

    mise ne place de l'historique des demandes cloturées côté bt

--- public/btlec/histo.ct.php
+++ public/btlec/histo.ct.php
@@ -1,17 +1,23 @@
 <div class="container">
 	<!-- titre -->
 	<h1 class="blue-text text-darken-2">Demandes magasin clôturées</h1>
-
-	<!-- select -->
+		<!-- select -->
 	<div class="row">
 		<form class="browser-default" action="request.ct.php" >
 			<div class="col l4"></div>
 			<div class="col l4">
 				<!-- <p class="center">Choisir un service</p> -->
 				<select class="browser-default select-service"  name="services" id="services">
-					<option name="default" id="default" value="default">Tous les services</option>
-					<?php foreach ($services as $service): ?>
-						<option name='<?= $service['id']?>' value='<?= $service['id']?>' >   <?= $service['full_name']?></option>
+						<option name='9999' value='tous' >toutes les demandes</option>
+
+					<?php foreach ($userService as $serviceu): ?>
+						<option name='<?= $serviceu['id']?>' selected='selected' value='<?= $serviceu['id']?>' ><?= $serviceu['full_name']?></option>
+					<?php endforeach ?>
+					<?php foreach ($one as $serviceo): ?>
+						<option name='<?= $serviceo['id']?>' value='<?= $serviceo['id']?>' ><?= $serviceo['full_name']?></option>
+					<?php endforeach ?>
+					<?php foreach ($two as $servicet): ?>
+						<option name='<?= $servicet['id']?>' value='<?= $servicet['id']?>' >   <?= $servicet['full_name']?></option>
 					<?php endforeach ?>
 				</select>
 			</div>
@@ -20,57 +26,93 @@
 	</div>
 
 	<div class="row">
-		<p class="err-msg">
-			En cours de construction
-		</p>
-<!-- debut boucle pour affichage des resultats => messages envoyés au services -->
+	<!-- debut boucle pour affichage des resultats => messages envoyés au services -->
 	<br><br>
 	<div id="list-msg">
 		<?php foreach ($msg as $key => $value): ?>
 		<!-- un message  -->
 		<article class="msg"  data-service='<?= $value['id_service']; ?>' >
 			<!-- entete -->
-			<div class="row blue-grey darken-1 box-border" >
-				<?php $idMag=$value['id_mag'];?>
-				<?php $magInfo=getMag($pdoBt,$idMag); ?>
-				<div class="col l3">
+
+			<div class="row <?= color($value['id_service'],$services)?> box-border" >
+				<?php $idMag=$value['id_mag'];
+				$panoGalec=getPanoGalec($pdoUser,$idMag);
+				$magInfo=getMag($pdoBt,$panoGalec['galec']); ?>
+				<div class="col l4">
 					<p class="boldtxt">MAGASIN :
-						<?= $magInfo['mag'] ?>
+						<?= $magInfo['mag']  ?>
 					</p>
 				</div>
-				<div class="col l3">
-					<p class="boldtxt">
-						<?= $magInfo['city'] . ' - ' .$magInfo['cp']  ?>
+
+				<div class="col l4">
+					<p class="boldtxt">Interlocuteur :
+						<?=$value['who'] ?>
+						 <?php 	 // $magInfo['city'] . ' - ' .$magInfo['cp']  ?>
 					</p>
 				</div>
-				<div class="col l3">
+				<div class="col l2">
 					<p class="boldtxt">Code BTLec :
 						<?= $magInfo['btlec']  ?>
 					</p>
 				</div>
-				<div class="col l3">
-					<p class="boldtxt">Panonceau galec :
+				<div class="col l2">
+					<p class="boldtxt">Code Galec :
 						<?= $magInfo['galec']  ?>
 					</p>
 				</div>
 			</div>
+			<?php
+			//si on a des réponse bt
+			if($nbRep=nbRep($pdoBt, $value['id'])){
+				$nbRepmsg=' - '. $nbRep['nb_rep'] . ' réponse(s)';
+				$lastDateRep=$nbRep['last_reply_date'];
+				$lastDateRep=date('d-m-Y', strtotime($lastDateRep));
+				$by=$nbRep['replied_by'];
+
+			}
+			else
+			{
+				$nbRepmsg='';
+				$lastDateRep="";
+				$by="";
+			}
+
+			?>
+
 			<!-- contenu du message -->
 			<div class="row white box-border">
 				<div class="col l12">
 					<?php
-					//formatage date
 						$date=new DateTime($value['date_msg']);
 						$date=$date->format('d-m-Y');
+						$found_key = array_search($value['id_service'], array_column($services, 'id'));
+						$serviceName= $services[$found_key]['full_name'];
 					?>
-					<p><span class="boldtxt">Demande du : </span><?= $date ?></p>
-					<p><span class="boldtxt">Objet : </span><?= $objet=$value['objet']; ?></p>
-				</div>
-					<div class="col l12 align-left"><?=isAttached($value['inc_file']) ?></div>
-					<div class="col l12"><span class="boldtxt">Message : </span><?= $msg=$value['msg']; ?></div>
+					<p class="center">SERVICE <?=strtoupper($serviceName)?></p>
+					<div class="col l9">
+						<p><span class="boldtxt">Demande du : </span><?= $date ?> </p>
+					</div>
+					<div class="col l3">
+						<p><span class="boldtxt">Etat : </span> <?= $value['etat'] .' ' .$nbRepmsg ?> </p>
+						<p>
+					</div>
+					<div class="col l9">
+						<p><span class="boldtxt">Dernière réponse le : </span><?= $lastDateRep ?> </p>
+					</div>
+					<div class="col l3">
+						<p><span class="boldtxt">Par : </span><?=  repliedByIntoName($pdoBt,$by) ?> </p>
+						<p>
+					</div>
 
-					<div class="col l16"><span class="boldtxt">Répondu le : </span><?= $value['date_reply'] ?></div>
-					<div class="col l16"><span class="boldtxt">Par </span><?= $value['reply_by']; ?></div>
-					<div class="col l12"><span class="boldtxt">Réponse : </span><?= $value['reply']; ?></div>
+					<div class="col l12">
+						<p><span class="boldtxt">Objet : </span><?= $objet=$value['objet']; ?></p>
+					</div>
+					<div class="col l12">
+						<p><span class="boldtxt">Message :<br> </span><?= $msg=$value['msg']; ?></p>
+					</div>
+					<div class="col l6 align-left"><?=isAttached($value['inc_file']) ?></div>
+					<div class="col l6 align-right"><a href="closedmsg.php?msg=<?= $value['id']?>" class="waves-effect waves-light btn blue darken-2">Détail</a></div>
+
 
 				</div>
 			</div>
@@ -79,8 +121,8 @@
 
 		<?php endforeach ?>
 	</div> <!-- end list-msg-->
+</div>
 </div> <!-- end container -->
-
 
 
 
