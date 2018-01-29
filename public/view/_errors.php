a621a30e63730a43a77999ca4e8fe963d770fc2b
valbtlec <valerie.montusclat@btlec.fr>
2018-01-29 15:13:14 +0100

    affichage générique des erreur - view

---
+++ public/view/_errors.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($errors)&& count($errors)!=0){
+
+    echo '<div class="alert alert-danger">';
+
+    foreach ($errors as $error) {
+        echo $error .'<br>';
+    }
+
+    echo '</div>';
+}
+if(isset($success)&& count($success)!=0){
+
+    echo '<div class="alert alert-success">';
+
+    foreach ($success as $s) {
+        echo $s .'<br>';
+    }
+
+    echo '</div>';
+}
