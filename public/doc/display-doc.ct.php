07e56d224b9fc562918a8550a6e5c6b81545aa58
valbtlec <valerie.montusclat@btlec.fr>
2018-04-03 09:19:27 +0200

    affichage plus serré

--- public/doc/display-doc.ct.php
+++ public/doc/display-doc.ct.php
@@ -2,9 +2,6 @@
 	<img src="../img/documents/analytics.png">
 
 	<h1 class="blue-text text-darken-4">Vos documents</h1>
-
-	<br>
-
 	<div class="row">
 		<!-- colonne gauche -->
 		<div class="col-sm-12 col-md-10">
@@ -12,52 +9,39 @@
 			<!-- 			SECTION ODR 		 			-->
 			<!-- ****************************************** -->
 			<h4 class="blue-text text-darken-4" id="odr-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>listing des ODR :</h4>
-			<hr>
-			<br>
+			<!-- <hr> -->
+			<!-- <br> -->
 			<p>- <a href="<?= $path .$odr['file']?>" target="_blank">Listing ODR mise à jour du <?= $odr['datefull']?></a> </p>
-			<br>
 			<!-- ****************************************** -->
 			<!-- 			SECTION TEL/BRII 			-->
 			<!-- ****************************************** -->
 			<h4 class="blue-text text-darken-4" id="tel-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>Les tickets et BRII :</h4>
-			<hr>
-			<br>
+			<!-- <hr> -->
+			<!-- <br> -->
 			<p>- <a href="<?= $path .$tel['file']?>" target="_blank">Tickets et BRII du <?= $tel['datefull']?></a> </p>
-			<br>
 			<!-- ****************************************** -->
 			<!-- 			SECTION ASSORTIMENT 			-->
 			<!-- ****************************************** -->
-			<h4 class="blue-text text-darken-4" id="assortiment-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>L'assortiment :</h4>
-			<hr>
-			<br>
-			<p>- <a href="<?= $path .$assortiment['file']?>" target="_blank">Fichier assortiment du <?= $assortiment['datefull']?></a> </p>
-			<br>
 			<!-- ****************************************** -->
 			<!-- 			SECTION PANIER PROMO 			-->
 			<!-- ****************************************** -->
-			<h4 class="blue-text text-darken-4" id="panier-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>Le panier Promo :</h4>
-			<hr>
-			<br>
-			<p>- <a href="<?= $path .$panier['file']?>" target="_blank">Panier promo du <?= $panier['datefull']?></a> </p>
-			<br>
 			<!-- ****************************************** -->
 			<!-- 			SECTION MDD 			-->
 			<!-- ****************************************** -->
-
 			<h4 class="blue-text text-darken-4" id="mdd-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>Le point stock MDD (Elsay, Eco+, Hoé, etc ):</h4>
-			<hr>
-			<br>
+			<!-- <hr> -->
+			<!-- <br> -->
 			<p>- <a href="<?= $path .$mdd['file']?>" target="_blank">Point stock MDD du <?= $mdd['datefull']?></a> </p>
-			<br>
+
 			<!-- ****************************************** -->
 			<!-- 			SECTION GFK			 			-->
 			<!-- ****************************************** -->
 
 			<h4 class="blue-text text-darken-4" id="gfk-title"><i class="fa fa-hand-o-right" aria-hidden="true"></i>les résultats GFK :</h4>
-			<hr>
-			<br>
+			<!-- <hr> -->
+			<!-- <br> -->
 			<p>- <a href="<?= $path .$gfk['file']?>" target="_blank">Résultats GFK de <?= $month ." ".$gfk['year'] ?></a> </p>
-			<br>
+
 		</div> <!-- fin col de gauche!-->
 
 		<!-- ****************************************** -->
@@ -73,12 +57,14 @@
 				<li class="nav-item">
 					<a class="nav-link" href="#tel-title">Tickets / BRII</a>
 				</li>
-				<li class="nav-item">
+				<!-- PANIER PROMO ET ASSORTIMENT -->
+				<!-- <li class="nav-item">
 					<a class="nav-link" href="#assortiment-title">Assortiment</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#panier-title">Panier promo</a>
-				</li>
+				</li> -->
+				<!-- END EN ATTENTE -->
 				<li class="nav-item">
 					<a class="nav-link" href="#mdd-title">Point stock MDD</a>
 				</li>
