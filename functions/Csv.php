ac2f93c21e15b48247afd4e805c9f9d1e5cd2a74
valbtlec <valerie.montusclat@btlec.fr>
2018-04-19 08:50:33 +0200

    av mise en prod : modif nom colonnes

--- functions/Csv.php
+++ functions/Csv.php
@@ -11,12 +11,17 @@ class CSV
 		$i=0;
 		foreach ($datas as $v)
 		{
-			// on récu^père les valeur des clé de notre tableu lors de la 1ere boucle
+			// soit on récu^père les valeur des clé de notre tableu lors de la 1ere boucle
 			// ces clés seront les intitulés de nos colonnes dans le tableau xls
-			// on pense à ajouter les guillemets et les ; autour de nos valeurs (séparateur csv)
+			//
+			// soit on écrit directement l'initulé des colonnes
+
 			if($i==0)
 			{
-				echo '"'.implode('";"',array_keys($v)) .'"'."\n";
+
+				// echo '"'.implode('";"',array_keys($v)) .'"'."\n";
+				echo "id; code galec; code bt;magasin;centrale;ville;nom;prenom;fonction;12-06-2018;13-06-2018;visite;repas" ."\n";
+
 			}
 
 				$v2 = array_map(function($value) { return str_replace('<br />', '', $value); }, $v);
