307eb0498ea2110a8bb5ec82437bc63a5e9a42c4
valbtlec <valerie.montusclat@btlec.fr>
2018-07-19 08:36:32 +0200

    modfi pour css page

--- public/view/_head-mig.php
+++ public/view/_head-mig.php
@@ -6,14 +6,18 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="stylesheet" href="<?= $tweakcss ?>">
-    <link rel="stylesheet" href="<?= $nav ?>">
-	<link rel="stylesheet" href="<?=isset($cssFile)? $cssFile: ''?>">
-	<!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="<?= $nav ?>">
+	<?php
+	if(isset($cssFile)){
+		echo '<link rel="stylesheet" href="' .$cssFile .'">';
+	}
+
+	?>
 	<link rel="stylesheet" href="<?= $bootstrap?>">
 
 	<link rel="stylesheet" href="<?= $md_css?>">
 	<link rel="stylesheet" href="<?=$awesome ?>">
-    <link rel="stylesheet" href="<?= $w3c ?>">
+	<link rel="stylesheet" href="<?= $w3c ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--  Scripts-->
 	<!-- on charge jquery dès le début pour pouvoir ajouter dynamiquement des scripts qui utilisent jquery -->
