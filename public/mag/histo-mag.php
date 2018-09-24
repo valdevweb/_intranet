25c49805f99d825e6697ed6edb6287a6e944d566
valbtlec <valerie.montusclat@btlec.fr>
2018-09-24 10:21:32 +0200

    passage bootstrap

--- public/mag/histo-mag.php
+++ public/mag/histo-mag.php
@@ -13,10 +13,16 @@ $page=basename(__file__);
 $action="tableau général";
 addRecord($pdoStat,$page,$action, $descr);
 
-
+//----------------------------------------------------------------
+//			css dynamique
+//----------------------------------------------------------------
+$page=(basename(__FILE__));
+$page=explode(".php",$page);
+$page=$page[0];
+$cssFile=ROOT_PATH ."/public/css/".$page.".css";
 
 //header et nav bar
-include ('../view/_head.php');
+include ('../view/_head-mig.php');
 include ('../view/_navbar.php');
 // echo "session " . $_SESSION['id'];
 //contenu
