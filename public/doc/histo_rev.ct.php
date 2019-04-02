9057d5a17b20e6c1845e1e1227404d01585ad360
valbtlec <valerie.montusclat@btlec.fr>
2019-04-02 17:47:24 +0200

    1

---
+++ public/doc/histo_rev.ct.php
@@ -0,0 +1,114 @@
+<div class="container white-container shadow">
+	<h1 class="blue-text text-darken-4">Historique des dépots de reversements</h1>
+
+	<h4 class="blue-text text-darken-4"><i class="fa fa-hand-o-right" aria-hidden="true"></i>Choisir un type de document pour filter l'historique</h4>
+	<!-- <hr> -->
+	<div class="row mb-5">
+		<div class="col">
+			<form method='post' action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>">
+				<div class="row">
+					<div class="col">
+						<div class="form-row pl-5">
+							<select class="form-control" id="doc_type" name="doc_type">
+								<option value="">Tout type de documents</option>
+								<?php foreach($revList as $rev): ?>
+									<?php
+									if(isset($_POST['doc_type']) & $_POST['doc_type']==$rev['id'])
+									{
+										$select="selected";
+									}
+									else
+									{
+										$select="";
+									}
+									echo '<option value="'.$rev['id'].'" '. $select.'>'.$rev['name'].'</option>'
+									?>
+								<?php endforeach ?>
+							</select>
+						</div>
+					</div>
+						<div class="col">
+							<button type="submit" id="submit" class="btn btn-primary" name="submit">OK</button>
+						</div>
+
+						<div class="col"></div>
+					</div>
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<?php include('../view/_errors.php') ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<div class="info-mag">
+				<p class="text-center"><i class="fa fa-lightbulb-o fa-2x pr-3" aria-hidden="true"></i>Toutes les pièces comptables sont disponibles dans Docubase.<br> <a href="http://172.30.101.66/rheaweb/auth" id="docubase" target="_blank">Cliquez ici pour y accéder</a></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row pt-4">
+		<div class="col"></div>
+
+		<?php
+			if(!empty($fullRevList))
+			{
+
+				echo '<div class="col-6 border shadow px-5 pb-5">';
+			}
+		 ?>
+
+
+
+			<?php
+			//  affichage : année puis mois
+			foreach ($fullRevList as $rev)
+			{
+				if(empty($rev['divers']))
+				{
+					$nomRev='reversements ' . $rev['name'];
+				}
+				else
+				{
+					$nomRev=$rev['divers'];
+				}
+
+				if($refYear != $rev['year'])
+				{
+					echo'<div class="pt-5"><time datetime="" class="icon"><em>&nbsp;</em><strong>&nbsp;</strong><span>'.$rev['year'].'</span></time></div>';
+					echo '<p class="month-img text-center pt-4"><img  src="../img/seasons/'.$rev['month'].'.png"></p>';
+					echo '<p class="text-center pb-4 month '.$monthsStr[$rev['month']].'">___ ' .ucfirst($monthsStr[$rev['month']]).' ___</span></p>';
+
+					echo '<p class="detail">- '.$rev['fulldate']. ' : '.$nomRev.'</p>';
+					$refYear = $rev['year']	;
+					$month=$rev['month'];
+				}
+				else
+				{
+					if($month!= $rev['month'])
+					{
+						echo '<p class="month-img text-center pt-4"><img  src="../img/seasons/'.$rev['month'].'.png"></p>';
+						echo '<p class=" text-center pb-3 month '.$monthsStr[$rev['month']].'">___ ' .ucfirst($monthsStr[$rev['month']]).' ___</span></p>';
+
+						echo '<p class="detail">- '.$rev['fulldate']. ' :  ' .$nomRev .'</p>';
+						$month=$rev['month'];
+					}
+					else
+					{
+						echo '<p class="detail">- '.$rev['fulldate']. ' : ' .$nomRev.'</p>';
+					}
+
+				}
+			}
+
+			?>
+			<!-- <hr> -->
+		</div>
+		<div class="col"></div>
+
+	</div>
+</div><!-- ./container -->
+
+<!-- </div>  -->
---
+++ public/doc/histo_rev.php
@@ -0,0 +1,102 @@
+<?php
+//----------------------------------------------------------------
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+
+	header('Location:'. ROOT_PATH.'/index.php');
+}
+
+//----------------------------------------------------------------
+require "../../functions/utilities.fn.php";
+//----------------------------------------------------------------
+require "../../functions/stats.fn.php";
+$descr="historique des reversements";
+$page=basename(__file__);
+$action="consultation";
+$code=101;
+// addRecord($pdoStat,$page,$action, $descr,$code);
+
+//----------------------------------------------------------------
+//			css dynamique
+//----------------------------------------------------------------
+// $page=basename(__file__);
+$pageCss=explode(".php",$page);
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+
+//header et nav bar
+include ('../view/_head-mig-bis.php');
+include ('../view/_navbar.php');
+
+//----------------------------------------------------------------
+//			functions
+//----------------------------------------------------------------
+
+$info=[];
+
+// alimentation du select (filtre tout l'affichage)
+function getRevType($pdoBt)
+{
+	$req=$pdoBt->query("SELECT name, id FROM doc_type WHERE category='reversement' ORDER BY ordre");
+	$req->execute();
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+}
+$revList=getRevType($pdoBt);
+
+
+function getRev($pdoBt)
+{
+	$req=$pdoBt->prepare("SELECT id_type,date_rev,YEAR(date_rev) as year,MONTH(date_rev) as month, DATE_FORMAT(date_rev, '%d/%m/%Y') as fulldate, divers, name FROM reversements LEFT JOIN doc_type ON reversements.id_type= doc_type.id ORDER BY date_rev DESC");
+	$req->execute();
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getRevFiltred($pdoBt)
+{
+	$req=$pdoBt->prepare("SELECT id_type,date_rev,YEAR(date_rev) as year,MONTH(date_rev) as month, DATE_FORMAT(date_rev, '%d/%m/%Y') as fulldate, divers, name FROM reversements LEFT JOIN doc_type ON reversements.id_type= doc_type.id WHERE id_type= :id_type ORDER BY date_rev DESC");
+	$req->execute(array(
+		':id_type'	=>$_POST['doc_type']
+	));
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+	// echo "<pre>";
+	// var_dump($_POST);
+	// echo '</pre>';
+
+
+$refYear="";
+if(isset($_POST['submit']) && isset($_POST['doc_type'])  && !empty($_POST['doc_type']))
+{
+	$fullRevList=getRevFiltred($pdoBt);
+	// echo "form submit";
+}
+elseif(empty($_POST['doc_type']))
+{
+	$fullRevList=getRev($pdoBt);
+	// echo "true";
+}
+else
+{
+	$fullRevList=getRev($pdoBt);
+}
+
+if(empty($fullRevList))
+{
+	$info[]="Pas de résultat pour le type de document sélectionné";
+}
+// echo "<pre>";
+// 	var_dump($fullRevList);
+// 	echo '</pre>';
+
+?>
+<?php
+
+//contenu
+include('histo_rev.ct.php');
+
+
+// footer avec les scripts et fin de html
+include('../view/_footer-mig-bis.php');
+?>
