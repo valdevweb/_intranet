3996210f0ebf7b97637cdedae03ba6a72adb8d36
valbtlec <valerie.montusclat@btlec.fr>
2019-04-10 09:18:01 +0200

    + info

--- public/view/_errors.php
+++ public/view/_errors.php
@@ -9,13 +9,26 @@ if(isset($errors)&& count($errors)!=0){
 
     echo '</div>';
 }
-elseif(isset($success)&& count($success)!=0){
+elseif((isset($success) && count($success) !=0) || (isset($info) && count($info) !=0))
+{
+    if(isset($success) && count($success) !=0)
+    {
+        echo '<div class="alert alert-success" role="alert">';
+        foreach ($success as $s)
+        {
+            echo $s .'<br>';
+        }
+        echo '</div>';
+    }
+    if(isset($info) && count($info) !=0)
+    {
+        echo '<div class="alert alert-primary" role="alert">';
+        foreach ($info as $i)
+        {
+            echo $i .'<br>';
+        }
+        echo '</div>';
 
-    echo '<div class="alert alert-success">';
-
-    foreach ($success as $s) {
-        echo $s .'<br>';
     }
 
-    echo '</div>';
 }
