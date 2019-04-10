df45c05204c122a08da5d14b29eb98e04171d7f8
valbtlec <valerie.montusclat@btlec.fr>
2019-04-10 16:33:46 +0200

    1er commit

---
+++ public/litiges/mail-bt-msgmag.php
@@ -0,0 +1,16 @@
+  <html>
+    <head>
+        <title></title>
+    </head>
+    <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
+        <p>Bonjour,</p>
+        <p>Le magasin <span style="color:darkblue;"><b>{MAG}</b></span> a ajouté un message sur la demande de litige <span style="color:darkblue;"><b>{DOSSIER}</b></span></p>
+
+        <p>Cordialement,</p>
+        <p style="color:darkblue;">------------------<br>
+        Portail BTLec EST</p>
+        <p style="color:firebrick;">*** Merci de ne pas répondre à ce mail, cette boîte mail n'est pas consultée ***</p>
+
+
+    </body>
+    </html>
---
+++ public/litiges/mail-mag-force-litige.php
@@ -0,0 +1,16 @@
+  <html>
+    <head>
+        <title></title>
+    </head>
+    <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
+        <p>Bonjour,</p>
+        <p>Nous vous informons que le service litige livraison a ouvert un dossier litige suite au problème que vous avez signalé. Vous retrouverez le détail de ce dossier sur le portail BTlec sous la référence n° <span style="color:darkblue;"><b>{DOSSIER}</b></span>. Vous pourrez ainsi suivre son avancement et communiquer avec notre service.</p>
+
+        <p>Cordialement,</p>
+        <p style="color:darkblue;">------------------<br>
+        Portail BTLec EST</p>
+        <p style="color:firebrick;">*** Merci de ne pas répondre à ce mail, cette boîte mail n'est pas consultée ***</p>
+
+
+    </body>
+    </html>
