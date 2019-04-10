00c61411fe637d064bb4b0361527628dc3cddf4e
valbtlec <valerie.montusclat@btlec.fr>
2019-04-10 09:14:59 +0200

    1er

---
+++ functions/utilities.fn.php
@@ -0,0 +1,8 @@
+<?php
+// calcul pour affichage jours méteo
+$joursStr=array("dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi");
+$joursShortStr=array("dim","lun","mar","mer","jeu","ven","sam");
+$monthsStr=array("","janvier", "février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre");
+$monthShort=array("","janv.", "fév.","mars","avr.","mai","juin","juil.","août","sept.","oct.","nov.","déc.");
+//$weekday = $jours[date('w')];
+
