2021e4e55e7d61c10b1f16dac64fafba945ef8fa
valbtlec <valerie.montusclat@btlec.fr>
2019-04-10 09:16:29 +0200

    in case

--- public/view/_navbar-save.php
+++ public/view/_navbar-save.php
@@ -1,4 +1,16 @@
 <?php
+// fonction pour vérifier les droits utilisateur
+function isUserAllowed($pdoUser, $params)
+{
+	$session=$_SESSION['id'];
+	$placeholders=implode(',', array_fill(0, count($params), '?'));
+	$req=$pdoUser->prepare("SELECT login FROM attributions WHERE id_droit IN($placeholders) AND id_user=$session" );
+	$req->execute($params);
+	 return $req->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+
 
 //determine si un user (id de la table web user appartient à un grou^pe
 function isUserInGroup($pdoBt,$idWebuser,$groupName)
@@ -10,46 +22,57 @@ function isUserInGroup($pdoBt,$idWebuser,$groupName)
 	));
 	return $req->rowCount();
 }
+// accès reversement : admin, compta, rev
+$revIds=array(5,7,8);
+$d_rev=isUserAllowed($pdoUser,$revIds);
+// accès comm : admin, comm
+$comIds=array(5,6);
+$d_comm=isUserAllowed($pdoUser,$comIds);
+// accès exploit : admin
+$exploitIds=array(5);
+$d_exploit=isUserAllowed($pdoUser,$exploitIds);
+// accès conseil : admin, consultation conseil,exploit conseil, inscription
+$conseilIds=array(5,9,10,27);
+$d_conseil=isUserAllowed($pdoUser,$conseilIds);
+
 
 // affichage ou non de la page de téléchargement des documents
-$idUser=$_SESSION['id'];
-if (isUserInGroup($pdoBt,$idUser,"communication"))
-{
-	$uploadDocument=true;
-	$exploit=false;
-	$conseil=false;
+// $idUser=$_SESSION['id'];
+// if (isUserInGroup($pdoBt,$idUser,"communication"))
+// {
+// 	$uploadDocument=true;
+// 	$exploit=false;
+// 	$conseil=false;
 
-}
-elseif(isUserInGroup($pdoBt,$idUser,"admin"))
-{
-	$uploadDocument=true;
-	$exploit=true;
-	$conseil=true;
-}
-elseif(isUserInGroup($pdoBt,$idUser,"admin_conseil"))
-{
-	$uploadDocument=false;
-	$exploit=false;
-	$conseil=true;
-}
-elseif(isUserInGroup($pdoBt,$idUser,"conseil"))
-{
-	$uploadDocument=false;
-	$exploit=false;
-	$conseil=true;
-}
-else
-{
-	$uploadDocument=false;
-	$exploit=false;
-	$conseil=false;
-}
+// }
+// elseif(isUserInGroup($pdoBt,$idUser,"admin"))
+// {
+// 	$uploadDocument=true;
+// 	$exploit=true;
+// 	$conseil=true;
+// }
+// elseif(isUserInGroup($pdoBt,$idUser,"admin_conseil"))
+// {
+// 	$uploadDocument=false;
+// 	$exploit=false;
+// 	$conseil=true;
+// }
+// elseif(isUserInGroup($pdoBt,$idUser,"conseil"))
+// {
+// 	$uploadDocument=false;
+// 	$exploit=false;
+// 	$conseil=true;
+// }
+// else
+// {
+// 	$uploadDocument=false;
+// 	$exploit=false;
+// 	$conseil=false;
+// }
 ?>
 <div id='cssmenu'>
 	<ul>
-			<!-- #0d47a1 blue darken-4 -->
-			<!-- <a href="" class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Accueil"><i class="fa fa-home fa-2x" aria-hidden="true"></i></a> -->
-			<li><a class="less-padding"  href='<?= ROOT_PATH ?>/public/home.php' data-tooltip="Accueil"><span><i class="fa fa-home fa-2x" aria-hidden="true"></i></span></a></li>
+		<li><a class="less-padding"  href='<?= ROOT_PATH ?>/public/home.php' data-tooltip="Accueil"><span><i class="fa fa-home fa-2x" aria-hidden="true"></i></span></a></li>
 			<!-- sous menu 1 -->
 			<?php
 
@@ -57,7 +80,7 @@ else
 				//							menu pour les magasins
 				//-----------------------------------------------------------------------------------------------------------------
 
-		 		ob_start();
+			ob_start();
 			?>
 			<li><a href="<?= ROOT_PATH?>/public/mag/histo-mag.php"><span>Vos demandes</span></a></li>
 			<li class='active has-sub'><a href='#'><span>Contacter nos services</span></a>
@@ -80,7 +103,7 @@ else
 				 </ul>
 			 </li>
 			 <?php
-			 	$mag=ob_get_contents();
+			 	$magNav=ob_get_contents();
 			 	ob_end_clean();
 				//-----------------------------------------------------------------------------------------------------------------
 			 	//						fin menu pour les magasin
@@ -152,7 +175,7 @@ else
 				 }
 				 elseif($_SESSION['type']=="mag" || $_SESSION['type']=="scapsav" || $_SESSION['type']=="centrale" || $_SESSION['type']=="" || $_SESSION['type']=="adh")
 				 {
-				 	echo $mag;
+				 	echo $magNav;
 				 }
 				 elseif ($_SESSION['type']=="btlec")
 				 {
@@ -164,7 +187,7 @@ else
 				 }
 			?>
 			<!-- section sans sous menu -->
-			<li><a href="<?= ROOT_PATH. '/public/entrepot/discover.php'?>" class="tooltipped" data-position="bottom" data-tooltip="Visitez l'entrepôt"><span>Entrepôt</span></a></li>
+			<li><a href="<?= ROOT_PATH. '/public/entrepot/discover.php'?>"><span>Entrepôt</span></a></li>
 
 			<li><a href="<?= ROOT_PATH. '/public/gazette/gazette.php'?>" >Les gazettes</a></li>
 			<li  class='active has-sub'><a href="<?= ROOT_PATH. '/public/doc/display-doc.php'?>" >documents</a>
@@ -179,65 +202,54 @@ else
 					<li><a href="<?= ROOT_PATH. '/public/doc/plancom2019.php'?>">Plan de Comm OP BTLec 2019</a></li>
 					<li><a href="<?= ROOT_PATH. '/public/doc/kitaffiche.php'?>">Kit affiches OP BTLec</a></li>
 					<li><a href="<?= ROOT_PATH. '/public/doc/convention.php'?>">Convention 2018</a></li>
-
-
+					<li><a href="<?= ROOT_PATH. '/public/doc/histo_rev.php'?>">Reversements</a></li>
 
 					<?php
-						ob_start();
-	 				?>
-					<li><a href="<?= ROOT_PATH .'/public/infos/twentyfour.php#plv'?>">PLV 48h</a></li>
-					<?php
-					$exceptSocara=ob_get_contents();
-					ob_end_clean();
+					$exceptSocara="<li><a href='".ROOT_PATH ."/public/infos/twentyfour.php#plv'>PLV 48h</a></li>";
 					if(!isset($_SESSION['centrale']))
 					{
 						echo $exceptSocara;
 					}
-					else{
+					else
+					{
 						if($_SESSION['centrale'] !="SOCARA")
 						{
 							echo $exceptSocara;
 						}
 					}
-					?>
 
-
-
-				<?php ob_start(); ?>
-					<li><a href="<?= ROOT_PATH. '/public/doc/upload-main.php'?>">Ajouter des documents</a></li>
-
-				<?php
-					$btdoc=ob_get_contents();
-					ob_end_clean();
-					echo ($uploadDocument) ? $btdoc : '' ;
-
+					$btdoc="<li><a href='".ROOT_PATH."/public/doc/upload-main.php'>Ajouter des documents</a></li>";
+					if($d_comm)
+					{
+						echo $btdoc;
+					}
+					$a_rev="<li><a href='".ROOT_PATH."/public/doc/exploit_rev.php'>Signaler des reversements</a></li>";
+					if($d_rev)
+					{
+						echo $a_rev;
+					}
 				?>
 				 </ul>
 			</li>
-			<!-- ajout menu exploitation salon -->
-			<?php ob_start(); ?>
-			<li  class='active has-sub'><a href="<?= ROOT_PATH. '/public/exploit/connexion.php'?>" class="tooltipped" data-position="bottom" data-tooltip="stats"><span>Exploit</span></a>
-				<ul><li><a href="<?= ROOT_PATH. '/public/salon/salon.php'?>" class="tooltipped" data-position="bottom" data-tooltip="salon inscriptions"><span>Salon</span></a></li>
-					<li><a href="<?= ROOT_PATH. '/public/exploit/connexion.php'?>" class="tooltipped" data-position="bottom" data-tooltip="stats"><span>Suivi magasins</span></a></li>
-				</ul>
-			</li>
 			<?php
-				$exploitNav=ob_get_contents();
-				ob_end_clean();
-				echo ($exploit) ? $exploitNav : '' ;?>
-				<!-- menu conseil -->
-			<?php ob_start(); ?>
-			<li><a href="http://172.30.92.53/<?= $version. 'conseil/home.php'?>" class="tooltipped" data-position="bottom" data-tooltip="Conseil d'administration"><span>Conseil</span></a></li>
-			<?php
-				$conseilNav=ob_get_contents();
-				ob_end_clean();
-				echo ($conseil) ? $conseilNav : '' ;?>
+			//ajout menu exploitation salon
+			$exploitNav="<li class='active has-sub'><a href='".ROOT_PATH. "/public/exploit/connexion.php' ><span>Exploit</span></a>";
+			$exploitNav.="<ul><li><a href='".ROOT_PATH."/public/salon/salon.php'><span>Salon</span></a></li>";
+			$exploitNav.="<li><a href='".ROOT_PATH."/public/exploit/connexion.php'><span>Suivi magasins</span></a></li></ul></li>";
+			if($d_exploit)
+			{
+			echo $exploitNav;
+			}
+			//menu conseil
+			if($d_conseil)
+			{
+			$conseilNav="<li><a href='http://172.30.92.53/".$version."conseil/home.php' class='tooltipped' data-position='bottom' data-tooltip='Conseil'><span>Conseil</span></a></li>";
+			echo $conseilNav;
+			}
+			?>
 			<li><a href="http://172.30.92.53/scapsav/intranet/magasin.php" class="tooltipped" data-position="bottom" data-tooltip="aller sur le site scapsav">Site Scapsav</a></li>
-			<!-- <li><a href="http://site.scapsav.fr/sitescapsavdev/intranet/magasin.php" class="tooltipped" data-position="bottom" data-tooltip="aller sur le site scapsav">Site Scapsav</a></li> -->
-
-			<!-- profile -->
-			<li><a href='<?= ROOT_PATH?>/public/user/profil.php' class="tooltipped" data-position="bottom" data-tooltip="Votre compte"><span><i class="fa fa-user"></i></span></a></li>
-			<li><a href="<?= ROOT_PATH ?>/public/logoff.php" class="tooltipped" data-position="bottom" data-tooltip="se déconnecter"><i class="fa fa-power-off"></i></a></li>
+			<li><a href="<?= ROOT_PATH ?>/public/user/profil.php" class="tooltipped" data-position="bottom" data-tooltip="Votre compte"><span><i class="fa fa-user"></i></span></a></li>
+			<li><a href="<?= ROOT_PATH ?>/public/logoff.php" class="tooltipped" data-position="bottom" data-tooltip="se déconnecter"><span><i class="fa fa-power-off"></i></span></a></li>
 	 </ul>
 </div>
 
