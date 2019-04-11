47a30a2ebd4f5f9705a7ca96f0831639de5779b3
valbtlec <valerie.montusclat@btlec.fr>
2019-04-11 17:09:34 +0200

    1er

---
+++ public/litiges/dt-facturation.php
@@ -0,0 +1,11 @@
+	<div class="bg-separation"></div>
+	<div class="row mt-3">
+		<div class="col">
+			<h5 class="khand text-main-blue pb-3">Information facturation : </h5>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col mb-3">
+			Nous vous avons établi la facture/l'avoir n° <span class="text-main-blue heavy"><?= $thisLitige[0]['fac_mag']?> d'un montant de <?= $thisLitige[0]['mt_mag']?> &euro; </span>
+		</div>
+	</div>
