11bb2c07abd9dd5581eb90d1f9d651759b162765
valbtlec <valerie.montusclat@btlec.fr>
2019-04-11 17:09:20 +0200

    mail 1er

---
+++ public/litiges/mail-mag-cloture.php
@@ -0,0 +1,17 @@
+  <html>
+    <head>
+        <title></title>
+    </head>
+    <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
+        <p>Bonjour,</p>
+        <p>Votre dossier litige <span style="color:darkblue;"><b>{DOSSIER}</b></span> a été clôturé.<br>
+        Vous pouvez le consulter en allant dans le menu <span style="color:darkblue;"><b>litige / Mes litiges</b></span> et en cliquant sur le numéro de dossier en référence.</p>
+
+        <p>Cordialement,</p>
+        <p style="color:darkblue;">------------------<br>
+        Portail BTLec EST</p>
+        <p style="color:firebrick;">*** Merci de ne pas répondre à ce mail, cette boîte mail n'est pas consultée ***</p>
+
+
+    </body>
+    </html>
---
+++ public/litiges/mail-mag-fac.php
@@ -0,0 +1,17 @@
+  <html>
+    <head>
+        <title></title>
+    </head>
+    <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
+        <p>Bonjour,</p>
+        <p>Le service litige livraison vous informe que vous avez reçu un avoir <span style="color:darkblue;"><b>N° {FACTURE} de {MONTANT} euros </b></span>suite à votre votre dossier litige <span style="color:darkblue;"><b>{DOSSIER}</b></span>.<br>
+        Vous pouvez consulter ce dossier en allant dans le menu <span style="color:darkblue;"><b>litige / Mes litiges</b></span>.</p>
+
+        <p>Cordialement,</p>
+        <p style="color:darkblue;">------------------<br>
+        Portail BTLec EST</p>
+        <p style="color:firebrick;">*** Merci de ne pas répondre à ce mail, cette boîte mail n'est pas consultée ***</p>
+
+
+    </body>
+    </html>
