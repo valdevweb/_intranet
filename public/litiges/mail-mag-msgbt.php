da9c1cf568f12283c36dd7a4782264ec11385941
valbtlec <valerie.montusclat@btlec.fr>
2019-04-19 16:45:30 +0200

    ajout lien portail

--- public/litiges/mail-mag-msgbt.php
+++ public/litiges/mail-mag-msgbt.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Le service litige livraison vous a envoyé un nouveau message sur le portail BTLec à propos de votre dossier <span style="color:darkblue;"><b>{DOSSIER}</b></span>.<br>
+        <p>Le service litige livraison vous a envoyé un nouveau message sur le <a href="http://172.30.92.53/btlecest">portail BTLec</a> à propos de votre dossier <span style="color:darkblue;"><b>{DOSSIER}</b></span>.<br>
         Vous pouvez le consulter en allant dans le menu <span style="color:darkblue;"><b>litige / Mes litiges</b></span> et en cliquant sur le numéro de dossier en référence.</p>
 
         <p>Cordialement,</p>
