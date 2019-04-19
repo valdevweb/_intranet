c1a465a7064eb8de7498fd57fd29baa01cff80ce
valbtlec <valerie.montusclat@btlec.fr>
2019-04-19 16:50:29 +0200

    lien vers portail extralec

--- public/doc/extralec.php
+++ public/doc/extralec.php
@@ -14,7 +14,8 @@ require '../view/_navbar.php';
 <div class="container">
 <h1 class="blue-text text-darken-4">L'application Extralec</h1>
 
-<br>
+
+<p>Pour toute information ou inscription, rendez-vous sur <a href="http://www.extralecbtlec.fr" class="blue-link"> http://www.extralecbtlec.fr</a></p>
  <embed src="<?=SITE_ADDRESS ."/public/doc/plaquetteCommercialeEXTRALEC-ZEPLV.pdf" ?>" type='application/pdf' width=100% height=900px/>
 </div>
 
