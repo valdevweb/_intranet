9b16b7f0256b253b0552e75a8bcc94d385c84f93
valbtlec <valerie.montusclat@btlec.fr>
2019-04-19 16:45:09 +0200

    ajout lien vers portail

--- public/litiges/mail-mag-new-litige.php
+++ public/litiges/mail-mag-new-litige.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Nous accusons réception de votre dossier litige n° <span style="color:darkblue;"><b>{DOSSIER}</b></span>. Vous pourrez suivre son avancement et communiquer avec le service litige livraison sur le portail BTLec grâce à ce numéro de dossier.</p>
+        <p>Nous accusons réception de votre dossier litige n° <span style="color:darkblue;"><b>{DOSSIER}</b></span>. Vous pourrez suivre son avancement et communiquer avec le service litige livraison sur le <a href="http://172.30.92.53/btlecest">portail BTLec </a>grâce à ce numéro de dossier.</p>
 
         <p>Cordialement,</p>
         <p style="color:darkblue;">------------------<br>
