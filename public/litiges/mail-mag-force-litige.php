8237107e44cbcb6b2e0b8fe0e9fe3e9cebd3690d
valbtlec <valerie.montusclat@btlec.fr>
2019-04-19 16:46:00 +0200

    modif texte

--- public/litiges/mail-mag-force-litige.php
+++ public/litiges/mail-mag-force-litige.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Nous vous informons que le service litige livraison a ouvert un dossier litige suite au problème que vous avez signalé. Vous retrouverez le détail de ce dossier sur le portail BTlec sous la référence n° <span style="color:darkblue;"><b>{DOSSIER}</b></span>. Vous pourrez ainsi suivre son avancement et communiquer avec notre service.</p>
+        <p>Nous vous informons que le service litige livraison a ouvert un dossier litige suite au problème que vous avez signalé. Vous retrouverez le détail de ce dossier sur le <a href="http://172.30.92.53/btlecest">portail BTlec</a> sous la référence n° <span style="color:darkblue;"><b>{DOSSIER}</b></span>. Vous pourrez ainsi suivre son avancement et communiquer avec notre service.</p>
 
         <p>Cordialement,</p>
         <p style="color:darkblue;">------------------<br>
