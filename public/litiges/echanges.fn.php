1be6964b15017ed27a5b382e73b9ee0a0c2d07e9
valbtlec <valerie.montusclat@btlec.fr>
2019-04-29 10:48:09 +0200

    1er echnage mag bt

---
+++ public/litiges/echanges.fn.php
@@ -0,0 +1,54 @@
+<?php
+
+function getDialog($pdoLitige)
+{
+	$req=$pdoLitige->prepare("SELECT id_dossier,DATE_FORMAT(date_saisie, '%d-%m-%Y') as dateFr, DATE_FORMAT(date_saisie, '%H:%i') as heure,msg,id_web_user,filename,mag FROM dial WHERE id_dossier= :id AND mag!=3 ORDER BY date_saisie DESC");
+	$req->execute(array(
+		':id'		=>$_GET['id']
+	));
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+
+}
+$dials=getDialog($pdoLitige);
+
+
+
+function getBtName($pdoBt, $idwebuser)
+{
+	$req=$pdoBt->prepare("SELECT CONCAT (prenom, ' ', nom) as name FROM btlec WHERE id_webuser= :id_web_user");
+	$req->execute(array(
+		':id_web_user'	=>$idwebuser
+	));
+	return $req->fetch(PDO::FETCH_ASSOC);
+}
+
+
+function getMagName($pdoUser, $idwebuser)
+{
+	$req=$pdoUser->prepare("SELECT btlec.sca3.mag, btlec.sca3.btlec FROM users LEFT JOIN btlec.sca3 ON users.galec=btlec.sca3.galec WHERE users.id= :id_web_user ");
+	$req->execute(array(
+		':id_web_user'	=>$idwebuser
+	));
+	return $req->fetch(PDO::FETCH_ASSOC);
+
+}
+
+function createFileLink($filelist)
+{
+	$rValue='';
+	$filelist=explode(';',$filelist);
+
+	for ($i=0; $i < count($filelist); $i++)
+	{
+		if($filelist[$i] !="")
+		{
+			$rValue.='<a href="'.UPLOAD_DIR.'/litiges/'.$filelist[$i].'"><span class="pr-3"><i class="fas fa-link"></i></span></a>';
+
+		}
+	}
+	return $rValue;
+}
+
+
+
+
