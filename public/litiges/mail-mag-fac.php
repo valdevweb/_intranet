c99285445103b145ae6e0d52f04af1582b5c3742
valbtlec <valerie.montusclat@btlec.fr>
2019-04-29 11:00:04 +0200

    avoir ou facture

--- public/litiges/mail-mag-fac.php
+++ public/litiges/mail-mag-fac.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Le service litige livraison vous informe que vous avez reçu un avoir <span style="color:darkblue;"><b>N° {FACTURE} de {MONTANT} euros </b></span>suite à votre votre dossier litige <span style="color:darkblue;"><b>{DOSSIER}</b></span>.<br>
+        <p>Le service litige livraison vous informe que vous avez reçu {TYPE}<span style="color:darkblue;"><b>N° {FACTURE} de {MONTANT} euros </b></span>suite à votre dossier litige <span style="color:darkblue;"><b>{DOSSIER}</b></span>.<br>
         Vous pouvez consulter ce dossier en allant dans le menu <span style="color:darkblue;"><b>litige / Mes litiges</b></span>.</p>
 
         <p>Cordialement,</p>
