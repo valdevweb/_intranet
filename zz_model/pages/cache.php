32cacc2b2018e9cf14335d1cf445a5a4ece60f95
valbtlec <valerie.montusclat@btlec.fr>
2019-05-09 08:47:22 +0200

    1

---
+++ zz_model/pages/cache.php
@@ -0,0 +1,3 @@
+<?php
+// avant toute autre ligne de code => permet de ne pas vider le cache qd change de page : utile si on veut conserver une sélection après visite d'une autre page
+session_cache_limiter('private_no_expire');
