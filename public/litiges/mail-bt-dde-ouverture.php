49f86fe656e11f837de0c287f8d37d05230221b2
valbtlec <valerie.montusclat@btlec.fr>
2019-05-09 08:48:59 +0200

    + lien site

--- public/litiges/mail-bt-dde-ouverture.php
+++ public/litiges/mail-bt-dde-ouverture.php
@@ -1,5 +1,6 @@
   <html>
     <head>
+        <!-- envoyé par : declaration-horsqlik -->
         <title></title>
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
@@ -7,7 +8,7 @@
         <p>Vous avez une demande d'ouverture de dossier litige par le magasin <span style="color:darkblue;"><b>{MAG}- {BTLEC}</b></span><br>
         Il a fourni les informations suivantes :</p>
         <p><span style="color:darkblue;">"{MSG}"</span></p>
-
+        <p>{LINK}</p>
         <p>Cordialement,</p>
         <p style="color:darkblue;">------------------<br>
         Portail BTLec EST</p>
