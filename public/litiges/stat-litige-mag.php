171cd1cf8e61d40229a454f13be65a5dad9b3d50
valbtlec <valerie.montusclat@btlec.fr>
2019-05-15 09:49:32 +0200

    + stat fréquentation

--- public/litiges/stat-litige-mag.php
+++ public/litiges/stat-litige-mag.php
@@ -63,9 +63,55 @@ function getFinance($pdoQlik, $btlec, $year)
 $yearN=date('Y');
 $yearNUn= date("Y",strtotime("-1 year"));
 $yearNDeux= date("Y",strtotime("-2 year"));
+function getNbDeclarationByMonth($pdoLitige)
+{
+	$req=$pdoLitige->prepare("SELECT date_format(date_crea, '%Y-%m'),count(id) as nb, date_format(date_crea, '%m-%Y') as dateFr FROM dossiers group by date_format(date_crea, '%Y-%m') order by date_crea");
+	$req->execute();
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getStats($pdoStat)
+{
+	$req=$pdoStat->prepare("SELECT *  FROM `stats_logs` LEFT JOIN web_users.users on stats_logs.id_web_user=web_users.users.id LEFT JOIN btlec.sca3 ON web_users.users.galec=btlec.sca3.galec WHERE `type_log` LIKE 'prod' AND type='mag' AND (`page` LIKE '%litige%' OR `page` LIKE '%declaration%')
+		ORDER BY date_format(`stats_logs`.date_heure, '%Y-%m-%d'), btlec.sca3.mag");
+	$req->execute();
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+}
+$statsReq=getStats($pdoStat);
 
 
+$firstDay=new DateTime($statsReq[0]['date_heure']);
+$lastDay=new DateTime(date('Y-m-d'));
 
+$nbDays=$firstDay->diff($lastDay);
+$nbDays=$nbDays->format('%a');
+
+
+// on réorganise le résultat de la requete en tableau de date
+$statByDay=[];
+foreach ($statsReq as $stat)
+{
+	$statDate=new DateTime($stat['date_heure']);
+	$statDate=$statDate->format('Y-m-d');
+	if(!isset($statByDay[$statDate])){
+		$statByDay[$statDate][]=$stat['mag'];
+		$witnessMag=$stat['mag'];
+
+	}
+	else{
+		if($stat['mag'] !=$witnessMag){
+			$statByDay[$statDate][]=$stat['mag'];
+			$witnessMag=$stat['mag'];
+
+		}
+
+	}
+}
+
+$litigeByMonth=getNbDeclarationByMonth($pdoLitige);
+
+
+$jour=['dimanche','lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
 
 //------------------------------------------------------
 //			DECLARATIONS
@@ -233,10 +279,99 @@ DEBUT CONTENU CONTAINER
 			<a href="print-stat-litige-mag.php?galec=<?=$_GET['galec']?>" class="btn btn-primary" target="_blank"><i class="fas fa-print pr-3"></i>Imprimer</a>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col">
+			<h5 class="text-main-blue">Nb de déclarations par jour</h5>
+			<p></p>
+		</div>
+	</div>
+<div class="row">
+	<?php
+	$col=0;
+	foreach($litigeByMonth as $nbLitiges)
+	{
+		if($col<12)
+		{
+			echo '<div class="col">';
+			echo $nbLitiges['dateFr'] .'<br>'.$nbLitiges['nb'];
+			echo '</div>';
+			$col++;
+		}
+		else
+		{
+			echo '</div>';
+			echo '<div class="row">';
+			echo '<div class="col">';
+			echo $nbLitiges['dateFr'] .'<br>'.$nbLitiges['nb'];
+			echo '</div>';
+			$col=0;
+
+		}
+
+	}
+
+
+	?>
+
+</div>
+
+	<div class="row mt-5">
+		<div class="col">
+			<h5 class="text-main-blue">Nb de magasins ayant consulté les litiges </h5>
+			<p>Cliquez sur la date pour afficher la liste des magasins</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col" id="accordion">
+			<?php
+			for($i=0;$i<=$nbDays;$i++)
+			{
+				$day=(clone $firstDay)->modify("+ $i days");
+
+
+				$dayFr=$day->format('d-m-Y');
+				$dayGb=$day->format('Y-m-d');
+
+					// echo "<pre>";
+					// print_r($statByDay[$day]);
+					// echo '</pre>';
+
+
+				if(isset($statByDay[$dayGb]))
+				{
+					echo '<div class="text-main-blue accordion-toggle">'.$jour[intval($day->format('w'))].' '.$dayFr .' : ' .count($statByDay[$dayGb]) .' magasins</div>';
+
+					// $magThisDay=$statByDay[$day];
+					echo '<div class="hide">';
+					for($j=0; $j<count($statByDay[$dayGb]);$j++){
+						echo $statByDay[$dayGb][$j];
+						echo '<br>';
+					}
+					echo '</div>';
+				}
+
+			}
+			?>
+		</div>
+	</div>
 
 	<!-- ./container -->
 </div>
 
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#accordion').find('.accordion-toggle').click(function(){
+
+
+      $(this).next().toggleClass('show');
+
+      //Hide the other panels
+      // $(".accordion-content").not($(this).next()).slideUp('fast');
+
+  });
+	});
+</script>
 <?php
 require '../view/_footer-bt.php';
 ?>
