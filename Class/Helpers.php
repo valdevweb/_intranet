37e4f12902be78b4721d4cb8b74f69cdd30df3b9
valbtlec <valerie.montusclat@btlec.fr>
2019-05-22 17:14:43 +0200

    1er commit

---
+++ Class/Helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+class Helpers{
+
+	/**
+	 * lien btn retour
+	 * @var string
+	 */
+	private $page;
+	/**
+	 * couleur btn retour optionnel
+	 * @var string
+	 */
+	private $color;
+
+	/**
+	 * création btn retour type
+	 * @param  string $page
+	 * @param  string $color optionnel
+	 * @return string
+	 */
+	public static function returnBtn($page,$color=null)
+	{
+
+			return '<div class="row py-3"><div class="col"><p class="text-right"><a href="'.$page.'" class="btn btn-primary">Retour</a></p></div></div>';
+	}
+
+
+    public static function withZero($chiffre){
+        if($chiffre < 10){
+            return '0' . $chiffre;
+        }else{
+            return $chiffre;
+        }
+    }
+
+}
