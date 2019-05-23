a95cbe8c502f0a118cd1d2dc1a434e04587875ac
valbtlec <valerie.montusclat@btlec.fr>
2019-05-23 16:28:29 +0200

    export vol

--- public/litiges/dt-prod.php
+++ public/litiges/dt-prod.php
@@ -1,78 +1,91 @@
 <div class="row mt-3 mb-3">
-		<div class="col">
-			<div class="row">
-				<div class="col">
-					<h5 class="khand text-main-blue pb-3">Produit(s) :</h5>
-					<p><span class="text-main-blue">Facture : </span><?=$fLitige[0]['facture'] .' du '.$fLitige[0]['datefacture']?></p>
-					<table class="table light-shadow">
-						<thead class="thead-dark">
-							<tr>
-								<th class="align-top">Article</th>
-								<th class="align-top">Dossier</th>
-								<th class="align-top">Palette</th>
-								<th class="align-top">Désignation</th>
-								<th class="align-top">Fournisseur</th>
-								<th class="align-top">Réclamation</th>
-								<th class="align-top">Qté <br>litige</th>
-								<!-- <th class="align-top text-right">Date déclaration</th> -->
-								<th class="align-top text-right">Valo</th>
-								<th class="align-top">PJ</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							$sumValo=0;
-							foreach ($fLitige as $prod)
-							{
-								$valo=round(($prod['tarif'] / $prod['qte_cde'])*$prod['qte_litige'],2);
-								$pj='';
+	<div class="col">
+		<div class="row">
+			<div class="col">
+				<div class="row">
+					<div class="col-10">
+						<h5 class="khand text-main-blue pb-3">Produit(s) :</h5>
+						<p><span class="text-main-blue">Facture : </span><?=$fLitige[0]['facture'] .' du '.$fLitige[0]['datefacture']?></p>
 
-								if($prod['pj']!='')
-								{
-									$pj=createFileLink($prod['pj']);
-								}
-								echo '<tr>';
-								echo '<td>'.$prod['article'].'</td>';
-								echo '<td>'.$prod['dossier_gessica'].'</td>';
-								echo '<td>'.$prod['palette'].'</td>';
-								echo '<td>'.$prod['descr'].'</td>';
-								echo '<td>'.$prod['fournisseur'].'</td>';
-								echo '<td>'.$prod['reclamation'].'</td>';
-								echo '<td class="text-right">'.$prod['qte_litige'].'</td>';
-								if($prod['id_reclamation']==6)
-								{
-									echo '<td class="text-right"> -'.number_format((float)$valo,2,'.','').'&euro;</td>';
-								}
-								else
-								{
-									echo '<td class="text-right">'.number_format((float)$valo,2,'.','').'&euro;</td>';
-								}
-								echo '<td class="text-right">'.$pj.'</td>';
-								echo '</tr>';
-								// si il s'agit d'une inversion de produit, on rajoute une ligne avec le produit inversé
-								if($prod['inversion'] !="")
-								{
-									$valoInv=round( $prod['inv_qte']*$prod['inv_tarif'],2);
-									echo '<tr class="text-center bg-reddish text-white"><td colspan="8">Produit reçu à la place de la référence ci-dessus :</td></tr>';
-									echo '<tr>';
-									echo '<td>'.$prod['inv_article'].'</td>';
-									echo '<td></td>';
-									echo '<td>'.$prod['inv_descr'].'</td>';
-									echo '<td>'.$prod['inv_fournisseur'].'</td>';
-									echo '<td></td>';
-									echo '<td class="text-right">'.$prod['inv_qte'].'</td>';
-									echo '<td class="text-right">'.number_format((float)$valoInv,2,'.','').'&euro;</td>';
-									echo '<td class="text-right"></td>';
-									echo '</tr>';
-								}
-							}
-							?>
-						</tbody>
-					</table>
-					<p class="text-right heavy bigger mb-3 text-main-blue pr-3">Valorisation magasin : <?= $valoMag?> </p>
-					<p><?= $articleAZero?></p>
 
+					</div>
+					<div class="col-2 text-right">
+						<?php if ($fLitige[0]['id_robbery'] !=null): ?>
+						<a href="xl-robbery.php?id=<?=$fLitige[0]['id_robbery']?>" class="img-overlay"></a>
+						<?php endif ?>
+
+					</div>
 				</div>
+
+				<table class="table light-shadow">
+					<thead class="thead-dark">
+						<tr>
+							<th class="align-top">Article</th>
+							<th class="align-top">Dossier</th>
+							<th class="align-top">Palette</th>
+							<th class="align-top">Désignation</th>
+							<th class="align-top">Fournisseur</th>
+							<th class="align-top">Réclamation</th>
+							<th class="align-top">Qté <br>litige</th>
+							<!-- <th class="align-top text-right">Date déclaration</th> -->
+							<th class="align-top text-right">Valo</th>
+							<th class="align-top">PJ</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						$sumValo=0;
+						foreach ($fLitige as $prod)
+						{
+							$valo=round(($prod['tarif'] / $prod['qte_cde'])*$prod['qte_litige'],2);
+							$pj='';
+
+							if($prod['pj']!='')
+							{
+								$pj=createFileLink($prod['pj']);
+							}
+							echo '<tr>';
+							echo '<td>'.$prod['article'].'</td>';
+							echo '<td>'.$prod['dossier_gessica'].'</td>';
+							echo '<td>'.$prod['palette'].'</td>';
+							echo '<td>'.$prod['descr'].'</td>';
+							echo '<td>'.$prod['fournisseur'].'</td>';
+							echo '<td>'.$prod['reclamation'].'</td>';
+							echo '<td class="text-right">'.$prod['qte_litige'].'</td>';
+							if($prod['id_reclamation']==6)
+							{
+								echo '<td class="text-right"> -'.number_format((float)$valo,2,'.','').'&euro;</td>';
+							}
+							else
+							{
+								echo '<td class="text-right">'.number_format((float)$valo,2,'.','').'&euro;</td>';
+							}
+							echo '<td class="text-right">'.$pj.'</td>';
+							echo '</tr>';
+								// si il s'agit d'une inversion de produit, on rajoute une ligne avec le produit inversé
+							if($prod['inversion'] !="")
+							{
+								$valoInv=round( $prod['inv_qte']*$prod['inv_tarif'],2);
+								echo '<tr class="text-center bg-reddish text-white"><td colspan="8">Produit reçu à la place de la référence ci-dessus :</td></tr>';
+								echo '<tr>';
+								echo '<td>'.$prod['inv_article'].'</td>';
+								echo '<td></td>';
+								echo '<td>'.$prod['inv_descr'].'</td>';
+								echo '<td>'.$prod['inv_fournisseur'].'</td>';
+								echo '<td></td>';
+								echo '<td class="text-right">'.$prod['inv_qte'].'</td>';
+								echo '<td class="text-right">'.number_format((float)$valoInv,2,'.','').'&euro;</td>';
+								echo '<td class="text-right"></td>';
+								echo '</tr>';
+							}
+						}
+						?>
+					</tbody>
+				</table>
+				<p class="text-right heavy bigger mb-3 text-main-blue pr-3">Valorisation magasin : <?= $valoMag?> </p>
+				<p><?= $articleAZero?></p>
+
 			</div>
 		</div>
 	</div>
+</div>
---
+++ public/litiges/xl-robbery.php
@@ -0,0 +1,315 @@
+<?php
+
+ // require('../../config/pdo_connect.php');
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+	echo "pas de variable session";
+	header('Location:'. ROOT_PATH.'/index.php');
+}
+
+require_once '../../vendor/autoload.php';
+
+//			css dynamique
+//----------------------------------------------------------------
+$pageCss=explode(".php",basename(__file__));
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+//------------------------------------------------------
+//			FONCTION
+//------------------------------------------------------
+function getAllDossier($pdoLitige)
+{
+	$req=$pdoLitige->prepare("
+		SELECT dossiers.id as id_main, dossiers.dossier, date_crea, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, user_crea, dossiers.galec, etat_dossier, vingtquatre,
+		mag, centrale, sca3.btlec,
+		transporteur, affrete, transit,
+		etat,
+		mt_transp, mt_assur, mt_fourn, mt_mag, fac_mag,
+		details.palette, details.facture, DATE_FORMAT(details.date_facture, '%d-%m-%Y') as datefacture, details.article, details.ean, details.dossier_gessica, details.descr, details.qte_cde, details.tarif, details.fournisseur, qte_litige,
+		reclamation, id_reclamation, valo, imputation, typo, analyse, conclusion, puv, pul
+
+
+		FROM dossiers
+		LEFT JOIN etat ON id_etat=etat.id
+		LEFT JOIN btlec.sca3 ON dossiers.galec=btlec.sca3.galec
+		LEFT JOIN transporteur ON id_transp=transporteur.id
+		LEFT JOIN affrete ON id_affrete=affrete.id
+		LEFT JOIN transit ON id_transit=transit.id
+		LEFT JOIN details ON dossiers.id=details.id_dossier
+		LEFT JOIN reclamation ON id_reclamation=reclamation.id
+		LEFT JOIN imputation ON id_imputation=imputation.id
+		LEFT JOIN typo ON id_typo=typo.id
+		LEFT JOIN analyse ON id_analyse=analyse.id
+		LEFT JOIN conclusion ON id_conclusion=conclusion.id
+		WHERE id_robbery= :id_robbery
+		ORDER BY dossiers.dossier DESC");
+	$req->execute([
+		':id_robbery'	=>$_GET['id']
+	]);
+	return $req->fetchAll(PDO::FETCH_ASSOC);
+	// return $req->errorInfo();
+}
+
+$dossiers=getAllDossier($pdoLitige);
+
+function getPaletteCde($pdoLitige,$id_dossier)
+{
+	$req=$pdoLitige->prepare("SELECT sum(tarif) as tarif_palette_cde, sum(qte_cde) as qte_art_cde FROM details WHERE id_dossier= :id_dossier");
+	$req->execute(array(
+		':id_dossier'	=>$id_dossier
+	));
+	return $req->fetch(PDO::FETCH_ASSOC);
+}
+
+function getPaletteRecu($pdoLitige, $id_dossier)
+{
+	$req=$pdoLitige->prepare("SELECT sum(tarif) as tarif_palette_recu, sum(qte_cde) as qte_art_recu FROM palette_inv WHERE id_dossier= :id_dossier");
+	$req->execute(array(
+		':id_dossier'	=>$id_dossier
+	));
+	return $req->fetch(PDO::FETCH_ASSOC);
+
+}
+
+
+//------------------------------------------------------
+//			DECLARATIONS
+//------------------------------------------------------
+$errors=[];
+$success=[];
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+// ------------------------------------
+// JULIE
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Dossier');
+$sheet->setCellValue('B1', 'Date déclaration');
+$sheet->setCellValue('C1', 'Magasin');
+$sheet->setCellValue('D1', 'Code BT');
+$sheet->setCellValue('E1', 'Galec');
+$sheet->setCellValue('F1', 'Centrale');
+$sheet->setCellValue('G1', 'Etat');
+$sheet->setCellValue('H1', '24/48h');
+$sheet->setCellValue('I1', 'palette');
+$sheet->setCellValue('J1', 'date facture');
+$sheet->setCellValue('K1', 'article');
+$sheet->setCellValue('L1', 'ean');
+$sheet->setCellValue('M1', 'Désignation');
+$sheet->setCellValue('N1', 'Fournisseur');
+$sheet->setCellValue('O1', 'Quantité commandée');
+$sheet->setCellValue('P1', 'Tarif');
+$sheet->setCellValue('Q1', 'Quantité litige');
+$sheet->setCellValue('R1', 'Réclamation');
+$sheet->setCellValue('S1', 'PUV');
+$sheet->setCellValue('T1', 'PUL');
+$sheet->setCellValue('U1', 'Valo');
+$sheet->setCellValue('V1', 'Transporteur');
+$sheet->setCellValue('W1', 'Affreteur');
+$sheet->setCellValue('X1', 'Transit');
+$sheet->setCellValue('YD1', 'Réglement Transporteur');
+$sheet->setCellValue('Z1', 'Réglement assurance');
+$sheet->setCellValue('AA1', 'Réglement fournisseur');
+$sheet->setCellValue('AB1', 'Réglement magasin');
+$sheet->setCellValue('AC1', 'Facture magasin');
+$sheet->setCellValue('AD1', 'Typologie');
+$sheet->setCellValue('AE1', 'Imputation');
+$sheet->setCellValue('AF1', 'Analyse');
+$sheet->setCellValue('AG1', 'Réponse');
+
+$styleArray = [
+	'font' => [
+		'bold' => true,
+		'color'=>['rgb'=>'FFFFFF']
+	],
+	'borders' => [
+		'top' => [
+			'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+		],
+	],
+];
+$styleArrayDossier = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => '0075BC',
+		],
+	],
+];
+$styleArrayDetail = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => '004570',
+		],
+	],
+];
+$styleArrayMontant = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => 'FF5666',
+		],
+	],
+];
+$styleArrayAutre = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => '38686A',
+		],
+	],
+];
+$styleArrayEquipe = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => 'A3B4A2',
+		],
+	],
+];
+$styleArrayAnalyse = [
+	'fill' => [
+		'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+		'color' => [
+			'rgb' => 'FF1B31',
+		],
+	],
+];
+
+$spreadsheet->getActiveSheet()->getStyle('A1:AL1')->applyFromArray($styleArray);
+
+$spreadsheet->getActiveSheet()->getStyle('A1:AG1')->applyFromArray($styleArrayDossier);
+
+$dossierInversionPalette='';
+$row=2;
+foreach ($dossiers as $key => $dossier)
+{
+	if($dossier['vingtquatre']==1)
+	{
+		$vingtquatre='oui';
+	}
+	else
+	{
+		$vingtquatre='non';
+	}
+	// calcul valo par article
+	// cas général
+	// $valoLig=($dossier['tarif']/$dossier['qte_cde'])*$dossier['qte_litige'];
+	// si excédent => id_reclamation= 6, 9 (palette) moins
+	// si inversion =>id_reclamation =5
+	// => ($dossier['tarif']/$dossier['qte_cde'])*$dossier['qte_litige'])-($dossier['inv_qte']*$dossier['inv_tarif'])
+	//inversion de palette = 7
+	if($dossier['id_reclamation']==6)
+	{
+		$valoLig=-($dossier['tarif']/$dossier['qte_cde'])*$dossier['qte_litige'];
+
+	}
+	elseif($dossier['id_reclamation']==5)
+	{
+		$valoLig=(($dossier['tarif']/$dossier['qte_cde'])*$dossier['qte_litige'])-($dossier['inv_qte']*$dossier['inv_tarif']);
+	}
+
+
+	else
+	{
+		$valoLig=($dossier['tarif']/$dossier['qte_cde'])*$dossier['qte_litige'];
+	}
+
+
+
+
+		$sheet->setCellValue('A'.$row, $dossier['dossier']);
+		$sheet->setCellValue('B'.$row, $dossier['datecrea']);
+		$sheet->setCellValue('C'.$row, $dossier['mag']);
+		$sheet->setCellValue('D'.$row, $dossier['btlec']);
+		$sheet->setCellValue('E'.$row, $dossier['galec']);
+		$sheet->setCellValue('F'.$row, $dossier['centrale']);
+		$sheet->setCellValue('G'.$row, $dossier['etat']);
+		$sheet->setCellValue('H'.$row, $vingtquatre);
+		$sheet->setCellValue('I'.$row, $dossier['palette']);
+		$sheet->setCellValue('J'.$row, $dossier['datefacture']);
+		$sheet->setCellValue('K'.$row, $dossier['article']);
+		$sheet->setCellValue('L'.$row, $dossier['ean']);
+		$sheet->setCellValue('M'.$row, $dossier['descr']);
+		$sheet->setCellValue('N'.$row, $dossier['fournisseur']);
+		$sheet->setCellValue('O'.$row, $dossier['qte_cde']);
+		$sheet->setCellValue('P'.$row, $dossier['tarif']);
+		$sheet->setCellValue('Q'.$row, $dossier['qte_litige']);
+		$sheet->setCellValue('R'.$row, $dossier['reclamation']);
+		$sheet->setCellValue('S'.$row, $dossier['puv']);
+		$sheet->setCellValue('T'.$row, $dossier['pul']);
+		$sheet->setCellValue('U'.$row, $valoLig);
+		$sheet->setCellValue('V'.$row, $dossier['transporteur']);
+		$sheet->setCellValue('W'.$row, $dossier['affrete']);
+		$sheet->setCellValue('X'.$row, $dossier['transit']);
+		$sheet->setCellValue('Y'.$row, $dossier['mt_transp']);
+		$sheet->setCellValue('Z'.$row, $dossier['mt_assur']);
+		$sheet->setCellValue('AA'.$row, $dossier['mt_fourn']);
+		$sheet->setCellValue('AB'.$row, $dossier['mt_mag']);
+		$sheet->setCellValue('AC'.$row, $dossier['fac_mag']);
+		$sheet->setCellValue('AD'.$row, $dossier['typo']);
+		$sheet->setCellValue('AE'.$row, $dossier['imputation']);
+		$sheet->setCellValue('AF'.$row, $dossier['analyse']);
+		$sheet->setCellValue('AG'.$row, $dossier['conclusion']);
+
+		$row++;
+
+
+
+
+	}
+ // dimensionnement des colnes
+	$cols=['A','B','C','D','E','F','G', 'H', 'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC', 'AD','AE','AF','AG'];
+	for ($i=0; $i < sizeof($cols) ; $i++)
+	{
+		$sheet->getColumnDimension($cols[$i])->setAutoSize(true);
+	}
+	$sheet->setTitle('litiges');
+
+
+
+
+// pour lancer le téléchargement sur le poste client
+$writer = new Xlsx($spreadsheet);
+$writer->save('export-litiges.xlsx');
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename="export-litiges.xlsx"');
+$writer->save("php://output");
+exit;
+
+
+
+
+
+//------------------------------------------------------
+//			VIEW
+//------------------------------------------------------
+include('../view/_head-bt.php');
+include('../view/_navbar.php');
+?>
+<!--********************************
+DEBUT CONTENU CONTAINER
+*********************************-->
+<div class="container">
+	<h1 class="text-main-blue py-5 ">Main title</h1>
+
+	<div class="row">
+		<div class="col-lg-1"></div>
+		<div class="col">
+			<?php
+			include('../view/_errors.php');
+			?>
+		</div>
+		<div class="col-lg-1"></div>
+	</div>
+
+
+	<!-- ./container -->
+</div>
+
+<?php
+require '../view/_footer-bt.php';
+?>
