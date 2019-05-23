f90e9013a2ec72c78e3d9c06d090ed25e6f63189
valbtlec <valerie.montusclat@btlec.fr>
2019-05-23 14:20:42 +0200

    divers

---
+++ public/litiges/css-tricks.php
@@ -0,0 +1,195 @@
+<?php
+
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+	echo "pas de variable session";
+	header('Location:'. ROOT_PATH.'/index.php');
+}
+//----------------------------------------------------------------
+//		INCLUDES
+//----------------------------------------------------------------
+
+
+
+
+//----------------------------------------------
+// css dynamique
+//----------------------------------------------
+$page=(basename(__FILE__));
+$page=explode(".php",$page);
+$page=$page[0];
+$cssFile="../css/".$page.".css";
+
+//----------------------------------------------
+//  		FUNCTIONS
+//----------------------------------------------
+
+
+include('../view/_head.php');
+include('../view/_navbar.php');
+?>
+
+<div class="container py-5">
+	<!-- main title -->
+	<div class="row">
+		<div class="col">
+			<h1 class="text-center underline-anim mt-5">CSS tricks and ideas</h1>
+		</div>
+	</div>
+	<!-- ./main title -->
+	<!-- start row -->
+	<div class="row my-5">
+		<div class="col-lg-1 col-xl-2"></div>
+		<div class="col">
+			<h4>Pulsing</h4>
+			<div class="text-blue text-center"><i class="fas fa-hourglass-half fa-lg circle-icon light-shadow"></i></div>
+			<div class="text-blue text-center"><i class="fas fa-hourglass-half fa-lg circle-icon-color light-shadow"></i></div>
+
+		</div>
+		<div class="col-lg-1 col-xl-2"></div>
+	</div>
+	<!-- ./row -->
+
+	<!-- start row -->
+	<div class="row">
+		<div class="col-lg-1 col-xxl-2"></div>
+		<div class="col">
+			<p><span class="step">1</span>One</p>
+			<p><span class="step">2</span>Two</p>
+			<p class="cut">ce text devrai être coupé et terminé par trois petits points grace au css</p>
+			<p><a href="" class="link">un lien adoucit !!</a></p>
+			<p><a href="" class="bg">un lien adoucit !!</a></p>
+			<p><a href="" class="side">un lien adoucit !!</a></p>
+
+			<div class="example">
+				<p>Hover on the link below:</p>
+				<a href="" class="underline">This is a link</a>
+			</div>
+			<div class="example">
+				<a href="" class="underline">This is another link but<br/> it's split over two lines</a>
+			</div>
+			<h1 class="bb">Big border ??</h1>
+
+			<p class="mt-5"><img src="../img/contact/new_phone.jpg" class="polaroid"></p>
+
+			<p>test de & <span class="amp">&  bof</span></p>
+			<p class="first">Hello you !<br>
+				attention, on utilise un float pour faire descendre la lettre.Elle descen un peu trop !!! <br>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+		</p>
+<img src="file.svg">
+
+	</div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- start row -->
+<div class="row">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col">
+	<div class="gr-bg">test</div>
+	</div>
+	<div class="col">
+		<div class="gr-bg2"></div>
+	</div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- ./row -->
+<!-- start row -->
+<div class="row my-3">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col-3"></div>
+	<div class="col" id="inner-shadow">Ombre interne</div>
+
+	<div class="col-3"></div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+
+<div class="row my-3">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col-3"></div>
+	<div class="col" id="outer-shadow">Ombre externe</div>
+
+	<div class="col-3"></div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- start row -->
+<div class="row mb-5">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col">
+		<div class="graybtn">button</div>
+
+	</div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- ./row -->
+
+<div class="row rowheight">
+	<div class="col-lg-1 col-xxl-2"></div>
+
+	<div class="col">
+		<div class="box">
+			<div class="ribbon">
+				<a href="">lien</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- start row -->
+<div class="row">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col">
+	<ul class="curl">
+		<li>un</li>
+		<li>deux </li>
+		<li>trois</li>
+	</ul>
+	<div class="featureBanner">bannière</div>
+		<p class="blurry-text">es tu flou ?</p>
+	</div>
+	<div class="blog-card">
+		<p>Text dans  une carte</p>
+	</div>
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- ./row -->
+<!-- start row -->
+<div class="row mt-5">
+	<div class="col-lg-1 col-xxl-2"></div>
+	<div class="col">
+		<p>assombrir une image pour que le texte soit lisible</p>
+		<p>//Black overlay - 0.5 opacity</p>
+		<p>background: rgba(0,0,0,0.5);</p>
+
+		<p>//White overlay - 0.2 opacity</p>
+		<p>background: rgba(255,255,255,0.2);</p>
+		<div class="image-container">
+			<img src="https://source.unsplash.com/daily" class="img">
+			<div class="text-container">
+				<div class="inner">
+					<h1 class="text-white">Title</h1>
+					<p class="text-white">This is some text</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-lg-1 col-xxl-2"></div>
+</div>
+<!-- ./row -->
+
+
+</div>
+
+
+<?php
+include('../view/_footer.php');
+?>
+
+
---
+++ socamil.php
@@ -0,0 +1,41 @@
+<?php
+				ob_start();
+				?>
+				<div class="row bg-white">
+					<div class="col">
+						<h5 class="center boldtxt">A compter du <span class="orange-text text-darken-2"> 1er Avril 2019</span>, les retours SAV BTLEC magasin transiteront par la <span class="orange-text text-darken-2">Centrale SOCAMIL</span></h5>
+						<p class="blue-text text-darken-2 boldtxt">EN MAGASINS</p>
+
+						<p class="bg-alert bg-alert-primary">Les Boxs Sav Btlec en retours, seront récupérés à vos réceptions magasins par nos transporteurs, tous les  <span class="boldtxt"> MARDI JEUDI ET  SAMEDI </span> de chaque semain e(cette fréquence de retour est impérative pour un suivi et surtout un retour SAV régulier).<br>
+							Le Chauffeur devra impérativement récupérer les boxs avec 2 plombs par box.<br>
+							Aucun ordre de retour ne sera émis par la Socamil puisque la reprise est impérative sur les trois jours prévus.<br>
+						Les premiers boxs Sav avec les kits de démarrage, vous seront livrés mardi matin.</p>
+
+						<p class="blue-text text-darken-2 boldtxt">EN CENTRALE</p>
+
+						<div class="bg-alert bg-alert-primary"><p>Les boxs SAV seront retournés et déchargés <span class="boldtxt">OBLIGATOIREMENT en Centrale sur l'entrepôt SEC</span><br>
+							<span class="boldtxt">INTERDICTION ABSOLUE DE DÉCHARGER AUX ENTREPÔTS FRAIS ET GRISOLLES.</span><br>
+						Les personnes chargées de transférer les boxs sur la zone de contrôle du technicien BTLEC SAV  en Socamil sont :</p>
+						<ul class="browser-default">
+							<li>Eric LACOMBE / Christophe JEANNET tel fixe 0567691867 et poste 1960 /  tel port 06.89.53.17.33   DU LUNDI AU VENDREDI DE 6h00 à 15h00</li>
+							<li>Alain HERNANDEZ / Michel RODRIGUEZ tel port 06.37.68.16.74   DU LUNDI AU VENDREDI  DE 15h00 à 20h00 et le SAMEDI DE 06h00 à 14h00</li>
+						</ul>
+					</div>
+					<p>Le cheminement de vos boxs SAV:</p>
+					<img src="../public/img/socamil/shema.gif">
+					<p>Votre interlocuteur Sav Btlec en Socamil est Mr Frédéric KOCIALKOWSKI /  tel port 0683232434 / tel poste fixe 1799<br>
+						<!-- Vous pouvez également contacter M. Alain Bénazet au 0607127103 si vous souhaitez des renseignements complémentaires. -->
+					</p>
+
+				</div>
+
+			</div>
+
+			<?php
+			$onlySocamil=ob_get_contents();
+			ob_end_clean();
+
+
+			ob_start();
+
+			?>
