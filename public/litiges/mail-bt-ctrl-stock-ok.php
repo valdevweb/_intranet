c01b7a5d33b2e77c429eb86ec5abb3173a50725d
valbtlec <valerie.montusclat@btlec.fr>
2019-06-05 17:19:10 +0200

    + code mag + nom mag + operateur

--- public/litiges/mail-bt-ctrl-stock-ok.php
+++ public/litiges/mail-bt-ctrl-stock-ok.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Le stock du dossier litige <span style="color:firebrick;"><b>{DOSSIER}</b></span> a été contrôlé. Ci dessous les informations saisies par les pilotes : </p>
+        <p>Le stock du dossier litige <span style="color:firebrick;"><b>{DOSSIER} - {MAG} ({CODEBT})</b></span> a été contrôlé par {OPERATEUR}. Ci dessous les informations saisies par les pilotes : </p>
         {RECAP}
         <p>Cordialement,</p>
         <p style="color:darkblue;">Portail BTLec EST</p>
