adfd1440d29c1d74d9c92fa8510e42346957ce16
valbtlec <valerie.montusclat@btlec.fr>
2019-06-07 10:29:55 +0200

    export presents

--- public/salon/stats-salon-2019.php
+++ public/salon/stats-salon-2019.php
@@ -159,15 +159,15 @@ $class=['nothing','present', 'repas'];
 //------------------------------------------------------
 //      DECLARATIONS
 //------------------------------------------------------
-                  $errors=[];
-                  $success=[];
+$errors=[];
+$success=[];
 
 //------------------------------------------------------
 //      VIEW
 //------------------------------------------------------
-                  include('../view/_head-bt.php');
-                  include('../view/_navbar.php');
-                  ?>
+include('../view/_head-bt.php');
+include('../view/_navbar.php');
+?>
 <!--********************************
 DEBUT CONTENU CONTAINER
 *********************************-->
@@ -282,61 +282,64 @@ DEBUT CONTENU CONTAINER
   </div>
 
 
+  <div class="row">
+    <div class="col-5">
+      <p class="text-main-blue heavy">Heures d'arrivées mardi :</p>
+    </div>
+    <div class="col-2"></div>
+    <div class="col-5">
+      <p class="text-main-blue heavy">Heures d'arrivées mercredi :</p>
+    </div>
+  </div>
+
+
+
 <div class="row">
+
   <div class="col-5">
-    <p class="text-main-blue heavy">Heures d'arrivées mardi :</p>
+    <table class="table table-sm table-bordered text-right">
+      <thead class="thead-dark">
+        <?php
+        echo '<tr>';
+        foreach ($statHeureMardi as $heureMa) {
+          echo '<th>'.$heureMa['hour'].'h</th>';
+        }
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        echo '<tr>';
+        foreach ($statHeureMardi as $heureMa) {
+          echo '<td>'.$heureMa['nb'].'</td>';
+        }
+        echo '</tr>';
+        ?>
+      </tbody>
+    </table>
   </div>
   <div class="col-2"></div>
-   <div class="col-5">
-    <p class="text-main-blue heavy">Heures d'arrivées mercredi :</p>
+
+
+  <div class="col-5">
+    <table class="table table-sm table-bordered text-right">
+      <thead class="thead-dark">
+        <?php
+        echo '<tr>';
+        foreach ($statHeureMercredi as $heureMe) {
+          echo '<th>'.$heureMe['hour'].'h</th>';
+        }
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        echo '<tr>';
+        foreach ($statHeureMercredi as $heureMe) {
+          echo '<td>'.$heureMe['nb'].'</td>';
+        }
+        echo '</tr>';
+        ?>
+      </tbody>
+    </table>
   </div>
 </div>
-<div class="row">
-
-    <div class="col-5">
-      <table class="table table-sm table-bordered text-right">
-        <thead class="thead-dark">
-          <?php
-          echo '<tr>';
-          foreach ($statHeureMardi as $heureMa) {
-            echo '<th>'.$heureMa['hour'].'h</th>';
-          }
-          echo '</tr>';
-          echo '</thead>';
-          echo '<tbody>';
-          echo '<tr>';
-          foreach ($statHeureMardi as $heureMa) {
-            echo '<td>'.$heureMa['nb'].'</td>';
-          }
-          echo '</tr>';
-          ?>
-        </tbody>
-      </table>
-    </div>
-  <div class="col-2"></div>
-
-
-    <div class="col-5">
-      <table class="table table-sm table-bordered text-right">
-        <thead class="thead-dark">
-          <?php
-          echo '<tr>';
-          foreach ($statHeureMercredi as $heureMe) {
-            echo '<th>'.$heureMe['hour'].'h</th>';
-          }
-          echo '</tr>';
-          echo '</thead>';
-          echo '<tbody>';
-          echo '<tr>';
-          foreach ($statHeureMercredi as $heureMe) {
-            echo '<td>'.$heureMe['nb'].'</td>';
-          }
-          echo '</tr>';
-          ?>
-        </tbody>
-      </table>
-    </div>
-  </div>
 
 <div class="row">
   <div class="col">
@@ -344,117 +347,120 @@ DEBUT CONTENU CONTAINER
 
   </div>
 </div>
-
-  <div class="row">
-    <div class="col">
-      <table class="table table-sm" id="table-presence">
-        <thead class="thead-dark">
-          <tr>
-            <th class="sortable" onclick="sortTable(0);">Magasin</th>
-            <th class="sortable" onclick="sortTable(2);">Centrale</th>
-            <th class="sortable" onclick="sortTable(3);">Nom</th>
-            <th class="sortable" onclick="sortTable(4);">Prénom</th>
-            <th class="sortable" onclick="sortTable(5);">Jour</th>
-            <th class="sortable" onclick="sortTable(5);">Heure</th>
-            <th class="sortable" onclick="sortTable(6);">Valise</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          foreach ($listParticipant as $part)
-          {
-            $valise = ($part['valise']==1) ? '<i class="fas fa-suitcase text-trois"></i>' : '' ;
-            if($part['date_passage'] !=''){
-              $jour=date('w',strtotime($part['date_passage']));
-              $jourStr = ($jour==2) ? 'mardi' : 'mercredi' ;
-            }
-            else{
-              $jourStr='';
-            }
-
-
-            echo '<tr>';
-            echo '<td class="'.strtolower($part['centrale']).'">'.$part['mag'].'</td>';
-            echo '<td>'.$part['centrale'].'</td>';
-            echo '<td>'.$part['nom'].'</td>';
-            echo '<td>'.$part['prenom'].'</td>';
-            echo '<td>'.$jourStr.'</td>';
-            echo '<td>'.$part['heure'].'</td>';
-            echo '<td class="text-center">'.$valise.'</td>';
-            echo '</tr>';
-
+ <div class="row mb-3">
+   <div class="col text-right">
+    <a href="xl-generate-participant2019.php" class="btn btn-green"><i class="fas fa-file-excel pr-3"></i>Export présence participants</a>
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    <table class="table table-sm" id="table-presence">
+      <thead class="thead-dark">
+        <tr>
+          <th class="sortable" onclick="sortTable(0);">Magasin</th>
+          <th class="sortable" onclick="sortTable(2);">Centrale</th>
+          <th class="sortable" onclick="sortTable(3);">Nom</th>
+          <th class="sortable" onclick="sortTable(4);">Prénom</th>
+          <th class="sortable" onclick="sortTable(5);">Jour</th>
+          <th class="sortable" onclick="sortTable(5);">Heure</th>
+          <th class="sortable" onclick="sortTable(6);">Valise</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($listParticipant as $part)
+        {
+          $valise = ($part['valise']==1) ? '<i class="fas fa-suitcase text-trois"></i>' : '' ;
+          if($part['date_passage'] !=''){
+            $jour=date('w',strtotime($part['date_passage']));
+            $jourStr = ($jour==2) ? 'mardi' : 'mercredi' ;
+          }
+          else{
+            $jourStr='';
           }
 
-          ?>
+
+          echo '<tr>';
+          echo '<td class="'.strtolower($part['centrale']).'">'.$part['mag'].'</td>';
+          echo '<td>'.$part['centrale'].'</td>';
+          echo '<td>'.$part['nom'].'</td>';
+          echo '<td>'.$part['prenom'].'</td>';
+          echo '<td>'.$jourStr.'</td>';
+          echo '<td>'.$part['heure'].'</td>';
+          echo '<td class="text-center">'.$valise.'</td>';
+          echo '</tr>';
+
+        }
+
+        ?>
 
 
-        </tbody>
-      </table>
-    </div>
+      </tbody>
+    </table>
   </div>
+</div>
 
 
 
 
 
 
-
-  <div class="row pb-3">
-    <div class="col">
-      <div class="text-main-blue heavy"> Listing des inscriptions :</div>
-    </div>
-    <div class="col text-right">
-      <a href="xl-generate-salon.php" class="btn btn-green"><i class="fas fa-file-excel pr-3"></i>Export</a>
-    </div>
+<div class="row pb-3">
+  <div class="col">
+    <div class="text-main-blue heavy"> Listing des inscriptions :</div>
   </div>
-  <div class="row">
-    <div class="col">
-      <table class="table table-sm" id="table-inscr">
-        <thead class="thead-dark">
-          <tr>
-            <th class="sortable" onclick="sortTable(0);">Magasin</th>
-            <th class="sortable" onclick="sortTable(1);">Galec</th>
-            <th class="sortable" onclick="sortTable(2);">Centrale</th>
-            <th class="sortable" onclick="sortTable(3);">Nom</th>
-            <th class="sortable" onclick="sortTable(4);">Prénom</th>
-            <th class="sortable" onclick="sortTable(5);">Fonction</th>
-            <th class="sortable" onclick="sortTable(6);">Date Inscription</th>
-            <th class="sortable" onclick="sortTable(7);">Mardi</th>
-            <th class="sortable" onclick="sortTable(8);">Mercredi</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          foreach ($listParticipant as $part)
-          {
-            $bgMardi=$part['mardi'] +$part['repas_mardi'];
-            $bgMardi=$class[$bgMardi];
-            $bgMercredi=$part['mercredi'] +$part['repas_mercredi'];
-            $bgMercredi=$class[$bgMercredi];
-            echo '<tr>';
-            echo '<td class="'.strtolower($part['centrale']).'">'.$part['mag'].'</td>';
-            echo '<td>'.$part['galec'].'</td>';
-            echo '<td>'.$part['centrale'].'</td>';
-            echo '<td>'.$part['nom'].'</td>';
-            echo '<td>'.$part['prenom'].'</td>';
-            echo '<td>'.$part['fonction'].'</td>';
-            echo '<td>'.$part['datesaisie'].'</td>';
-            echo '<td class="'.$bgMardi.'">'.$presence[$part['mardi']].$repas[$part['repas_mardi']].'</td>';
-            echo '<td class="'.$bgMercredi.'">'.$presence[$part['mercredi']].$repas[$part['repas_mercredi']].'</td>';
-            echo '</tr>';
-
-          }
-
-          ?>
-
-
-        </tbody>
-      </table>
-    </div>
+  <div class="col text-right">
+    <a href="xl-generate-salon.php" class="btn btn-green"><i class="fas fa-file-excel pr-3"></i>Export</a>
   </div>
+</div>
+<div class="row">
+  <div class="col">
+    <table class="table table-sm" id="table-inscr">
+      <thead class="thead-dark">
+        <tr>
+          <th class="sortable" onclick="sortTable(0);">Magasin</th>
+          <th class="sortable" onclick="sortTable(1);">Galec</th>
+          <th class="sortable" onclick="sortTable(2);">Centrale</th>
+          <th class="sortable" onclick="sortTable(3);">Nom</th>
+          <th class="sortable" onclick="sortTable(4);">Prénom</th>
+          <th class="sortable" onclick="sortTable(5);">Fonction</th>
+          <th class="sortable" onclick="sortTable(6);">Date Inscription</th>
+          <th class="sortable" onclick="sortTable(7);">Mardi</th>
+          <th class="sortable" onclick="sortTable(8);">Mercredi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($listParticipant as $part)
+        {
+          $bgMardi=$part['mardi'] +$part['repas_mardi'];
+          $bgMardi=$class[$bgMardi];
+          $bgMercredi=$part['mercredi'] +$part['repas_mercredi'];
+          $bgMercredi=$class[$bgMercredi];
+          echo '<tr>';
+          echo '<td class="'.strtolower($part['centrale']).'">'.$part['mag'].'</td>';
+          echo '<td>'.$part['galec'].'</td>';
+          echo '<td>'.$part['centrale'].'</td>';
+          echo '<td>'.$part['nom'].'</td>';
+          echo '<td>'.$part['prenom'].'</td>';
+          echo '<td>'.$part['fonction'].'</td>';
+          echo '<td>'.$part['datesaisie'].'</td>';
+          echo '<td class="'.$bgMardi.'">'.$presence[$part['mardi']].$repas[$part['repas_mardi']].'</td>';
+          echo '<td class="'.$bgMercredi.'">'.$presence[$part['mercredi']].$repas[$part['repas_mercredi']].'</td>';
+          echo '</tr>';
+
+        }
+
+        ?>
 
 
-  <!-- ./container -->
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
+<!-- ./container -->
 </div>
 
 
