5ac4b492d28dee28e634a2948d0398563e2449c0
valbtlec <valerie.montusclat@btlec.fr>
2019-08-16 14:50:45 +0200

    favico

--- public/view/_head-bt.php
+++ public/view/_head-bt.php
@@ -6,6 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="stylesheet" href="../css/font.css">
+	<link rel="icon" href="http://172.30.92.53/btlecest/favicon.ico" />
+
 	<!-- nouvelle page pour style commun qui remplacera main  05/02/2019 -->
 	<link rel="stylesheet" href="../css/commun.css?<?= filemtime('../css/commun.css');?>">
 	<link rel="stylesheet" href="../css/nav.css">
--- public/view/_head.php
+++ public/view/_head.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="icon" href="http://172.30.92.53/btlecest/favicon.ico" />
 	<link rel="stylesheet" href="<?= $tweakcss ?>">
     <link rel="stylesheet" href="<?= $nav ?>">
     <?php
