5d7360e042118503c7a85bb64bf311b0543bf94b
valbtlec <valerie.montusclat@btlec.fr>
2019-09-27 09:20:35 +0200

    correction qd lien : tri erroné

--- public/js/sortmultitable.js
+++ public/js/sortmultitable.js
@@ -1,11 +1,11 @@
 var asc = 0;
-function sort_table(table, col)
-{
+function sort_table(table, col){
   $('.sortorder').remove();
   if (asc == 2) {asc = -1;} else {asc = 2;}
   var rows = table.tBodies[0].rows;
-  var rlen = rows.length-1;
+  var rlen = rows.length;
   var arr = new Array();
+
   var i, j, cells, clen;
   // fill the array with values from the table
   for(i = 0; i < rlen; i++)
@@ -13,33 +13,66 @@ function sort_table(table, col)
     cells = rows[i].cells;
     clen = cells.length;
     arr[i] = new Array();
-    for(j = 0; j < clen; j++) { arr[i][j] = cells[j].innerHTML; }
-  }
-  // sort the array by the specified column number (col) and order (asc)
-  arr.sort(function(a, b)
-  {
-    var retval=0;
-    var col1 = a[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
-    var col2 = b[col].toLowerCase().replace(',', '').replace('$', '').replace(' usd', '')
-    var fA=parseFloat(col1);
-    var fB=parseFloat(col2);
-    if(col1 != col2)
-    {
-      if((fA==col1) && (fB==col2) ){ retval=( fA > fB ) ? asc : -1*asc; } //numerical
-      else { retval=(col1 > col2) ? asc : -1*asc;}
+    for(j = 0; j < clen; j++) {
+      arr[i][j] = cells[j].innerHTML;
     }
-    return retval;
-  });
-  for(var rowidx=0;rowidx<rlen;rowidx++)
-  {
-    for(var colidx=0;colidx<arr[rowidx].length;colidx++){ table.tBodies[0].rows[rowidx].cells[colidx].innerHTML=arr[rowidx][colidx]; }
-  }
 
-  hdr = table.rows[0].cells[col];
-  if (asc == -1) {
-    // $(hdr).html($(hdr).html() + '<span class="sortorder">▲</span>');
-    } else {
-    // $(hdr).html($(hdr).html() + '<span class="sortorder">▼</span>');
+  }
+// sort the array by the specified column number (col) and order (asc)
+arr.sort(function(a, b)
+{
+  var retval=0;
+  var col1 = a[col].toLowerCase()
+  var col2 = b[col].toLowerCase()
+  // passe cahine de caratère dans fonction qui supprime les liens
+  var noLink1=stripLink(col1);
+  var noLink2=stripLink(col2);
+
+
+
+  var fA=parseFloat(col1);
+  var fB=parseFloat(col2);
+
+  if(col1 != col2){
+    if((fA==col1) && (fB==col2) ){
+      retval=( fA > fB ) ? asc : -1*asc;
+    } //numerical
+    else{
+      if(noLink1 > noLink2 ){
+          retval=asc;
+      }
+      else{
+        retval=-1 *asc;
+      }
+      // retval=(col1 > col2) ? asc : -1 * asc;
+    }
+  }
+  return retval;
+// console.log(retval);
+
+});
+// console.log(arr);
+
+for(var rowidx=0;rowidx<rlen;rowidx++)
+{
+  for(var colidx=0;colidx<arr[rowidx].length;colidx++){
+    table.tBodies[0].rows[rowidx].cells[colidx].innerHTML=arr[rowidx][colidx];
   }
 }
+
+hdr = table.rows[0].cells[col];
+if (asc == -1) {
+  $(hdr).addClass('asc');
+  $(hdr).removeClass('desc');
+} else {
+  $(hdr).addClass('desc');
+  $(hdr).removeClass('asc');
+}
+}
+
+
+
+ function stripLink(txt) {
+    return txt.replace(/<a\b[^>]*>/i,"").replace(/<\/a>/i, "");
+ }
 
