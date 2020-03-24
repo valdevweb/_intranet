0a63e18009536cf2160d1d7e8a449aa18b197ede
valbtlec <valerie.montusclat@btlec.fr>
2020-03-24 11:57:21 +0100

    concat ville sca et non gessica

--- public/basemag/ajax-search-mag.php
+++ public/basemag/ajax-search-mag.php
@@ -10,7 +10,7 @@ if(isset($_POST["query"]))
 	$page=$response[1];
 	$searchTerm=$response[0];
 
-	$req=$pdoMag->prepare("SELECT * FROM mag  LEFT JOIN sca3 ON mag.id=sca3.btlec_sca WHERE concat(mag.deno,mag.galec,mag.id,mag.ville) LIKE :search");
+	$req=$pdoMag->prepare("SELECT * FROM mag  LEFT JOIN sca3 ON mag.id=sca3.btlec_sca WHERE concat(mag.deno,mag.galec,mag.id,sca3.ville_sca) LIKE :search");
 	$req->execute([
 		':search' =>'%'.$searchTerm .'%'
 	]);
