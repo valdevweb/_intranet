9081cae14483ba9e32c9509b118f6a5194364743
valbtlec <valerie.montusclat@btlec.fr>
2020-03-25 11:27:19 +0100

    prise en compte mag inf à 1000

--- batch-mag/newmag-import-qlik.php
+++ batch-mag/newmag-import-qlik.php
@@ -15,7 +15,7 @@ include 'functions\tasklog.fn.php';
 function getQlik($pdoQlik){
 	$req=$pdoQlik->query("SELECT mag_gessica.id as id, mag_ctbt_param.id  as ctbtparam_id, mag_ctbt.id as ctbt_id, ADH_RS, ADH_PANBT, ADH_VALCOD, ADH_ADR1, ADH_ADR2, ADH_CP,ADH_ADR3, ADH_TEL, ADH_TLC,ADH_SURF,ADH_NOMADH, ADH_NOMCHEF,		DIC_GEL, ADH_DATOUV, ADH_DATFER, ADH_NUMACT, AAC_COD, ADH_NUMORD, ADH_EAN, ADH_CSIRET, ADH_IBAN, ADH_BIC, ADH_RUM, ADH_ADHPYR, BCG_ADH,
 		PID_CRE, PID_RAD
-		FROM mag_gessica LEFT JOIN mag_ctbt ON mag_gessica.id= mag_ctbt.id LEFT JOIN mag_ctbt_param ON mag_gessica.id= mag_ctbt_param.id  WHERE (mag_gessica.id >3000 AND mag_gessica.id <5000) OR  (mag_gessica.id >5999 AND mag_gessica.id <7999)");
+		FROM mag_gessica LEFT JOIN mag_ctbt ON mag_gessica.id= mag_ctbt.id LEFT JOIN mag_ctbt_param ON mag_gessica.id= mag_ctbt_param.id  WHERE (mag_gessica.id >2 AND mag_gessica.id <1000) OR (mag_gessica.id >3000 AND mag_gessica.id <5000) OR  (mag_gessica.id >5999 AND mag_gessica.id <7999)");
 
 	// return $req->errorInfo();
 	return $req->fetchAll(PDO::FETCH_ASSOC);
