bf697eb98137caf69e7b8a3a99f34b302891299c
valbtlec <valerie.montusclat@btlec.fr>
2020-03-31 15:17:25 +0200

    refacto => exrpot exce à part

---
+++ public/basemag/base-mag-export.php
@@ -0,0 +1,47 @@
+		<div class="row mt-3">
+			<div class="col">
+				<form action="#">
+					<fieldset class="position-relative">
+						<legend><i class="fas fa-file-excel pr-3"></i> Exports excel :</legend>
+						<div class="row">
+							<div class="col">
+								<a href="xl-basemag.php?nofilter=1" class="btn btn-green">Export complet</a>
+							</div>
+							<div class="col">
+								<a href="xl-basemag.php?nofilter=0" class="btn btn-green">Export avec filtres actifs</a>
+							</div>
+						</div>
+						<!-- <p class="mt-3">
+							<a data-toggle="collapse" href="#collapseFieldFilter" role="button" aria-expanded="false" aria-controls="collapseFieldFilter">
+								Préciser les champs à exporter :
+							</a>
+						</p>
+
+						<div class="collapse" id="collapseFieldFilter">
+							<div class="card card-body">
+								<div class="form-check">
+									<input class="form-check-input" type="checkbox" value="" id="address" name="address">
+									<label class="form-check-label" for="model">Adresse </label>
+								</div>
+
+
+							</div>
+						</div> -->
+					</fieldset>
+				</form>
+			</div>
+		</div>
+
+
+
+
+		<div class="row mt-3">
+			<div class="col">
+				<div class="alert border text-center">
+					<span class="pr-5">Origine des données :</span>
+					<span class="text-gessica pr-5"><i class="fas fa-palette pr-3"></i>gessica</span>
+					<span class="text-sca pr-5"><i class="fas fa-palette pr-3"></i>sca3</span>
+					<span class="text-ctbt pr-5"><i class="fas fa-palette pr-3"></i>centrale BT</span>
+				</div>
+			</div>
+		</div>
