006ee63d31efbc22f0c9f1495fdf3a9073f0b964
valbtlec <valerie.montusclat@btlec.fr>
2020-03-31 15:17:49 +0200

    refacto => export excel à part

--- public/basemag/base-mag-filtre.php
+++ public/basemag/base-mag-filtre.php
@@ -161,33 +161,3 @@
 			</div>
 		</div>
 
-		<div class="row mt-3">
-			<div class="col">
-				<form action="#">
-					<fieldset class="position-relative">
-						<legend><i class="fas fa-file-excel pr-3"></i> Exports excel :</legend>
-						<div class="row">
-							<div class="col">
-								<a href="xl-basemag.php?nofilter=1" class="btn btn-green">Export complet</a>
-							</div>
-							<div class="col">
-								<a href="xl-basemag.php?nofilter=0" class="btn btn-green">Export avec filtres actifs</a>
-							</div>
-						</div>
-
-					</fieldset>
-				</form>
-			</div>
-		</div>
-
-
-		<div class="row mt-3">
-			<div class="col">
-				<div class="alert border text-center">
-					<span class="pr-5">Origine des données :</span>
-					<span class="text-gessica pr-5"><i class="fas fa-palette pr-3"></i>gessica</span>
-					<span class="text-sca pr-5"><i class="fas fa-palette pr-3"></i>sca3</span>
-					<span class="text-ctbt pr-5"><i class="fas fa-palette pr-3"></i>centrale BT</span>
-				</div>
-			</div>
-		</div>
