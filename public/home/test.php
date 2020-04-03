dffc48c34dea95051aacd179365d495e9692bf8a
valbtlec <valerie.montusclat@btlec.fr>
2020-04-03 09:01:06 +0200

    tests

---
+++ public/home/test.php
@@ -0,0 +1,86 @@
+<?php
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+	echo "pas de variable session";
+	header('Location:'. ROOT_PATH.'/index.php');
+}
+//			css dynamique
+//----------------------------------------------------------------
+$pageCss=explode(".php",basename(__file__));
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+
+
+//------------------------------------------------------
+//			REQUIRES
+//------------------------------------------------------
+// require_once '../../vendor/autoload.php';
+
+
+
+//---------------------------------------
+//	ajout enreg dans stat
+//---------------------------------------
+// require "../../functions/stats.fn.php";
+// addRecord($pdoStat,basename(__file__),'consultation', "fichiers d'info du service achats", 101);
+
+
+ //------------------------------------------------------
+//			DECLARATIONS
+//------------------------------------------------------
+$errors=[];
+$success=[];
+
+function btInfo($pdoBt)
+{
+	$req=$pdoBt->prepare("SELECT * FROM btlec WHERE id= :btlec");
+	$req->execute(array(
+		':btlec'		=>$_SESSION['id_btlec']
+	));
+
+	return $req->fetch(PDO::FETCH_ASSOC);
+}
+$test=btInfo($pdoBt);
+	echo "<pre>";
+	print_r($test);
+	echo '</pre>';
+
+	echo "<pre>";
+	print_r($_SESSION);
+	echo '</pre>';
+
+
+//------------------------------------------------------
+//			FONCTION
+//------------------------------------------------------
+
+
+//------------------------------------------------------
+//			VIEW
+//------------------------------------------------------
+include('../view/_head-bt.php');
+include('../view/_navbar.php');
+?>
+<!--********************************
+DEBUT CONTENU CONTAINER
+*********************************-->
+<div class="container">
+	<h1 class="text-main-blue py-5 ">Main title</h1>
+
+	<div class="row">
+		<div class="col-lg-1"></div>
+		<div class="col">
+			<?php
+			include('../view/_errors.php');
+			?>
+		</div>
+		<div class="col-lg-1"></div>
+	</div>
+
+
+	<!-- ./container -->
+</div>
+
+<?php
+require '../view/_footer-bt.php';
+?>
