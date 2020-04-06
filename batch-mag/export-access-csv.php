48bb0100c1ad9e4c5a3c9fcb67d433ad96559b62
valbtlec <valerie.montusclat@btlec.fr>
2020-04-06 08:32:53 +0200

    modif export => 1 fichier pr 3 tables +syno

--- batch-mag/export-access-csv.php
+++ batch-mag/export-access-csv.php
@@ -27,6 +27,10 @@ $magSyno=getMagSyno($pdoMag);
 
 
 
+$fieldsName=array_keys($sca[0]);
+
+
+
 foreach ($sca as $key => $oldMag) {
 	$centraleSca=convertCentraleExport($oldMag['centrale_sca'], $centraleList);
 	$centraleDoris=convertCentraleExport($oldMag['centrale_doris'], $centraleList);
@@ -39,15 +43,9 @@ foreach ($sca as $key => $oldMag) {
 	$dateOuverture=convertToDateExport($oldMag['date_ouverture']);;
 	$affilie=($oldMag['affilie']==1)? 2 :0;
 	$datasetSca3[]=[
-		$oldMag['nom_gesap'],
-		$oldMag['obs'],
-		$oldMag['galec_old'],
-		$sorti,
-		$dateSortie,
-		$centraleDoris,
 		$oldMag['id_sca'],
-		$oldMag['galec_sca'],
 		$oldMag['btlec_sca'],
+		$oldMag['galec_sca'],
 		$oldMag['deno_sca'],
 		$centraleSca,
 		$oldMag['ad1_sca'],
@@ -58,20 +56,14 @@ foreach ($sca as $key => $oldMag) {
 		$oldMag['tel_sca'],
 		$oldMag['fax_sca'],
 		$oldMag['surface_sca'],
-		$oldMag['adherent_sca']
-	];
-
-	$datasetPano[]=[
-		$oldMag['galec_sca'],
-		$oldMag['pole_sav_sca'],
-		$centraleSmiley,
-		$oldMag['racine_list'],
-
-	];
-	$datasetBt[]=[
-		$oldMag['apple_id'],
-		$oldMag['docubase_pwd'],
-		$oldMag['docubase_login'],
+		$oldMag['adherent_sca'],
+		$oldMag['nom_gesap'],
+		$oldMag['lotus_rbt'],
+		$oldMag['obs'],
+		$oldMag['galec_old'],
+		$centraleDoris,
+		$sorti,
+		$dateSortie,
 		$oldMag['raison_sociale'],
 		$oldMag['mandat'],
 		$dateResiliation,
@@ -79,64 +71,34 @@ foreach ($sca as $key => $oldMag) {
 		$affilie,
 		$dateFermeture,
 		$dateOuverture,
-		$oldMag['btlec_sca'],
-		$oldMag['mots_cles']
+		$oldMag['docubase_login'],
+		$oldMag['docubase_pwd'],
+		$oldMag['apple_id'],
+		$oldMag['mots_cles'],
+		$oldMag['pole_sav_sca'],
+		$centraleSmiley,
+		$oldMag['racine_list'],
+		$oldMag['date_insert'],
+		$oldMag['date_update'],
 	];
 }
 
 
 
-// INSERT INTO `btlec`.`InfosPanonceau` (`Panonceau`, `PoleSAV`, `CentraleSmiley`, `RacineListe`) VALUES ('0097', '', 'DIVERS', '');
-// INSERT INTO `btlec`.`InfosNumBT` (`apple_id`, `docubase_pwd`, `docubase_login`, `Raison sociale`, `MandatC3S`, `DateResiliation`, `DateAdhesion`, `Affilie`, `DateFermeture`, `DateOuverture`, `NumBT`, `mots_cles`) VALUES ('', '5741', 'M6895', '', 'Faux', '', '', '1', '30/09/2016', '01/06/2016', '4381', '');
-
-// $datasetSyno[]=[
-// 		$oldMag['galec_old'],
-
-// 	];
-
-
-
-
-// INSERT INTO `btlec`.`MagSyno` (`MagasinOld`, `MagasinActuel`) VALUES ('4034', '4005');
-
-
-/*
-		FICHIER SCA3
- */
-
-
-// $exportSca=DIR_EXPORT_CSV."\\export-sca3_".date('YmdHis').'.csv';
-// $exportNumBt=DIR_EXPORT_CSV."\\export-infosnumbt_".date('YmdHis').'.csv';
-// $exportPanonco=DIR_EXPORT_CSV."\\export-infospanonceau_".date('YmdHis').'.csv';
-// $exportMagSyno=DIR_EXPORT_CSV."\\export-magsyno_".date('YmdHis').'.csv';
-
-
 $exportSca=DIR_EXPORT_CSV."\\export-sca3.csv";
-$exportNumBt=DIR_EXPORT_CSV."\\export-infosnumbt.csv";
-$exportPanonco=DIR_EXPORT_CSV."\\export-infospanonceau.csv";
 $exportMagSyno=DIR_EXPORT_CSV."\\export-magsyno.csv";
 
 
 
 
 $file = fopen($exportSca, "w") or die("Unable to open file!");
+    fputcsv($file, $fieldsName);
 foreach ($datasetSca3 as $key => $value) {
     fputcsv($file, $value);
 }
 
 fclose($file);
 
-$file = fopen($exportNumBt, "w") or die("Unable to open file!");
-foreach ($datasetBt as $key => $value) {
-    fputcsv($file, $value);
-}
-fclose($file);
-
-$file = fopen($exportPanonco, "w") or die("Unable to open file!");
-foreach ($datasetPano as $key => $value) {
-    fputcsv($file, $value);
-}
-fclose($file);
 
 
 $file = fopen($exportMagSyno, "w") or die("Unable to open file!");
