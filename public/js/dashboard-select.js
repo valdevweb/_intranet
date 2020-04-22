b00d0a795497a66ce6007d2b205eace12f277f38
valbtlec <valerie.montusclat@btlec.fr>
2020-04-22 11:03:33 +0200

    migration refonte service mag

--- public/js/dashboard-select.js
+++ public/js/dashboard-select.js
@@ -1,47 +1,38 @@
 
 $(document).ready(function() {
-	var list = $("#list-msg .msg");
-	$(list).fadeOut("fast");
+	// var list = $("#list-msg .msg");
+	// $(list).fadeOut("fast");
 
-	value= $("select#services").attr("selected","selected" ).val();
-	$("#list-msg").find("article[data-service=" + value + "]").each(function (i) {
-		$(this).delay(200).slideDown("fast");
-	});
+	// value= $("select#services").attr("selected","selected" ).val();
+	// $("#list-msg").find("article[data-service=" + value + "]").each(function (i) {
+	// 	$(this).delay(200).slideDown("fast");
+	// });
 
-	//on récupère la valeur de la list deroulante => son id
-	$('select#services').change(function()
-	{
-		var value = $(this).val()
-		if(value=='tous')
-		{
-				$(list).fadeIn("fast");
-		}
-		else
-		{
-			filterList(value);
-		}
-	});
+	// //on récupère la valeur de la list deroulante => son id
+	// $('select#services').change(function()
+	// {
+	// 	var value = $(this).val()
+	// 	if(value=='tous')
+	// 	{
+	// 			$(list).fadeIn("fast");
+	// 	}
+	// 	else
+	// 	{
+	// 		filterList(value);
+	// 	}
+	// });
 
 	//News filter function
-	function filterList(value) {
-		// on cache tout
-		var list = $("#list-msg .msg");
-		$(list).fadeOut("fast");
+	// function filterList(value) {
+	// 	// on cache tout
+	// 	var list = $("#list-msg .msg");
+	// 	$(list).fadeOut("fast");
 
 
-		//on affiche que ce qui est selectionné
-		// if (value == "default")
-		// {
-		// 	$("#list-msg").find("article").each(function (i) {
-		// 		$(this).delay(200).slideDown("fast");
-		// 	});
-		// }
-		// else
-		// {
-			$("#list-msg").find("article[data-service=" + value + "]").each(function (i) {
-				$(this).delay(200).slideDown("fast");
-			});
-		// }
-	}
+	// 		$("#list-msg").find("article[data-service=" + value + "]").each(function (i) {
+	// 			$(this).delay(200).slideDown("fast");
+	// 		});
+	// 	// }
+	// }
 });
 
