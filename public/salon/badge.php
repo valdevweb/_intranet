6ae5d3d2e71c1e16ac3bdb046c4167b7ca5935b1
montusclat <vmontusclat@gmail.com>
2020-09-03 09:48:36 +0200

    test création badge salon

---
+++ public/salon/badge.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		body{
+			font-family: helvetica, sans-serif;
+			color:  #4f4f4f;
+		}
+
+
+		/* .bigger{
+			font-size: 20px;
+		}
+			.normal{
+			font-size: 12px;
+		}
+
+		th{
+			background-color :#0075BC;
+			color: #fff;
+			padding: 10px;
+		} */
+		table,td, tr{
+			font-size: 10px;
+
+		}
+
+		.text-date{
+			/* font-style:  italic; */
+			font-family: Aegyptus;
+			font-size: 28px;
+
+		}
+
+
+		.border-table-grey, .border-table-grey td{
+			border: 1px solid #343a40;
+			border-collapse: collapse;
+		}
+
+		.width{
+			width: 9cm;
+			height: 5.4cm;
+		}
+
+
+	</style>
+
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td class="deux"><img src="../img/salon/bt200.jpg"></td>
+			<td class="huit"></td>
+			<td class="trois">BTLec Est<BR>
+				Parc d'activités Witry Caurel - 2 rue des Moissons<br>
+				51420 WITRY LES REIMS
+			 </td>
+		</tr>
+	</table>
+	<div class="spacing-l"></div>
+
+<table class="border-table-grey">
+	<tr>
+		<td class="width"></td>
+	</tr>
+</table>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
