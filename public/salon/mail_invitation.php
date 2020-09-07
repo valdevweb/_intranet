882a5dc8f79edc5a62daf6fae2292f994f7a3435
montusclat <vmontusclat@gmail.com>
2020-09-07 17:22:55 +0200

    chg mail car envoi badge et non invit

--- public/salon/mail_invitation.php
+++ public/salon/mail_invitation.php
@@ -4,9 +4,11 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Veuillez trouver ci joint vos invitations pour le salon BTLec Est 2020.</p>
-
-        <p>Dans l'attente de vous accueillir,<br>
+        <p>Veuillez trouver ci joint vos badges pour le salon BTLec Est 2020.</p>
+        <br>
+        <p>Si vous rencontrez des problèmes de taille de badges à l'impression, sachez que les pdf doivent vous permettre d'imprimer des badges de 9cm x 5.4cm. Si ce n'est pas le cas,
+        vérifiez vos paramètres d'impression. Dans les paramètres avancés, modifiez la mise à l'échelle pour qu'elle soit sur la valeur "défaut"
+        <p>Dans l'attente de vous accueillir,</p>
         Cordialement,</p>
         <p style="color:darkblue;">------------------<br>
         Portail BTLec EST</p>
