a56b3e4978fd09bfe3500bb3860d24c4d74b1766
montusclat <vmontusclat@gmail.com>
2020-09-14 10:49:03 +0200

    fin nouveau traitement formulaire de recherche

--- public/litiges/bt-litige-encours.php
+++ public/litiges/bt-litige-encours.php
@@ -11,6 +11,14 @@ if(!isset($_SESSION['id'])){
 $pageCss=explode(".php",basename(__file__));
 $pageCss=$pageCss[0];
 $cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+
+
+
+require('../../Class/FormHelpers.php');
+
+
+
+
 //------------------------------------------------------
 //			FONCTION
 //------------------------------------------------------
@@ -260,14 +268,22 @@ if(isset($_GET['notallowed'])){
 // 		}
 // 	}
 // }
-echo "<pre>";
-print_r($_SESSION);
-echo '</pre>';
 
+
+// 1- traitement des formulaires de recherche
+// si un des 2 formulaires est validé, on stock les post en var de sessions
+// ce sont elle qui sont vérifiée pour le réaffichage des formulaires
+// on reset les var de session du formulaire non validé pour le cas où il aurait été
+// utilisé précédemment. Cela permet de ne pas cumuler les critères de recherches
+// en revanche les critères de filtres eux sont cumulés aux formulaires
+// pour retirer les filtres, l'utilisateur doit cliquer sur le bouton adéquat
+// on recharge la page après traitement des posts pour que les var de session soient utilisées par la requete
 
 
 if(isset($_POST['search_one'])){
 	unset($_SESSION['form-data']);
+	unset($_SESSION['form-data-deux']);
+	// date début
 	if (isset($_POST['date_start']) && !empty($_POST['date_start'])) {
 		$_SESSION['form-data']['date_start']=$_POST['date_start'];
 	}else{
@@ -276,7 +292,7 @@ if(isset($_POST['search_one'])){
 		}
 	}
 
-
+	// date fin
 	if (isset($_POST['date_end']) && !empty($_POST['date_end'])) {
 		$_SESSION['form-data']['date_end']=$_POST['date_end'];
 	}else{
@@ -284,6 +300,8 @@ if(isset($_POST['search_one'])){
 			unset($_SESSION['form-data']['date_end']);
 		}
 	}
+
+	// etat
 	if (isset($_POST['etat']) && !empty($_POST['etat'])) {
 		$_SESSION['form-data']['etat']=$_POST['etat'];
 	}else{
@@ -291,34 +309,51 @@ if(isset($_POST['search_one'])){
 			unset($_SESSION['form-data']['etat']);
 		}
 	}
-	header("Location: ".$_SERVER['PHP_SELF'].$successQ,true,303);
+
+	header("Location: ".$_SERVER['PHP_SELF'],true,303);
 }
 
 if(isset($_POST['search_two'])){
 	unset($_SESSION['form-data']);
 	unset($_SESSION['form-data-deux']);
+
+	// article
+	// si article est coché, on fait une requete avec une jointure sur la table détail
 	if(isset($_POST['article'])){
 		$_SESSION['form-data-deux']['article']=true;
 	}
+
+	// search_strg
 	if(isset($_POST['search_strg']) && !empty($_POST['search_strg'])){
-		$_SESSION['form-data-deux']['strg']=$_POST['search_strg'];
+		$_SESSION['form-data-deux']['search_strg']=$_POST['search_strg'];
 	}else{
 		unset($_SESSION['form-data-deux']['search_strg']);
 
 	}
-header("Location: ".$_SERVER['PHP_SELF'].$successQ,true,303);
+	header("Location: ".$_SERVER['PHP_SELF'],true,303);
 }
-// if(isset($_POST['clear_form'])){
-// 	unset($_SESSION['form-data']);
-// 	$_SESSION['form-data']['date_start']=$_POST['date_start']=((new DateTime())->modify('- 90 days'))->format('Y-m-d');
-// 	// $_SESSION['form-data']['date_start']=$_POST['date_start']=((new DateTime())->modify('first day of January this year'))->format('Y-m-d');
-// 	$_SESSION['form-data']['date_end']=$_POST['date_end']=date('Y-m-d');
-
-// }
+if(isset($_POST['clear_form'])){
+	unset($_SESSION['form-data']);
+	unset($_SESSION['form-data-deux']);
+	unset($_SESSION['filter-data']);
+	header("Location: ".$_SERVER['PHP_SELF'],true,303);
+}
 
 include 'bt-litige-encours-filtres-ex.php';
 
 // $fAllActive=globalSearch($pdoLitige);
+
+
+
+// 2- traitement des variables de session
+// on compose la requete en fonction des var de session existantes
+// 3 grand groupes :
+// form-data pour le formulaire 1
+// form-data-deux pour le formulaire 2
+// filter data pour les filtres
+// on ajoute tous les critères de recherche au tableau paramList
+
+
 if(isset($_SESSION['form-data'])){
 	$paramList=[];
 	// unset($_SESSION['form-data']);
@@ -346,8 +381,12 @@ if(isset($_SESSION['form-data'])){
 	// $listLitige=getListLitige($pdoLitige);
 }
 if (isset($_SESSION['form-data-deux'])) {
-	if(isset($_SESSION['form-data-deux']['strg'])){
-		$paramStrg= "concat(dossiers.dossier,magasin.mag.deno,dossiers.galec,magasin.mag.id) LIKE '%".$_POST['search_strg'] ."%'";
+	if(isset($_SESSION['form-data-deux']['search_strg']) && !isset($_SESSION['form-data-deux']['article'])){
+		$paramStrg= "concat(dossiers.dossier,magasin.mag.deno,dossiers.galec,magasin.mag.id) LIKE '%".$_SESSION['form-data-deux']['search_strg'] ."%'";
+	}elseif(isset($_SESSION['form-data-deux']['search_strg']) && isset($_SESSION['form-data-deux']['article'])){
+		$paramStrg= "details.article LIKE '%".$_SESSION['form-data-deux']['search_strg'] ."%'";
+	}else{
+		$paramStrg="";
 	}
 	$paramList[]=$paramStrg;
 }
@@ -370,12 +409,11 @@ if(isset($_SESSION['filter-data'])){
 	$paramList[]=$paramCommission;
 }
 
-
+// 3- requete grace à paramList si il existe ou
+// requete par défaut
 
 if(!isset($paramList)){
 	$listLitige=getListLitige($pdoLitige);
-
-	echo "pas de paramètres";
 }else{
 
 	$paramList=array_filter($paramList);
@@ -387,22 +425,29 @@ if(!isset($paramList)){
 	};
 
 
-    // retire les élements videq du tableau
 	$params=join(' AND ',array_map($joinParam,$paramList));
 
-	$query="SELECT dossiers.id as id_main, dossiers.dossier, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, dossiers.galec, dossiers.etat_dossier, dossiers.esp,
-	dossiers.vingtquatre, dossiers.valo, dossiers.ctrl_ok, dossiers.commission,	magasin.mag.deno, magasin.mag.centrale,  magasin.mag.id as btlec, etat.etat	FROM dossiers
-	LEFT JOIN etat ON id_etat=etat.id
-	LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec WHERE
-	$params
-	ORDER BY dossiers.dossier DESC";
-	echo "FORM ONE " .$query;
+	// 2 requetes types : une sur la table dossier "seule", une sur la table dossier jointe à la table article
+	if(isset($_SESSION['form-data-deux']['article'])){
+		$query="SELECT dossiers.id as id_main, dossiers.dossier, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, dossiers.galec, dossiers.etat_dossier, dossiers.esp,
+		dossiers.vingtquatre, dossiers.valo, dossiers.ctrl_ok, dossiers.commission,	magasin.mag.deno, magasin.mag.centrale,  magasin.mag.id as btlec, etat.etat	FROM dossiers
+		LEFT JOIN details ON dossiers.id=details.id_dossier
+		LEFT JOIN etat ON id_etat=etat.id
+		LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec WHERE
+		$params
+		ORDER BY dossiers.dossier DESC";
+
+	}else{
+		$query="SELECT dossiers.id as id_main, dossiers.dossier, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, dossiers.galec, dossiers.etat_dossier, dossiers.esp,
+		dossiers.vingtquatre, dossiers.valo, dossiers.ctrl_ok, dossiers.commission,	magasin.mag.deno, magasin.mag.centrale,  magasin.mag.id as btlec, etat.etat	FROM dossiers
+		LEFT JOIN etat ON id_etat=etat.id
+		LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec WHERE
+		$params
+		ORDER BY dossiers.dossier DESC";
+	}
 
 	$req=$pdoLitige->query($query);
-
-
 	$listLitige=$req->fetchAll(PDO::FETCH_ASSOC);
-
 }
 $arCentrale=getListCentrale($pdoMag);
 $nbLitiges=count($listLitige);
