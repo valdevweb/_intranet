643157656b8f2d51600748cb289ebd1a7dd371d6
montusclat <vmontusclat@gmail.com>
2020-09-14 10:56:54 +0200

    nettoyage et découpage

--- public/litiges/bt-litige-encours.php
+++ public/litiges/bt-litige-encours.php
@@ -126,26 +126,6 @@ function getListCentrale($pdoMag){
 // 	));
 // 	return $req->fetch(PDO::FETCH_ASSOC);
 // }
-function test($pdoLitige){
-	$dateStart="2020-01-01 00:00:00";
-	$dateEnd="2020-09-11 00:00:00";
-
-	$req=$pdoLitige->prepare("SELECT dossiers.id as id_main, dossiers.dossier, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, dossiers.galec, dossiers.etat_dossier, dossiers.esp,
-		dossiers.vingtquatre, dossiers.valo, dossiers.ctrl_ok, dossiers.commission,	magasin.mag.deno, magasin.mag.centrale,  magasin.mag.id as btlec, etat.etat	FROM dossiers
-		LEFT JOIN etat ON id_etat=etat.id
-		LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec WHERE
-		date_crea BETWEEN :date_start AND :date_end
-
-		ORDER BY dossiers.dossier DESC");
-
-	$req->execute([
-		':date_start'=>$dateStart,
-		':date_end'	=>$dateEnd,
-
-	]);
-	return $req->fetchAll(PDO::FETCH_ASSOC);
-}
-
 
 
 function getSumValoByType($pdoLitige)
@@ -255,159 +235,21 @@ if(isset($_GET['notallowed'])){
 	$errors[]="Vous n'êtes pas autorisé à modifier le statut 'validé en commission'";
 }
 
-// initialisation
 
 
-// if(isset($_POST['search_form'])){
-// 	foreach ($_POST as $key => $value) {
-// 		if($key != 'reset-pending' || $key != 'clear_form' || $key != 'pending' || $key !='vingtquatre' || $key != 'reset-vingtquatre'){
-// 			if($value !=''){
-// 				$_SESSION['form-data'][$key]=$value;
-
-// 			}
-// 		}
-// 	}
-// }
 
 
-// 1- traitement des formulaires de recherche
-// si un des 2 formulaires est validé, on stock les post en var de sessions
-// ce sont elle qui sont vérifiée pour le réaffichage des formulaires
-// on reset les var de session du formulaire non validé pour le cas où il aurait été
-// utilisé précédemment. Cela permet de ne pas cumuler les critères de recherches
-// en revanche les critères de filtres eux sont cumulés aux formulaires
-// pour retirer les filtres, l'utilisateur doit cliquer sur le bouton adéquat
-// on recharge la page après traitement des posts pour que les var de session soient utilisées par la requete
 
 
-if(isset($_POST['search_one'])){
-	unset($_SESSION['form-data']);
-	unset($_SESSION['form-data-deux']);
-	// date début
-	if (isset($_POST['date_start']) && !empty($_POST['date_start'])) {
-		$_SESSION['form-data']['date_start']=$_POST['date_start'];
-	}else{
-		if(isset($_SESSION['form-data']['date_start'])){
-			unset($_SESSION['form-data']['date_start']);
-		}
-	}
 
-	// date fin
-	if (isset($_POST['date_end']) && !empty($_POST['date_end'])) {
-		$_SESSION['form-data']['date_end']=$_POST['date_end'];
-	}else{
-		if(isset($_SESSION['form-data']['date_end'])){
-			unset($_SESSION['form-data']['date_end']);
-		}
-	}
-
-	// etat
-	if (isset($_POST['etat']) && !empty($_POST['etat'])) {
-		$_SESSION['form-data']['etat']=$_POST['etat'];
-	}else{
-		if(isset($_SESSION['form-data']['etat'])){
-			unset($_SESSION['form-data']['etat']);
-		}
-	}
-
-	header("Location: ".$_SERVER['PHP_SELF'],true,303);
-}
-
-if(isset($_POST['search_two'])){
-	unset($_SESSION['form-data']);
-	unset($_SESSION['form-data-deux']);
-
-	// article
-	// si article est coché, on fait une requete avec une jointure sur la table détail
-	if(isset($_POST['article'])){
-		$_SESSION['form-data-deux']['article']=true;
-	}
-
-	// search_strg
-	if(isset($_POST['search_strg']) && !empty($_POST['search_strg'])){
-		$_SESSION['form-data-deux']['search_strg']=$_POST['search_strg'];
-	}else{
-		unset($_SESSION['form-data-deux']['search_strg']);
-
-	}
-	header("Location: ".$_SERVER['PHP_SELF'],true,303);
-}
-if(isset($_POST['clear_form'])){
-	unset($_SESSION['form-data']);
-	unset($_SESSION['form-data-deux']);
-	unset($_SESSION['filter-data']);
-	header("Location: ".$_SERVER['PHP_SELF'],true,303);
-}
-
+include 'bt-litige-encours-formsearch-ex.php';
 include 'bt-litige-encours-filtres-ex.php';
-
-// $fAllActive=globalSearch($pdoLitige);
-
+include 'bt-litige-encours-sessions-ex.php';
 
 
-// 2- traitement des variables de session
-// on compose la requete en fonction des var de session existantes
-// 3 grand groupes :
-// form-data pour le formulaire 1
-// form-data-deux pour le formulaire 2
-// filter data pour les filtres
-// on ajoute tous les critères de recherche au tableau paramList
 
 
-if(isset($_SESSION['form-data'])){
-	$paramList=[];
-	// unset($_SESSION['form-data']);
-	unset($_SESSION['form-data-deux']);
-	if(isset($_SESSION['form-data']['date_start']) && !empty($_SESSION['form-data']['date_start'])){
-		$dateStart=$_SESSION['form-data']['date_start'];
-	}else{
-		$dateStart="2019-01-01 00:00:00";
-	}
-	if(isset($_SESSION['form-data']['date_end']) && !empty($_SESSION['form-data']['date_end'])){
-		$dateEnd=$_SESSION['form-data']['date_end'];
-	}else{
-		$dateEnd=date('Y-m-d') ." 00:00:00";
-	}
-	$paramDate="date_crea BETWEEN '".$dateStart ."'  AND '".$dateEnd."'";
-	$paramList[]=$paramDate;
 
-
-	if(isset($_SESSION['form-data']['etat'])){
-		$paramEtat= ' id_etat = '.$_SESSION['form-data']['etat'];
-	}else{
-		$paramEtat='';
-	}
-	$paramList[]=$paramEtat;
-	// $listLitige=getListLitige($pdoLitige);
-}
-if (isset($_SESSION['form-data-deux'])) {
-	if(isset($_SESSION['form-data-deux']['search_strg']) && !isset($_SESSION['form-data-deux']['article'])){
-		$paramStrg= "concat(dossiers.dossier,magasin.mag.deno,dossiers.galec,magasin.mag.id) LIKE '%".$_SESSION['form-data-deux']['search_strg'] ."%'";
-	}elseif(isset($_SESSION['form-data-deux']['search_strg']) && isset($_SESSION['form-data-deux']['article'])){
-		$paramStrg= "details.article LIKE '%".$_SESSION['form-data-deux']['search_strg'] ."%'";
-	}else{
-		$paramStrg="";
-	}
-	$paramList[]=$paramStrg;
-}
-if(isset($_SESSION['filter-data'])){
-	if(isset($_SESSION['filter-data']['vingtquatre']) ){
-		$paramVingtQuatre= ' vingtquatre = 1 OR esp = 1';
-	}else{
-		$paramVingtQuatre= '';
-	}
-	$paramList[]=$paramVingtQuatre;
-
-	if (isset($_SESSION['filter-data']['pending']) && $_SESSION['filter-data']['pending']==1) {
-		$paramCommission= " commission=1 ";
-	}elseif (isset($_SESSION['filter-data']['pending']) && $_SESSION['filter-data']['pending']==0){
-		$paramCommission= " commission=0 ";
-	}else{
-		$paramCommission= "";
-	}
-
-	$paramList[]=$paramCommission;
-}
 
 // 3- requete grace à paramList si il existe ou
 // requete par défaut
@@ -451,7 +293,7 @@ if(!isset($paramList)){
 }
 $arCentrale=getListCentrale($pdoMag);
 $nbLitiges=count($listLitige);
-// $valoTotal=getSumValo($pdoLitige);
+$valoTotal=getSumValo($pdoLitige);
 // $valoEtat=getSumValoByType($pdoLitige);
 
 
