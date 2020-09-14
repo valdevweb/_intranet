e634c301fe69a2dd68cfbdf428115b213c5ec79f
montusclat <vmontusclat@gmail.com>
2020-09-14 10:48:43 +0200

    traitement article coché ou non ap submit + déplacement clear form

--- public/litiges/bt-litige-encours-formsearch.php
+++ public/litiges/bt-litige-encours-formsearch.php
@@ -59,9 +59,7 @@
 			</div>
 			<div class="row">
 				<div class="col"></div>
-				<div class="col-auto">
-					<button class="btn btn-red" type="submit" name="clear_form"><i class="fas fa-eraser pr-2"></i>Effacer toutes les sélections</button>
-				</div>
+
 				<div class="col-auto">
 					<button class="btn btn-black mr-5" type="submit"  name="search_one"><i class="fas fa-search pr-2"></i>Rechercher</button>
 				</div>
@@ -78,12 +76,12 @@
 			<div class="row">
 				<div class="col-4">
 					<div class="form-group" id="equipe">
-						<input class="form-control mr-5 pr-5" placeholder="n°litige,  magasin, galec" name="search_strg" type="text"  value="<?=isset($_SESSION['form-data']['search_strg'])? $_SESSION['form-data']['search_strg'] : "" ?>">
+						<input class="form-control mr-5 pr-5" placeholder="n°litige,  magasin, galec" name="search_strg" type="text"  value="<?=isset($_SESSION['form-data-deux']['search_strg'])? $_SESSION['form-data-deux']['search_strg'] : "" ?>">
 					</div>
 				</div>
 				<div class="col">
 					<div class="form-check form-check-inline mt-3">
-						<input type="checkbox" class="form-check-input" name="article" id="article">
+						<input type="checkbox" class="form-check-input" name="article" id="article" <?= isset($_SESSION['form-data-deux']['article'])? "checked" : ""?>>
 						<label for="article" class="form-check-label">recherche d'un code article</label>
 					</div>
 				</div>
@@ -93,6 +91,13 @@
 
 			</div>
 		</form>
+		<div class="row">
+			<div class="col text-center">
+					<form action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+						<button class="btn btn-red" type="submit" name="clear_form"><i class="fas fa-eraser pr-2"></i>Effacer toutes les sélections</button>
+					</form>
+			</div>
+		</div>
 
 
 	</div>
