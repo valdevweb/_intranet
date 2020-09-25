f39cf2385500dfbef31493cc52a16e30954656ac
montusclat <vmontusclat@gmail.com>
2020-09-25 14:14:53 +0200

    test

--- batch-lotus/temp.php
+++ batch-lotus/temp.php
@@ -1,24 +1,36 @@
 <?php
 
-if (preg_match('/_btlecest/', dirname(__FILE__))){
-	set_include_path("D:\www\intranet\btlecest\\");
-	$manuel=true;
-
-}
-else{
-	set_include_path("D:\www\intranet\btlecest\\");
-	$manuel=false;
-
-}
+$ldFull="ACHERERE - DISTRIBUTION-egag";
+$decoupageLdFull=explode('-',$ldFull);
+$suffixe='-'.end($decoupageLdFull);
+$suffixeLength=strlen($suffixe);
+$ldShort=substr($ldFull,0,strlen($ldFull)-$suffixeLength);
 
 
-include 'config\config.inc.php';
-function getContact($pdoCm){
-	$req=$pdoCm->query("SELECT * FROM mag_contact");
-	return $req->fetchAll(PDO::FETCH_ASSOC);
-}
 
-$contactList=getContact($pdoCm);
-	echo "<pre>";
-	print_r($contactList);
-	echo '</pre>';
+echo $suffixe;
+echo "<br>";
+
+echo $ldShort;
+// if (preg_match('/_btlecest/', dirname(__FILE__))){
+// 	set_include_path("D:\www\intranet\btlecest\\");
+// 	$manuel=true;
+
+// }
+// else{
+// 	set_include_path("D:\www\intranet\btlecest\\");
+// 	$manuel=false;
+
+// }
+
+
+// include 'config\config.inc.php';
+// function getContact($pdoCm){
+// 	$req=$pdoCm->query("SELECT * FROM mag_contact");
+// 	return $req->fetchAll(PDO::FETCH_ASSOC);
+// }
+
+// $contactList=getContact($pdoCm);
+// 	echo "<pre>";
+// 	print_r($contactList);
+// 	echo '</pre>';
