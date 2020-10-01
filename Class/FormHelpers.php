f32eb6102759d9707d9329fda4daef42ffe04c88
montusclat <vmontusclat@gmail.com>
2020-10-01 09:07:53 +0200

    ajout commentaires

--- Class/FormHelpers.php
+++ Class/FormHelpers.php
@@ -2,51 +2,55 @@
 
 class FormHelpers{
 
-    public static function checkChecked($value,$field){
-        if(isset($_POST[$field])){
-            if($_POST[$field]==$value){
-                return "checked";
+/*---------------------------------------------------------
+    Réaffichage de données après soumission d'un formulaire
+---------------------------------------------------------*/
+
+ // case à cocher, btn radio
+        public static function checkChecked($value,$field){
+            if(isset($_POST[$field])){
+                if($_POST[$field]==$value){
+                    return "checked";
+                }
             }
+            return "";
         }
-
-        return "";
-    }
-// réffichage liste déroulante formulaire
-    public static function checkSelected($value,$field){
-        if(isset($_POST[$field])){
-            if($_POST[$field]==$value){
-                return "selected";
+// liste déroulante
+        public static function checkSelected($value,$field){
+            if(isset($_POST[$field])){
+                if($_POST[$field]==$value){
+                    return "selected";
+                }
             }
+            return "";
         }
 
-        return "";
-    }
-
-    public static function restorePost($field){
-        if(isset($_POST[$field])){
-            return $_POST[$field];
-        }
-        return "";
-    }
-
-    public static function checkCheckedArray($value,$array){
-        if(isset($array)){
-            if($array==$value){
-                return "checked";
+// input ou textareo
+        public static function restorePost($field){
+            if(isset($_POST[$field])){
+                return $_POST[$field];
             }
+            return "";
         }
 
-        return "";
-    }
-// réffichage liste déroulante formulaire
-    public static function checkSelectedSession($value,$field){
-        if(isset($_SESSION[$field])){
-            if($_SESSION[$field]==$value){
-                return "selected";
+ // case à cocher, btn radio
+        public static function checkCheckedArray($value,$array){
+            if(isset($array)){
+                if($array==$value){
+                    return "checked";
+                }
             }
+
+            return "";
+        }
+//  liste déroulante
+        public static function checkSelectedSession($value,$field){
+            if(isset($_SESSION[$field])){
+                if($_SESSION[$field]==$value){
+                    return "selected";
+                }
+            }
+            return "";
         }
 
-        return "";
     }
-
-}
