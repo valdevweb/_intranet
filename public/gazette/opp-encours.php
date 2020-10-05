6a62611cdb6866ddbf6d9d052bece4ea1307408f
montusclat <vmontusclat@gmail.com>
2020-10-05 15:26:37 +0200

    opp en cours pour magasin

---
+++ public/gazette/opp-encours.php
@@ -0,0 +1,85 @@
+<?php
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+	header('Location:'. ROOT_PATH.'/index.php');
+	echo "pas de variable session";
+
+}
+//			css dynamique
+//----------------------------------------------------------------
+$pageCss=explode(".php",basename(__file__));
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+
+require_once '../../Class/OpportuniteDAO.php';
+
+
+$errors=[];
+$success=[];
+
+define("DIR_UPLOAD_OPP",DIR_UPLOAD."opportunites\\");
+define("URL_UPLOAD_OPP",URL_UPLOAD."opportunites/");
+$oppDao=new OpportuniteDAO($pdoBt);
+
+
+$listOpp=$oppDao->getActiveOpp();
+
+
+
+
+//------------------------------------------------------
+//			FONCTION
+//------------------------------------------------------
+
+
+//------------------------------------------------------
+//			VIEW
+//------------------------------------------------------
+include('../view/_head-bt.php');
+include('../view/_navbar.php');
+?>
+<!--********************************
+DEBUT CONTENU CONTAINER
+*********************************-->
+<div class="container">
+	<h1 class="text-main-blue pt-5">Opportunités en cours</h1>
+
+	<div class="row">
+		<div class="col-lg-1"></div>
+		<div class="col">
+			<?php
+			include('../view/_errors.php');
+			?>
+		</div>
+		<div class="col-lg-1"></div>
+	</div>
+	<div class="row mb-3">
+		<div class="col-lg-2 col-xl-3"></div>
+		<div class="col border rounded">
+			<div class="text-center">Accès rapide : </div>
+			<ul class="leaders">
+				<?php foreach ($listOpp as $key => $opp): ?>
+					<li><span>
+						<a href="#<?=$opp['id']?>" class="mini-nav"><?=$opp['title']?></a>
+						<?=($opp['date_start']==date('Y-m-d') ||  $opp['date_start']==(new DateTime('yesterday'))->format('Y-m-d'))?
+									"<span class='badge badge-warning ml-3'>Nouveau</span>" :""
+									?>
+					</span>
+						<span>jusqu'au <?=date('d/m/Y', strtotime($opp['date_end']))?></span>
+					</li>
+					<?php $oppIds[]=$opp['id'];?>
+				<?php endforeach ?>
+			</ul>
+		</div>
+		<div class="col-lg-2 col-xl-3"></div>
+	</div>
+	<?php
+	include 'opp-display-inc.php'
+	?>
+
+	<!-- ./container -->
+</div>
+
+<?php
+require '../view/_footer-bt.php';
+?>
