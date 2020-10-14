c61d59732df6012ad8c24b67432f64a09f32d3af
montusclat <vmontusclat@gmail.com>
2020-10-14 15:04:07 +0200

    remplacement fn page par methode class LitigeDao

--- public/litiges/bt-detail-litige.php
+++ public/litiges/bt-detail-litige.php
@@ -30,62 +30,7 @@ require('../../Class/LitigeDao.php');
 //			FONCTION
 //------------------------------------------------------
 
-// info produit
 
-
-
-
-function getFirstDial($pdoLitige){
-	$req=$pdoLitige->prepare("SELECT * FROM `dial` WHERE id_dossier=:id AND mag=3");
-	$req->execute(array(
-		':id'	=>$_GET['id']
-	));
-	return $req->fetch(PDO::FETCH_ASSOC);
-}
-
-
-function getInfos($pdoLitige){
-	$req=$pdoLitige->prepare("SELECT transporteur.transporteur, affrete.affrete, transit.transit, CONCAT(prepa.nom,' ', prepa.prenom) as fullprepa, CONCAT(ctrl.nom,' ',ctrl.prenom) as fullctrl,CONCAT(chg.nom,' ',chg.prenom) as fullchg, mt_transp, mt_assur, mt_fourn, mt_mag, fac_mag, DATE_FORMAT(date_prepa,'%d-%m-%Y') as dateprepa, ctrl_ok FROM dossiers
-		LEFT JOIN transporteur ON id_transp=transporteur.id
-		LEFT JOIN affrete ON id_affrete=affrete.id
-		LEFT JOIN transit ON id_transit=transit.id
-		LEFT JOIN equipe as prepa ON id_prepa=prepa.id
-		LEFT JOIN equipe as ctrl ON id_ctrl=ctrl.id
-		LEFT JOIN equipe as chg ON id_chg=chg.id
-		LEFT JOIN equipe as ctrl_stock ON id_ctrl_stock=ctrl_stock.id
-		WHERE  dossiers.id= :id ");
-
-	$req->execute(array(
-		':id'	=>$_GET['id']
-	));
-	return $req->fetch(PDO::FETCH_ASSOC);
-}
-
-
-function getAnalyse($pdoLitige){
-	$req=$pdoLitige->prepare("SELECT gt, imputation, typo, etat, analyse, conclusion FROM dossiers
-		LEFT JOIN gt ON id_gt=gt.id
-		LEFT JOIN imputation ON id_imputation=imputation.id
-		LEFT JOIN typo ON id_typo=typo.id
-		LEFT JOIN etat ON id_etat=etat.id
-		LEFT JOIN analyse ON id_analyse=analyse.id
-		LEFT JOIN conclusion ON id_conclusion=conclusion.id
-		WHERE dossiers.id= :id");
-	$req->execute(array(
-		':id'	=>$_GET['id']
-	));
-	return $req->fetch(PDO::FETCH_ASSOC);
-
-}
-
-function getAction($pdoLitige){
-	$req=$pdoLitige->prepare("SELECT libelle, id_web_user, DATE_FORMAT(date_action, '%d-%m-%Y')as dateFr,  pj, sav, achats FROM action  WHERE action.id_dossier= :id ORDER BY date_action");
-	$req->execute(array(
-		':id'		=>$_GET['id']
-
-	));
-	return $req->fetchAll(PDO::FETCH_ASSOC);
-}
 
 function getFinance($pdoQlik, $btlec, $year){
 	$req=$pdoQlik->prepare("SELECT CA_Annuel FROM statsventesadh WHERE CodeBtlec= :btlec AND AnneeCA= :year");
@@ -269,11 +214,10 @@ function addSerials($pdoLitige,$idDetail,$values){
 
 $litigeDao=new LitigeDao($pdoLitige);
 $infoLitige=$litigeDao->getLitigeDossierDetailReclamMagEtatById($_GET['id']);
-
-$actionList=getAction($pdoLitige);
-$infos=getInfos($pdoLitige);
-$analyse=getAnalyse($pdoLitige);
-$firstDial=getFirstDial($pdoLitige);
+$firstDial=$litigeDao->getFirstDial($_GET['id']);
+$infos=$litigeDao->getInfos($_GET['id']);
+$analyse=$litigeDao->getAnalyse($_GET['id']);
+$actionList=$litigeDao->getAction($_GET['id']);
 
 $coutTotal=$infos['mt_transp']+$infos['mt_assur']+$infos['mt_fourn']+$infos['mt_mag'];
 
