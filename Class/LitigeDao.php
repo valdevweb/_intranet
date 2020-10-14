5e29b27493f4762ed387db7c42cb6ed56bb7e776
montusclat <vmontusclat@gmail.com>
2020-10-14 11:43:02 +0200

    Crea d'une class pour les litges

---
+++ Class/LitigeDao.php
@@ -0,0 +1,39 @@
+<?php
+
+class LitigeDao{
+
+	// la db est pdoLitige
+	private $pdo;
+
+
+	public function __construct($pdo){
+		$this->setPdo($pdo);
+	}
+
+	public function setPdo($pdo){
+		$this->pdo=$pdo;
+		return $pdo;
+	}
+
+	public function getLitigeDossierDetailReclamMagEtatById($idLitige){
+		$req=$this->pdo->prepare("
+			SELECT
+			dossiers.id as id_main,	dossiers.dossier, dossiers.date_crea, DATE_FORMAT(dossiers.date_crea, '%d-%m-%Y') as datecrea, dossiers.user_crea, dossiers.galec, dossiers.etat_dossier, dossiers.vingtquatre, dossiers.id_web_user, dossiers.nom, dossiers.valo, dossiers.flag_valo, dossiers.id_robbery, dossiers.commission,
+			details.inv_palette, details.inv_qte, details.box_tete, details.box_art, details.id as id_detail, details.id_reclamation, details.ean, details.id_dossier, details.palette, details.facture, details.article, details.tarif, details.qte_cde, details.qte_litige, details.valo_line, details.dossier_gessica, details.descr, details.fournisseur, details.pj, DATE_FORMAT(details.date_facture, '%d-%m-%Y') as datefacture, details.serials, details.inversion, details.inv_article, details.inv_fournisseur, details.inv_tarif, details.inv_descr,
+			reclamation.reclamation,
+			magasin.mag.deno as mag, magasin.mag.centrale, magasin.mag.id as btlec,
+			etat.etat
+			FROM dossiers
+			LEFT JOIN details ON dossiers.id=details.id_dossier
+			LEFT JOIN reclamation ON details.id_reclamation = reclamation.id
+			LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec
+			LEFT JOIN etat ON etat_dossier=etat.id
+			WHERE dossiers.id= :id ORDER BY date_crea");
+		$req->execute(array(
+			':id'	=>$idLitige
+		));
+		return $req->fetchAll(PDO::FETCH_ASSOC);
+	// return $req->errorInfo();
+	}
+
+}
