178e22cc6e2134f0b7259a1976e93139afa53b12
montusclat <vmontusclat@gmail.com>
2020-10-14 15:02:41 +0200

    Ajout methodes ut dans detail et bt-generate-fiche

--- Class/LitigeDao.php
+++ Class/LitigeDao.php
@@ -36,4 +36,58 @@ class LitigeDao{
 	// return $req->errorInfo();
 	}
 
+
+	public function getFirstDial($idLitige){
+		$req=$this->pdo->prepare("SELECT * FROM `dial` WHERE id_dossier=:id AND mag=3");
+		$req->execute(array(
+			':id'	=>$idLitige
+		));
+		return $req->fetch(PDO::FETCH_ASSOC);
+	}
+
+
+	public function getInfos($idLitige){
+		$req=$this->pdo->prepare("SELECT transporteur.transporteur, affrete.affrete, transit.transit, CONCAT(prepa.nom,' ', prepa.prenom) as fullprepa, CONCAT(ctrl.nom,' ',ctrl.prenom) as fullctrl,CONCAT(chg.nom,' ',chg.prenom) as fullchg, mt_transp, mt_assur, mt_fourn, mt_mag, fac_mag, DATE_FORMAT(date_prepa,'%d-%m-%Y') as dateprepa, ctrl_ok FROM dossiers
+			LEFT JOIN transporteur ON id_transp=transporteur.id
+			LEFT JOIN affrete ON id_affrete=affrete.id
+			LEFT JOIN transit ON id_transit=transit.id
+			LEFT JOIN equipe as prepa ON id_prepa=prepa.id
+			LEFT JOIN equipe as ctrl ON id_ctrl=ctrl.id
+			LEFT JOIN equipe as chg ON id_chg=chg.id
+			LEFT JOIN equipe as ctrl_stock ON id_ctrl_stock=ctrl_stock.id
+			WHERE  dossiers.id= :id ");
+
+		$req->execute(array(
+			':id'	=>$idLitige
+		));
+		return $req->fetch(PDO::FETCH_ASSOC);
+	}
+
+
+	public function getAnalyse($idLitige){
+		$req=$this->pdo->prepare("SELECT gt, imputation, typo, etat, analyse, conclusion FROM dossiers
+			LEFT JOIN gt ON id_gt=gt.id
+			LEFT JOIN imputation ON id_imputation=imputation.id
+			LEFT JOIN typo ON id_typo=typo.id
+			LEFT JOIN etat ON id_etat=etat.id
+			LEFT JOIN analyse ON id_analyse=analyse.id
+			LEFT JOIN conclusion ON id_conclusion=conclusion.id
+			WHERE dossiers.id= :id");
+		$req->execute(array(
+			':id'	=>$idLitige
+		));
+		return $req->fetch(PDO::FETCH_ASSOC);
+
+	}
+
+	public function getAction($idLitige){
+		$req=$this->pdo->prepare("SELECT libelle, action.id_web_user, DATE_FORMAT(date_action, '%d-%m-%Y')as dateFr, concat(prenom, ' ', nom) as name, pj, action.sav, achats FROM action LEFT JOIN web_users.intern_users ON action.id_web_user=web_users.intern_users.id_web_user WHERE action.id_dossier= :id ORDER BY date_action");
+		$req->execute(array(
+			':id'		=>$idLitige
+
+		));
+		return $req->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+
 }
