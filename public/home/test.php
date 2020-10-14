60c8fde0af507ba382529c5562e0245671cfdef6
montusclat <vmontusclat@gmail.com>
2020-10-14 09:15:37 +0200

    tests

--- public/home/test.php
+++ public/home/test.php
@@ -1,6 +1,70 @@
 <?php
+require('../../config/autoload.php');
+if(!isset($_SESSION['id'])){
+	header('Location:'. ROOT_PATH.'/index.php');
+	echo "pas de variable session";
+
+}
+//			css dynamique
+//----------------------------------------------------------------
+$pageCss=explode(".php",basename(__file__));
+$pageCss=$pageCss[0];
+$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
 
 
-	echo "<pre>";
-	print_r($_SESSION);
-	echo '</pre>';
+//------------------------------------------------------
+//			REQUIRES
+//------------------------------------------------------
+// require_once '../../vendor/autoload.php';
+
+
+
+//---------------------------------------
+//	ajout enreg dans stat
+//---------------------------------------
+// require "../../functions/stats.fn.php";
+// addRecord($pdoStat,basename(__file__),'consultation', "fichiers d'info du service achats", 101);
+
+
+ //------------------------------------------------------
+//			DECLARATIONS
+//------------------------------------------------------
+$errors=[];
+$success=[];
+
+
+
+//------------------------------------------------------
+//			FONCTION
+//------------------------------------------------------
+
+
+//------------------------------------------------------
+//			VIEW
+//------------------------------------------------------
+include('../view/_head-bt.php');
+include('../view/_navbarfull.php');
+?>
+<!--********************************
+DEBUT CONTENU CONTAINER
+*********************************-->
+<div class="container">
+	<h1 class="text-main-blue py-5 ">Main title</h1>
+
+	<div class="row">
+		<div class="col-lg-1"></div>
+		<div class="col">
+			<?php
+			include('../view/_errors.php');
+			?>
+		</div>
+		<div class="col-lg-1"></div>
+	</div>
+
+
+	<!-- ./container -->
+</div>
+
+<?php
+require '../view/_footer-bt.php';
+?>
