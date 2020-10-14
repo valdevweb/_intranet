a50e61b141da47a26ac74a973b4f61f5a2cd923d
montusclat <vmontusclat@gmail.com>
2020-10-14 11:51:32 +0200

    pas de modif

--- public/litiges/echanges.fn.php
+++ public/litiges/echanges.fn.php
@@ -13,8 +13,7 @@ $dials=getDialog($pdoLitige);
 
 
 
-function getBtName($pdoBt, $idwebuser)
-{
+function getBtName($pdoBt, $idwebuser){
 	$req=$pdoBt->prepare("SELECT CONCAT (prenom, ' ', nom) as name FROM btlec WHERE id_webuser= :id_web_user");
 	$req->execute(array(
 		':id_web_user'	=>$idwebuser
@@ -23,8 +22,7 @@ function getBtName($pdoBt, $idwebuser)
 }
 
 
-function getMagName($pdoUser, $idwebuser)
-{
+function getMagName($pdoUser, $idwebuser){
 	$req=$pdoUser->prepare("SELECT btlec.sca3.mag, btlec.sca3.btlec FROM btlec.sca3 LEFT JOIN web_users.users ON btlec.sca3.galec= web_users.users.galec WHERE web_users.users.id= :id_web_user ");
 	$req->execute(array(
 		':id_web_user'	=>$idwebuser
