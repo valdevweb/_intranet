6fc27a4bc1ec21d8719e17f72b28d3994d7bd5dc
montusclat <vmontusclat@gmail.com>
2020-10-19 08:47:24 +0200

    LITIGE renommage

--- public/litiges/declaration-hidden-session.php
+++ public/litiges/declaration-hidden-session.php
@@ -1,14 +1,11 @@
 <?php
-
- // require('../../config/pdo_connect.php');
 require('../../config/autoload.php');
 if(!isset($_SESSION['id'])){
-	echo "pas de variable session";
 	header('Location:'. ROOT_PATH.'/index.php');
 }
 
 
 $_SESSION['id_galec']= $_GET['galec'];
 
-	header('Location:declaration-basic.php');
+	header('Location:declaration-stepone.php');
 
