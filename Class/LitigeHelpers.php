7ae4ff84d339c2dab4e1927329ffa556784a2200
montusclat <vmontusclat@gmail.com>
2020-10-27 09:22:49 +0100

    2 méthodes pour list reclamation, une avec les masques l'autre sans

--- Class/LitigeHelpers.php
+++ Class/LitigeHelpers.php
@@ -3,7 +3,16 @@
 
 class LitigeHelpers{
 
+	public static function listReclamationIncludingMasked($pdoLitige){
+		// !!!!! attention, on a masqué les inversions de référence mais on converti les manquant /excédent en inversion de référence
+		// donc on a parfois besoin des relcamation non masquée
+		$req=$pdoLitige->query("SELECT id, reclamation FROM reclamation ORDER BY reclamation");
+		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
+	}
+
 	public static function listReclamation($pdoLitige){
+		// !!!!! attention, on a masqué les inversions de référence mais on converti les manquant /excédent en inversion de référence
+		// donc on a parfois besoin des relcamation non masquée
 		$req=$pdoLitige->query("SELECT id, reclamation FROM reclamation WHERE mask=0 ORDER BY reclamation");
 		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
 	}
