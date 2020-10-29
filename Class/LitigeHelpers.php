fc99d62498ea4295244736238d77891605df1d48
montusclat <vmontusclat@gmail.com>
2020-10-29 08:55:42 +0100

    methode list reclamation noEdit : liste reclamation sans palette, inver
    pour modif litige

--- Class/LitigeHelpers.php
+++ Class/LitigeHelpers.php
@@ -16,6 +16,10 @@ class LitigeHelpers{
 		$req=$pdoLitige->query("SELECT id, reclamation FROM reclamation WHERE mask=0 ORDER BY reclamation");
 		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
 	}
+	public static function listReclamationEdit($pdoLitige){
+		$req=$pdoLitige->query("SELECT id, reclamation FROM reclamation WHERE noedit=0 ORDER BY reclamation");
+		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
+	}
 	public static function listTypo($pdoLitige){
 		$req=$pdoLitige->query("SELECT id, typo FROM typo WHERE mask=0 ORDER BY typo");
 		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
