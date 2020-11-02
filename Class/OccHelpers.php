8638fc08f470cf8e7162438cc17224d48d6a331c
montusclat <vmontusclat@gmail.com>
2020-11-02 09:09:04 +0100

    getPaletteNameByArticlePalette: nom de la palette pr 1 articlePalette

--- Class/OccHelpers.php
+++ Class/OccHelpers.php
@@ -2,9 +2,22 @@
 
 class OccHelpers{
 
-	   public static function arrayPalette($pdoOcc){
-        $req=$pdoOcc->query("SELECT id,palette FROM palettes ORDER BY id");
-        return $req->fetchAll(PDO::FETCH_KEY_PAIR);
-    }
+	public static function arrayPalette($pdoOcc){
+		$req=$pdoOcc->query("SELECT id,palette FROM palettes ORDER BY id");
+		return $req->fetchAll(PDO::FETCH_KEY_PAIR);
+	}
+
+	public static function getPaletteNameByArticlePalette($pdoOcc, $articlePalette){
+		$req=$pdoOcc->prepare("SELECT palette FROM palettes_articles LEFT JOIN palettes ON id_palette=palettes.id WHERE article_palette= :article_palette");
+		$req->execute([
+			':article_palette'	=>$articlePalette
+		]);
+		$data=$req->fetch(PDO::FETCH_ASSOC);
+		if(!empty($data)){
+			return $data['palette'];
+		}
+		return "";
+	}
+
 
 }
