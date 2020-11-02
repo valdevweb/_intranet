dbf637977ac4179f7f24eae065720f42a74f5df1
montusclat <vmontusclat@gmail.com>
2020-11-02 09:09:39 +0100

    affiche nom palette occ

--- public/litiges/bt-detail-litige/04-view-prods.php
+++ public/litiges/bt-detail-litige/04-view-prods.php
@@ -62,102 +62,108 @@
 					?>
 					<?php if ($prod['inversion'] ==""): ?>
 						<tr <?=$classBoxHead?>>
-							<td><?=$prod['article']?></td>
-							<td><?=$prod['dossier_gessica']?></td>
-							<td><?=$prod['palette']?></td>
-							<td><?=$boxIco.$prod['descr']?><br>Ean : <?=$prod['ean']?></td>
-							<td><?=$prod['fournisseur']?></td>
-							<td><?=$prod['reclamation']?></td>
-							<td class="text-right"><?=$prod['qte_litige']?></td>
-							<td class="text-right"><?=number_format((float)$prod['valo_line'],2,'.','')?>&euro;</td>
-							<td class="text-right"><?=$pj?></td>
-							<td><a href="#largeModal" data-toggle="modal" data-id="<?=$prod['id_detail']?>"><img src="../img/litiges/<?=$serialIcon?>"></a></td>
-						</tr>
-
-						<?php else: ?>
-
-							<tr class="text-reddish">
+							<?php if (empty($prod['occ_article_palette'])): ?>
 								<td><?=$prod['article']?></td>
 								<td><?=$prod['dossier_gessica']?></td>
+								<?php else: ?>
+									<td colspan="2">Palette Occasion <?= OccHelpers::getPaletteNameByArticlePalette($pdoOcc,$prod['occ_article_palette'])?></td>
+
+								<?php endif ?>
+
 								<td><?=$prod['palette']?></td>
-								<td><?=$prod['descr']?><br>Ean : <?=$prod['ean']?></td>
+								<td><?=$boxIco.$prod['descr']?><br>Ean : <?=$prod['ean']?></td>
 								<td><?=$prod['fournisseur']?></td>
 								<td><?=$prod['reclamation']?></td>
 								<td class="text-right"><?=$prod['qte_litige']?></td>
-								<td class="text-right"><?=number_format((float)$prod['tarif']/$prod['qte_cde']*$prod['qte_litige'],2,'.','')?>&euro;</td>
+								<td class="text-right"><?=number_format((float)$prod['valo_line'],2,'.','')?>&euro;</td>
 								<td class="text-right"><?=$pj?></td>
-
-								<td><a href="#largeModal" data-toggle="modal" data-id="<?=$prod['id_detail']?>">SN</a></td>
-
+								<td><a href="#largeModal" data-toggle="modal" data-id="<?=$prod['id_detail']?>"><img src="../img/litiges/<?=$serialIcon?>"></a></td>
 							</tr>
-							<?php if ($prod['inv_article']==''): ?>
-								<tr class="text-center text-reddish">
-									<td colspan="10">Produit reçu à la place de la référence ci-dessus :</td>
-								</tr>
+
+							<?php else: ?>
+
 								<tr class="text-reddish">
-									<td colspan="4">Produit non trouvé - EAN saisi :</td>
+									<td><?=$prod['article']?></td>
+									<td><?=$prod['dossier_gessica']?></td>
+									<td><?=$prod['palette']?></td>
+									<td><?=$prod['descr']?><br>Ean : <?=$prod['ean']?></td>
+									<td><?=$prod['fournisseur']?></td>
+									<td><?=$prod['reclamation']?></td>
+									<td class="text-right"><?=$prod['qte_litige']?></td>
+									<td class="text-right"><?=number_format((float)$prod['tarif']/$prod['qte_cde']*$prod['qte_litige'],2,'.','')?>&euro;</td>
+									<td class="text-right"><?=$pj?></td>
 
-									<td colspan="2" class="text-left"><?=$prod['inversion']?></td>
-
-									<td class="text-right"><?=$prod['inv_qte']?></td>
-									<td class="text-right"></td>
-									<td class="text-right"></td>
+									<td><a href="#largeModal" data-toggle="modal" data-id="<?=$prod['id_detail']?>">SN</a></td>
 
 								</tr>
-								<tr class="text-center bg-reddish text-white">
-									<td colspan="10" class="text-right">&nbsp;</td>
-
-								</tr>
-								<tr>
-									<td colspan="10" class="text-right">&nbsp;</td>
-								</tr>
-
-								<?php else: ?>
-									<?php $valoInv=round( $prod['inv_qte']*$prod['inv_tarif'],2); ?>
-									<tr class="text-center text-reddish"><td colspan="11">Produit reçu à la place de la référence ci-dessus :</td></tr>
+								<?php if ($prod['inv_article']==''): ?>
+									<tr class="text-center text-reddish">
+										<td colspan="10">Produit reçu à la place de la référence ci-dessus :</td>
+									</tr>
 									<tr class="text-reddish">
-										<td><?=$prod['inv_article']?></td>
-										<td>&nbsp;</td>
-										<td>&nbsp;</td>
-										<td><?=$prod['inv_descr']?></td>
-										<td><?=$prod['inv_fournisseur']?></td>
-										<td></td>
+										<td colspan="4">Produit non trouvé - EAN saisi :</td>
+
+										<td colspan="2" class="text-left"><?=$prod['inversion']?></td>
+
 										<td class="text-right"><?=$prod['inv_qte']?></td>
-										<td class="text-right"><?=number_format((float)$valoInv,2,'.','')?>&euro;</td>
 										<td class="text-right"></td>
-										<td class="text-center"></td>
+										<td class="text-right"></td>
+
 									</tr>
 									<tr class="text-center bg-reddish text-white">
-										<td colspan="8" class="text-right"><?=number_format((float)$prod['valo_line'],2,'.','')?>&euro;</td>
-										<td colspan="2"></td>
+										<td colspan="10" class="text-right">&nbsp;</td>
 
 									</tr>
 									<tr>
 										<td colspan="10" class="text-right">&nbsp;</td>
 									</tr>
+
+									<?php else: ?>
+										<?php $valoInv=round( $prod['inv_qte']*$prod['inv_tarif'],2); ?>
+										<tr class="text-center text-reddish"><td colspan="11">Produit reçu à la place de la référence ci-dessus :</td></tr>
+										<tr class="text-reddish">
+											<td><?=$prod['inv_article']?></td>
+											<td>&nbsp;</td>
+											<td>&nbsp;</td>
+											<td><?=$prod['inv_descr']?></td>
+											<td><?=$prod['inv_fournisseur']?></td>
+											<td></td>
+											<td class="text-right"><?=$prod['inv_qte']?></td>
+											<td class="text-right"><?=number_format((float)$valoInv,2,'.','')?>&euro;</td>
+											<td class="text-right"></td>
+											<td class="text-center"></td>
+										</tr>
+										<tr class="text-center bg-reddish text-white">
+											<td colspan="8" class="text-right"><?=number_format((float)$prod['valo_line'],2,'.','')?>&euro;</td>
+											<td colspan="2"></td>
+
+										</tr>
+										<tr>
+											<td colspan="10" class="text-right">&nbsp;</td>
+										</tr>
+									<?php endif ?>
+
 								<?php endif ?>
 
-							<?php endif ?>
+							<?php endforeach ?>
 
-						<?php endforeach ?>
+						</tbody>
+					</table>
 
-					</tbody>
-				</table>
-
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col text-right">
-				<a href="edit-litige.php?id=<?=$prod['id_main']?>" class="btn btn-primary"><i class="fas fa-tools pr-3"></i>Modifier</a>
+			<div class="row">
+				<div class="col text-right">
+					<a href="edit-litige.php?id=<?=$prod['id_main']?>" class="btn btn-primary"><i class="fas fa-tools pr-3"></i>Modifier</a>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<p class="text-right heavy bigger mb-3 text-main-blue pr-3">Valorisation magasin : <?= $prod['valo']?> </p>
-				<p><?= $articleAZero?></p>
+			<div class="row">
+				<div class="col">
+					<p class="text-right heavy bigger mb-3 text-main-blue pr-3">Valorisation magasin : <?= $prod['valo']?> </p>
+					<p><?= $articleAZero?></p>
 
+				</div>
 			</div>
-		</div>
 
 
 
