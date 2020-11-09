d5c71d70199ba0a5e958fb1d5badfb34aeb6a878
montusclat <vmontusclat@gmail.com>
2020-11-09 08:35:02 +0100

    fin demnde evo 7 et 59

---
+++ public/gtocc/ajax-cmt-palette.php
@@ -0,0 +1,30 @@
+<?php
+include('../../config/autoload.php');
+
+
+
+function updateCmt($pdoOcc){
+	$req=$pdoOcc->prepare("UPDATE palettes SET cmt_palette= :cmt WHERE id = :id");
+	$req->execute([
+		':id'		=>$_POST['id'],
+		':cmt'			=>$_POST['value'],
+
+	]);
+	if($req->rowCount()==1){
+		echo "ok";
+	}else{
+
+	}
+}
+
+
+
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+		$done=updateCmt($pdoOcc);
+
+}
+
+?>
