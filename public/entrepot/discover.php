bf9d7a92d9fc35c990e91e27f0a72b1b29a9ef63
montusclat <vmontusclat@gmail.com>
2021-02-22 14:48:58 +0100

    migration header en header-bt

--- public/entrepot/discover.php
+++ public/entrepot/discover.php
@@ -31,7 +31,7 @@ addRecord($pdoStat,$page,$action, $descr);
 //----------------------------------------------------
 // VIEW - HEADER
 //----------------------------------------------------
-include('../view/_head.php');
+include('../view/_head-bt.php');
 include('../view/_navbar.php');
 
 //----------------------------------------------------
@@ -43,48 +43,35 @@ include('../view/_navbar.php');
 <div class="container">
 
 
-	<h1 class="light-blue-text text-darken-2">L'entrepôt</h1>
-	<!-- 	<p><img src="../img/soon.png"></p> -->
-<!-- 	<div class="row center">
-		<div class="col l4"></div>
-		<div class="col l4">
-			<h4 class="light-blue-text text-darken-2">Visite virtuelle de l'entrepôt</h4>
-
-			<div class="down"></div>
-			<video width="400" height="300" controls poster="../img/entrepot/before-video.png">
-				<source src="" type="video/mp4">
-					<source src="" type="video/ogg">
-					</video>
-				</div>
-				<div class="col l4"></div>
-
- -->
+	<h1 class="text-main-blue">L'entrepôt</h1>
 	 <div class="row ">
-			<h4 class="light-blue-text text-darken-2">Visite virtuelle de l'entrepôt</h4>
+	 	<div class="col">
+			<h4 class="text-main-blue">Visite virtuelle de l'entrepôt</h4>
+	 	</div>
+
 	</div>
-	 <div class="row center">
-	 	<div class="col l4">
-	 		<video width="400" height="300" controls poster="">
+	 <div class="row pb-5">
+	 	<div class="col text-center">
+	 		<video width="340" height="200" controls poster="">
 	 			<source src="../video/entrepot02.mp4" type="video/mp4">
 					<source src="" type="video/ogg">
 			</video>
 			<p>quai de chargements</p>
 	 	</div>
-	 	<div class="col l4">
-	 		<video width="400" height="300" controls poster="">
+	 	<div class="col text-center">
+	 		<video width="340" height="200" controls poster="">
 	 			<source src="../video/entrepot03.mp4" type="video/mp4">
 	 			<source src="" type="video/ogg">
 	 		</video>
 			<p>zone internet</p>
 
 	 	</div>
-	 	<div class="col l4">
-	 		<video width="400" height="300" controls poster="">
+	 	<div class="col text-center">
+	 		<video width="340" height="200" controls poster="">
 	 			<source src="../video/entrepot04.mp4" type="video/mp4">
 	 			<source src="" type="video/ogg">
 	 		</video>
 			<p>"filmeuse"</p>
-
 	 	</div>
 	</div>
 
@@ -94,6 +81,6 @@ include('../view/_navbar.php');
 //----------------------------------------------------
 // VIEW - FOOTER
 //----------------------------------------------------
-require '../view/_footer.php';
+require '../view/_footer-bt.php';
 
 ?>
