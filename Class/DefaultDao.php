297d7c7d0c77d68e07d092b78ac9766fca1c5e98
montusclat <vmontusclat@gmail.com>
2021-02-24 16:49:07 +0100

    model de class dao

---
+++ Class/DefaultDao.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class DefaultDao{
+
+	// pdoOcc
+	private $pdo;
+
+	public function __construct($pdo){
+		$this->setPdo($pdo);
+	}
+	public function setPdo($pdo){
+		$this->pdo=$pdo;
+		return $pdo;
+	}
+
+
+}
+
+
