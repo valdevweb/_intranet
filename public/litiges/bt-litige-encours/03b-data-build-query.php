e90ac86b285f6c197a1b40bd38cd7caee050a67f
montusclat <vmontusclat@gmail.com>
2021-03-03 09:32:08 +0100

    suppression des where dans requete

--- public/litiges/bt-litige-encours/03b-data-build-query.php
+++ public/litiges/bt-litige-encours/03b-data-build-query.php
@@ -43,8 +43,8 @@ if(isset($paramList)){
 		$litigeQuery="SELECT dossiers.id as id_main, dossiers.dossier, DATE_FORMAT(date_crea, '%d-%m-%Y') as datecrea, dossiers.galec, dossiers.etat_dossier, dossiers.esp, dossiers.vingtquatre, dossiers.valo, dossiers.ctrl_ok, dossiers.commission, dossiers.id_etat, dossiers.occasion, dossiers.id_robbery, dossiers.id_typo, details.*, details.ean as ean_detail, magasin.mag.deno, magasin.mag.centrale,  magasin.mag.id as btlec, etat.etat FROM dossiers
 		LEFT JOIN details ON dossiers.id=details.id_dossier
 		LEFT JOIN etat ON id_etat=etat.id
-		LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec WHERE";
-		$litigeMod="GROUP BY dossiers.id ORDER BY dossiers.dossier DESC ";
+		LEFT JOIN magasin.mag ON dossiers.galec=magasin.mag.galec ";
+		$litigeMod=" GROUP BY dossiers.id ORDER BY dossiers.dossier DESC ";
 		$statutQuery="SELECT  sum(valo) as valo, dossiers.id_etat, etat.etat, count(dossiers.id) as nbEtat, etat.occ_etat FROM dossiers
 		LEFT JOIN etat ON id_etat=etat.id
 		LEFT JOIN details ON dossiers.id=details.id_dossier ";
