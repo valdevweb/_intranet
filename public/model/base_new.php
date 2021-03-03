19f983e11f9bd7de69dd26d85a15d066a10aed41
montusclat <vmontusclat@gmail.com>
2021-03-03 09:34:43 +0100

    ut class db

--- public/model/base_new.php
+++ public/model/base_new.php
@@ -4,54 +4,34 @@ if(!isset($_SESSION['id'])){
 	header('Location:'. ROOT_PATH.'/index.php');
 	exit();
 }
-require '../../config/db-connect.php';
 
-//			css dynamique
-//----------------------------------------------------------------
 $pageCss=explode(".php",basename(__file__));
 $pageCss=$pageCss[0];
 $cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
 
 
-//------------------------------------------------------
-//			REQUIRES
-//------------------------------------------------------
+require '../../Class/Db.php';
 // require_once '../../vendor/autoload.php';
 
 
-
-//---------------------------------------
-//	ajout enreg dans stat
-//---------------------------------------
-// require "../../functions/stats.fn.php";
-// addRecord($pdoStat,basename(__file__),'consultation', "fichiers d'info du service achats", 101);
-
-
- //------------------------------------------------------
-//			DECLARATIONS
-//------------------------------------------------------
 $errors=[];
 $success=[];
+$db=new Db();
+$pdoUser=$db->getPdo('web_users');
 
 
 
-//------------------------------------------------------
-//			FONCTION
-//------------------------------------------------------
 
 
-//------------------------------------------------------
-//			VIEW
+
+
 //------------------------------------------------------
 include('../view/_head-bt.php');
 include('../view/_navbar.php');
 ?>
-<!--********************************
-DEBUT CONTENU CONTAINER
-*********************************-->
+
 <div class="container">
 	<h1 class="text-main-blue py-5 ">Main title</h1>
-
 	<div class="row">
 		<div class="col-lg-1"></div>
 		<div class="col">
@@ -61,9 +41,6 @@ DEBUT CONTENU CONTAINER
 		</div>
 		<div class="col-lg-1"></div>
 	</div>
-
-
-	<!-- ./container -->
 </div>
 
 <?php
