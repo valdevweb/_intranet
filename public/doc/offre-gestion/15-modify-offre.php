4eeff26bfdc28bc9f200c2320d8e7eed6a4b596b
montusclat <vmontusclat@gmail.com>
2021-03-05 12:00:20 +0100

    ajout form-primary

--- public/doc/offre-gestion/15-modify-offre.php
+++ public/doc/offre-gestion/15-modify-offre.php
@@ -14,7 +14,7 @@
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="id_prosp">Prospectus</label>
-								<select class="form-control" name="id_prosp" id="id_prosp">
+								<select class="form-control form-primary" name="id_prosp" id="id_prosp">
 									<option value="">Sélectionner</option>
 									<?php foreach ($listProsp as $key => $prosp): ?>
 										<option value="<?=$prosp['id']?>" <?= FormHelpers::restoreSelected($prosp['id'],$offreMod['id_prosp'])?>>
@@ -31,31 +31,31 @@
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="marque">Marque</label>
-								<input type="text" class="form-control" name="marque" id="marque" value="<?=$offreMod['marque']?>">
+								<input type="text" class="form-control form-primary" name="marque" id="marque" value="<?=$offreMod['marque']?>">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="produit">Produit</label>
-								<input type="text" class="form-control" name="produit" id="produit" value="<?=$offreMod['produit']?>">
+								<input type="text" class="form-control form-primary" name="produit" id="produit" value="<?=$offreMod['produit']?>">
 							</div>
 						</div>
 						<div class="col-lg-2">
 							<div class="form-group">
 								<label for="reference">Référence</label>
-								<input type="text" class="form-control" name="reference" id="reference" value="<?=$offreMod['reference']?>">
+								<input type="text" class="form-control form-primary" name="reference" id="reference" value="<?=$offreMod['reference']?>">
 							</div>
 						</div>
 						<div class="col-lg-1">
 							<div class="form-group">
 								<label for="gt">GT</label>
-								<input type="text" class="form-control" name="gt" value="<?=$offreMod['gt']?>" title="Veuillez saisir le numéro de GT" id="gt" pattern="^(?:[1-9]|0[1-9]|10)$">
+								<input type="text" class="form-control form-primary" name="gt" value="<?=$offreMod['gt']?>" title="Veuillez saisir le numéro de GT" id="gt" pattern="^(?:[1-9]|0[1-9]|10)$">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="ean">EAN :</label>
-								<input type="text" class="form-control" name="ean" id="ean" value="<?=$offreMod['ean']?>">
+								<input type="text" class="form-control form-primary" name="ean" id="ean" value="<?=$offreMod['ean']?>">
 							</div>
 						</div>
 					</div>
@@ -63,25 +63,25 @@
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="pvc">Prix de vente conseillé</label>
-								<input type="text" class="form-control" name="pvc" id="pvc" value="<?=$offreMod['pvc']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés">
+								<input type="text" class="form-control form-primary" name="pvc" id="pvc" value="<?=$offreMod['pvc']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="montant">Montant</label>
-								<input type="text" class="form-control"  value="<?=$offreMod['montant']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés" name="montant" id="montant">
+								<input type="text" class="form-control form-primary"  value="<?=$offreMod['montant']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés" name="montant" id="montant">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="montant_finance">Montant financé</label>
-								<input type="text" class="form-control" name="montant_finance" value="<?=$offreMod['montant_finance']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés"  id="montant_finance">
+								<input type="text" class="form-control form-primary" name="montant_finance" value="<?=$offreMod['montant_finance']?>" pattern="^\d+(\.\d{1,2})?$" title="Seuls les chiffres sont autorisés"  id="montant_finance">
 							</div>
 						</div>
 						<div class="col-lg-3">
 							<div class="form-group">
 								<label for="offre">Offre</label>
-								<select class="form-control" name="offre" id="offre">
+								<select class="form-control form-primary" name="offre" id="offre">
 									<option value="">Sélectionner</option>
 									<option value="1" <?= FormHelpers::restoreSelected(1,$offreMod['offre'])?>>BRII</option>
 									<option value="2" <?= FormHelpers::restoreSelected(2,$offreMod['offre'])?>>TEL</option>
