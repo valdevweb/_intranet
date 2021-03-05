40e943adf02dc002eec60bcee71f2a65797d88bc
montusclat <vmontusclat@gmail.com>
2021-03-05 11:59:13 +0100

    rien

--- public/doc/offre-gestion/01-add-prospectus.php
+++ public/doc/offre-gestion/01-add-prospectus.php
@@ -1,7 +1,6 @@
 <?php
 $ficFile="";
 	if(isset($_FILES['fic']['tmp_name']) && !empty($_FILES['fic']['tmp_name'])){
-
 		$uploaded=move_uploaded_file($_FILES['fic']['tmp_name'],DIR_UPLOAD.'ficwopc\\'.$_FILES['fic']['name'] );
 		if($uploaded==false){
 			$errors[]="Nous avons rencontré un problème avec votre fichier, la clôture n'a pas pu se faire";
@@ -27,3 +26,4 @@ $ficFile="";
 			}
 		}
 	}
+
