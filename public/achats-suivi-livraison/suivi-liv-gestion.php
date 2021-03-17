2a93b3476700488ec91a1e559f76e8a22c019f12
montusclat <vmontusclat@gmail.com>
2021-03-17 16:21:10 +0100

    mise en commun

--- public/achats-suivi-livraison/suivi-liv-gestion.php
+++ public/achats-suivi-livraison/suivi-liv-gestion.php
@@ -43,8 +43,6 @@ $gt="";
 
 
 if (isset($_POST['filter_op'])) {
-
-
 	$param=join(' OR ',array_map(
     function($value){return "code_op='".$value."'";},
     $_POST['select_op']));
@@ -147,65 +145,7 @@ include('../view/_navbar.php');
 			<h6 class="text-main-blue"><span class="step">1</span>Sélection du catalogue</h6>
 		</div>
 	</div>
-
-
-
-	<div class="row pb-5">
-		<div class="col border rounded p-3">
-			<form action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-				<div class="row">
-					<div class="col pr-3">
-						<span class="text-main-blue font-weight-boldless"><span class="text-orange"><i class="fas fa-angle-double-right pr-3"></i></span>Soit par numéro d'opération</span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col">
-						<div class="form-group">
-							<label for="code_op">Code de l'opération :</label>
-							<input type="text" class="form-control" name="code_op" id="code_op">
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col text-right">
-						<button class="btn btn-primary" name="search_by_cata">Afficher</button>
-					</div>
-				</div>
-			</form>
-		</div>
-
-		<div class="col-lg-8 border rounded p-3 ml-3">
-			<form action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-				<div class="row">
-					<div class="col">
-						<span class="text-main-blue font-weight-boldless"><span class="text-orange"><i class="fas fa-angle-double-right pr-3"></i></span>Soit par sélection de semaine</span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col">
-						<div class="form-group">
-							<label for="week">Semaine de l'opération :</label>
-							<input type="week" class="form-control" name="week" id="week">
-						</div>
-					</div>
-					<div class="col">
-						<div class="form-group">
-							<label for="op">Opérations :</label>
-							<select class="form-control" name="op" id="op">
-								<option value="">Sélectionner</option>
-							</select>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col text-right">
-						<button class="btn btn-primary" name="search_by_week">Afficher</button>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-
+	<?php include '../achats-commun/10-form-search-cata.php' ?>
 	<?php if (isset($listArticle)): ?>
 		<?php if (!empty($listArticle)): ?>
 			<?php include 'suivi-liv-gestion/form-info-liv.php' ?>
@@ -231,14 +171,12 @@ include('../view/_navbar.php');
 			</div>
 		</div>
 		<script type="text/javascript">
-
-
 			$(document).ready(function() {
 				$('#week').on('change', function() {
 					var week=$('#week').val();
 					$.ajax({
 						type:'POST',
-						url:'ajax-get-cata-week.php',
+						url:'../achats-commun/ajax-get-cata-week.php',
 						data:{week:week},
 						success: function(html){
 							$("#op").empty();
