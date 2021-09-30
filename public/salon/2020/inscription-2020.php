1abe5f7acdf741a5ed6f8d072808f36da29a773c
montusclat <vmontusclat@gmail.com>
2021-09-30 09:26:29 +0200

    swiftmailer

--- public/salon/2020/inscription-2020.php
+++ public/salon/2020/inscription-2020.php
@@ -6,7 +6,6 @@ if(!isset($_SESSION['id'])){
 }
 require '../../config/db-connect.php';
 
-require '../../functions/mail.fn.php';
 
 //-----------------------------------------------------
 //	css dynamique
