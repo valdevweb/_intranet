2d5593a10abdf37351fddd9475517060d791027f
montusclat <vmontusclat@gmail.com>
2021-09-30 09:25:34 +0200

    suppression salon et création fichier externe salon

---
+++ public/home/salon-home.php
@@ -0,0 +1,24 @@
+
+<div class="row pb-5 align-items-center">
+		<div class="col-lg-1"></div>
+		<div class="col  py-3 text-center  ">
+			<img class="border border-secondary" src="../img/salon/salon2021.jpg">
+		</div>
+		<div class="col text-main-blue">
+			<h1 class="jump">
+				<span>S</span>
+				<span>A</span>
+				<span>L</span>
+				<span>O</span>
+				<span>N</span>
+				<span>&nbsp;</span>
+				<span>2</span>
+				<span>0</span>
+				<span>2</span>
+				<span>1</span>
+			</h1>
+			<h4 class="text-center">Les inscriptions sont ouvertes !!</h4>
+			<div class="text-center" ><a href="../salon/inscription-2021.php"><button class="glow-on-hover" >S'INSCRIRE AU SALON</button></a></div>
+		</div>
+		<div class="col-lg-1"></div>
+	</div>
