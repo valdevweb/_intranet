3d302409785b4399b14ef5b7f203a7431c2cd236
montusclat <vmontusclat@gmail.com>
2021-09-30 09:26:17 +0200

    inutile

--- public/model/mini-nav.php
+++
@@ -1,7 +0,0 @@
-			<div id="floating-nav">
-				<h6 class="text-main-blue text-center">Aller à</h6>
-				<div class="pb-2"><i class="fas fa-newspaper fa-sm circle-icon-blue mr-3"></i><a href="#list-prosp">Listes des prospectus</a></div>
-				<div class="pb-2"><i class="fas fa-euro-sign fa-sm circle-icon-blue mr-3 p-euro"></i><a href="#list-offre">Listes des offres en cours ou à venir</a></div>
-				<div class="pb-2"><i class="fas fa-newspaper fa-sm circle-icon-orange mr-3"></i><a href="#add-prosp-title">Créer un prospectus</a></div>
-				<div class="pb-2"><i class="fas fa-plus fa-sm circle-icon-orange mr-3 p-add"></i><a href="#add-offre-title">Ajouter une offre</a></div>
-			</div>
