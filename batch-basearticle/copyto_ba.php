9f15ca7e4502a97f0e79833f65fbaddc9cd93ad8
montusclat <vmontusclat@gmail.com>
2022-01-27 15:15:35 +0100

    externalisation fn + traitement SCEBFCAB pour recup code article gessica

--- batch-basearticle/copyto_ba.php
+++ batch-basearticle/copyto_ba.php
@@ -1,4 +1,3 @@
-
 <?php
 if (preg_match('/_btlecest/', dirname(__FILE__))) {
 	set_include_path("D:\www\_intranet\_btlecest\\");
@@ -8,62 +7,22 @@ if (preg_match('/_btlecest/', dirname(__FILE__))) {
 
 include 'config/config.inc.php';
 require 'Class/Db.php';
+require 'Class/CrudDao.php';
+require 'functions.php';
+
 
 $db = new Db();
 $pdoQlik = $db->getPdo('qlik');
 
 $file = DIR_IMPORT_GESSICA . "SCEBFART.csv";
+$fileArtGessica = DIR_IMPORT_GESSICA . "SCEBFCAB.csv";
+
+$qlikCrud=new CrudDao($pdoQlik);
+
 
 $row = 0;
 
-function getArticle($pdoQlik, $article, $dossier)
-{
-	$req = $pdoQlik->prepare("SELECT id from ba where article= :article AND dossier= :dossier");
 
-	$req->execute([
-		':article'		=> $article,
-		':dossier'		=> $dossier
-	]);
-	return $req->fetch(PDO::FETCH_ASSOC);
-}
-function updateBa($pdoQlik)
-{
-	$req = $pdoQlik->query("UPDATE ba LEFT JOIN ba_newfields ON ba.id=ba_newfields.id SET ba.ref=ba_newfields.ref, ba.codelec=ba_newfields.codelec");
-}
-
-function getNewFields($pdoQlik)
-{
-	// $req=$pdoQlik->query("SELECT * FROM ba_newfields LIMIT 500 OFFSET 357500");
-	$req = $pdoQlik->prepare("SELECT * FROM ba_newfields ", array(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false));
-	$req->execute();
-	return $req->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function insertNewFields($pdoQlik, $article, $dossier, $ref, $codelec)
-{
-	$req = $pdoQlik->prepare("INSERT INTO ba_newfields (id, article, dossier, ref, codelec) VALUES (:id, :article, :dossier, :ref, :codelec)");
-	$req->execute([
-		':id'	=> $article . $dossier,
-		':article'	=> $article,
-		':dossier'	=> $dossier,
-		':ref'		=> $ref,
-		':codelec'	=>$codelec
-
-	]);
-}
-
-function copyBa($pdoQlik)
-{
-	$req = $pdoQlik->query("INSERT INTO ba (`id`, id_ba, `date_import`, `article`, `dossier`, `panf`, `deee`, `codif_deee`, `sorecop`, `pfnp`, `ppi`, `pvp`, `pvi`, `gt`, `lib_gt`, `libelle`, `marque`, `pcb`, `cnuf`, `fournisseur`, `ean`, `poids_colis`, `poids_brut_uv`, `poids_brut_ul`, `stock_entrepot`)  SELECT concat(`GESSICA.CodeArticle`, `GESSICA.CodeDossier`), `id`, `DateExecutionScriptQlik`, `GESSICA.CodeArticle`, `GESSICA.CodeDossier`, `GESSICA.PANF`, `GESSICA.D3E`, `GESSICA.CodifD3E`, `GESSICA.SORECOP`, `GESSICA.PFNP`, `GESSICA.PPI`, `GESSICA.PVP`, `GESSICA.PVI`, `GESSICA.GT`, `GESSICA.LibGT`, `GESSICA.LibelleArticle`, `GESSICA.Marque`, `GESSICA.PCB`, `GESSICA.CodeFournisseur`, `GESSICA.NomFournisseur`, `GESSICA.Gencod`, `GESSICA.PoidsColis`, `GESSICA.PoidsBrutUV`, `GESSICA.PoidsBrutUL`, `CTBT.StkEnt` FROM basearticles");
-}
-
-
-//
-function getBasearticle($pdoQlik)
-{
-	$req = $pdoQlik->query("SELECT * FROM basearticles LIMIT 1");
-	return $req->fetch(PDO::FETCH_ASSOC);
-}
 
 
 // on récup la date de la dernièremise à jour de la table article de david
@@ -81,7 +40,7 @@ if (isset($qlikBa['DateExecutionScriptQlik'])) {
 	}
 }
 
-// récupération des nouveaux champs dans ba_newfields
+// récupération de champs issus du fichier SCEBFART.csv  => vers ba_newfields
 if (($handle = fopen($file, "r")) !== FALSE) {
 	$pdoQlik->query("DELETE FROM `ba_newfields`");
 
@@ -106,7 +65,47 @@ if (($handle = fopen($file, "r")) !== FALSE) {
 	fclose($handle);
 }
 
+$row = 0;
 
+
+// récupération des correspondances code article/code article gessica issues de SCEBFCAB => vers ba_newfields
+if (($handle = fopen($fileArtGessica, "r")) !== FALSE) {
+
+	$errArr = [];
+	$i = 0;
+	while (($data = fgetcsv($handle, 10000, ",")) !== FALSE) {
+		if ($row == 0) {
+			$entete = $data;
+		} else {
+            $artGessica=$data[8];
+            $art=$data[1];
+            $dossier=$data[0];
+            $id=$art.$dossier;
+            echo 'id recherché '.$id;
+            echo "<br>";
+			if ($artGessica != "" && $art!="" && $dossier!="") {
+                $idExist=$qlikCrud->getOneById('ba_newfields', $id);
+  
+                if(!empty($idExist)){
+                    echo $idExist['id'];
+                    echo "<br>";
+                    echo "<br>";
+
+                    $qlikCrud->updateOneField('ba_newfields', 'article_gessica',$artGessica, $id);
+                }else{
+                    echo 'non trouvé';
+                    echo "<br>";
+                    echo "<br>";
+
+                }
+			}
+		}
+		$row++;
+	}
+	$row = 0;
+	fclose($handle);
+}
+// update de ba avec les champs de ba_newfields
 updateBa($pdoQlik);
 
 
