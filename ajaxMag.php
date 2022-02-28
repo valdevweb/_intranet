080a08d83b613d308fddb0dfaff57cb4bbc499c1
montusclat <vmontusclat@gmail.com>
2022-02-28 15:42:09 +0100

    nettoyage

--- ajaxMag.php
+++ ajaxMag.php
@@ -2,13 +2,9 @@
 require 'config/config.inc.php';
 require 'config/db-connect.php';
 
-//https://www.codexworld.com/dynamic-dependent-select-box-using-jquery-ajax-php/
-// $_POST['centrale']="SCADIF";
 
 if(!empty($_POST['centrale'])){
-	// echo $_POST['centrale'];
-	// $req=$pdoBt->query("SELECT * FROM sca3 WHERE centrale=" . $_POST['centrale'] ."");
-	// $req=$pdoBt->prepare("SELECT* FROM sca3 where centrale= :centrale AND mag NOT LIKE '*%' ORDER BY mag");
+
 	$req=$pdoMag->prepare("SELECT * FROM mag where centrale= :centrale  AND  gel!=9 ORDER BY deno ");
 	$req->execute(array(
 		'centrale' => $_POST['centrale']
