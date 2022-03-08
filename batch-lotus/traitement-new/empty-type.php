abbfba5d8090efda1044b76461359ca19e5d821a
montusclat <vmontusclat@gmail.com>
2022-03-08 08:47:35 +0100

    suppresion batch lotus

--- batch-lotus/traitement-new/empty-type.php
+++
@@ -1,5 +0,0 @@
-<?php
-foreach ($empty as $key => $extraction) {
-	$data=['id_import'=>$newImport['id'], 'id_extraction'=>$extraction['id'], 'id_error'=>4];
-	$crudMag->insert('listdiffu_errors',$data);
-}
--- batch-lotus/traitement-new/ld-type.php
+++
@@ -1,8 +0,0 @@
-	<?php
-
-	foreach ($ld as $key => $extraction) {
-		$codeErr=9;
-		$data=['id_import'=>$newImport['id'], 'id_extraction'=>$extraction['id'], 'id_error'=>9];
-		$crudMag->insert('listdiffu_errors',$data);
-
-	}
