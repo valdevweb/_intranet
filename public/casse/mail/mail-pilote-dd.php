26d36b8a8deee293257d42b4e0110a5369428dd5
montusclat <vmontusclat@gmail.com>
2022-03-16 08:48:26 +0100

    ajout info num exp

--- public/casse/mail/mail-pilote-dd.php
+++ public/casse/mail/mail-pilote-dd.php
@@ -4,7 +4,7 @@
     </head>
     <body style="font-family:arial,helvetica, 'sans serif'; color:dimGray;">
         <p>Bonjour,</p>
-        <p>Nous allons faire partir <span style="color:darkblue;">{NB} palettes pour le magasin {MAG}</span> . Vous trouverez ci joint le tableau de correspondance des contremarques.<br>
+        <p>Nous allons faire partir <span style="color:darkblue;">{NB} palettes pour le magasin {MAG}</span> sur l'expédition {NUMEXP} . Vous trouverez ci joint le tableau de correspondance des contremarques.<br>
         Pourriez vous faire déstocker ces palettes et les vérifier avant leur envoi ?<br> Une fois validées, les mettre en RAQ et faire un retour sur le portail {LINK}</p>
         <p>&nbsp;</p>
 
