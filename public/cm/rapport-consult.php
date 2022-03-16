988a3308b45597961807873da3a3c44def03da52
montusclat <vmontusclat@gmail.com>
2022-03-16 08:48:46 +0100

    bug : doublon déclaration constantes

--- public/cm/rapport-consult.php
+++ public/cm/rapport-consult.php
@@ -17,8 +17,7 @@ require '../../Class/cm/ThemeHelpers.php';
 require '../../Class/cm/RapportHelpers.php';
 // require_once '../../vendor/autoload.php';
 
-define("CM_UPLOAD_URL","http://".SERVER_NAME."/".VERSION."upload-main/cm/" );
-define("ANDROID_UPLOAD","http://".SERVER_NAME."/".VERSION."android/upload/" );
+
 
 
 $pageCss=explode(".php",basename(__file__));
