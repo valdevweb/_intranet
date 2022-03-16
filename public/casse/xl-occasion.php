805f639e206133ee25069751a4082ce2b84accca
montusclat <vmontusclat@gmail.com>
2022-03-16 08:48:13 +0100

    modif ordre et données fichier excel

--- public/casse/xl-occasion.php
+++ public/casse/xl-occasion.php
@@ -1,13 +1,13 @@
 <?php
 require('../../config/autoload.php');
-if(!isset($_SESSION['id'])){
-	header('Location:'. ROOT_PATH.'/index.php');
+if (!isset($_SESSION['id'])) {
+	header('Location:' . ROOT_PATH . '/index.php');
 	exit();
 }
 
-$pageCss=explode(".php",basename(__file__));
-$pageCss=$pageCss[0];
-$cssFile=ROOT_PATH ."/public/css/".$pageCss.".css";
+$pageCss = explode(".php", basename(__file__));
+$pageCss = $pageCss[0];
+$cssFile = ROOT_PATH . "/public/css/" . $pageCss . ".css";
 
 
 require '../../Class/Db.php';
@@ -21,17 +21,17 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 
-$errors=[];
-$success=[];
-$db=new Db();
-$pdoUser=$db->getPdo('web_users');
-$pdoCasse=$db->getPdo('casse');
+$errors = [];
+$success = [];
+$db = new Db();
+$pdoUser = $db->getPdo('web_users');
+$pdoCasse = $db->getPdo('casse');
 
-$trtDao=new TrtDao($pdoCasse);
-$expDao=new ExpDao($pdoCasse);
-$casseCrud= new CrudDao($pdoCasse);
+$trtDao = new TrtDao($pdoCasse);
+$expDao = new ExpDao($pdoCasse);
+$casseCrud = new CrudDao($pdoCasse);
 
-$articles=$expDao->getExpPaletteCasse($_GET['id']);
+$articles = $expDao->getExpPaletteCasse($_GET['id']);
 
 
 $spreadsheet = new Spreadsheet();
@@ -40,45 +40,51 @@ $sheet->setCellValue('A1', 'Article');
 $sheet->setCellValue('B1', 'Désignation');
 $sheet->setCellValue('C1', 'Quantité');
 $sheet->setCellValue('D1', 'N° palette');
-$sheet->setCellValue('E1', 'PAF unitaire -60%');
+$sheet->setCellValue('E1', 'PAF unitaire moins 40');
 $sheet->setCellValue('F1', 'Prix de vente conseillé occasion TTC');
 $sheet->setCellValue('G1', 'Taux de marge magasin');
-$sheet->setCellValue('H1', 'Article BT');
-$sheet->setCellValue('I1', 'Palette contremarque');
-$row=2;
+$sheet->setCellValue('H1', 'Article palette G6K');
+$sheet->setCellValue('I1', 'Article BT');
+$row = 2;
 
 foreach ($articles as $key => $art) {
-	$paf=0;
-	$sheet->setCellValue('A'.$row,  $art['ean']);
-	$sheet->setCellValue('B'.$row,  $art['designation']);
-	$sheet->setCellValue('C'.$row,  $art['uvc']);
-	if($art['pfnp']!=null){
-		$paf=$art['pfnp']/2+round($art['pfnp']/10);
+	$paf = 0;
+	$sheet->setCellValue('A' . $row,  $art['ean']);
+	$sheet->getStyle('A' . $row)
+		->getNumberFormat()
+		->setFormatCode(
+			'0000000000000'
+		);
+	$sheet->setCellValue('B' . $row,  $art['designation']);
+	$sheet->setCellValue('C' . $row,  $art['uvc']);
+	if ($art['pfnp'] != null) {
+		$paf = $art['pfnp'] / 2 + round($art['pfnp'] / 10);
 	}
-	$sheet->setCellValue('E'.$row,  $paf);
-	$sheet->setCellValue('F'.$row,  $art['ppi']);
-	$sheet->setCellValue('H'.$row,  $art['article']);
-	$sheet->setCellValue('I'.$row,  $art['palette']);
+	$sheet->setCellValue('D' . $row,  $art['contremarque']);
+
+	$sheet->setCellValue('E' . $row,  $paf);
+	$sheet->setCellValue('F' . $row,  $art['ppi']);
+	$sheet->setCellValue('I' . $row,  $art['article']);
 	$row++;
 }
 
 
 
 $writer = new Xlsx($spreadsheet);
-$xlFile='occasion/fichier-occasion-'.date('YmdHis').'.xlsx';
-$xlPath=DIR_UPLOAD.$xlFile;
+$xlFile = 'occasion/fichier-occasion-exp-'.$_GET['id'] . '.xlsx';
+$xlPath = DIR_UPLOAD . $xlFile;
 $writer->save($xlPath);
 
-$casseCrud->update("exps", "id=".$_GET['id'], ['file_occ'=>$xlFile]);
+$casseCrud->update("exps", "id=" . $_GET['id'], ['file_occ' => $xlFile]);
 
 
-if(VERSION=="_"){
-	$dest=['valerie.montusclat@btlecest.leclerc'];
-	$cc=[];
-}else{
+if (VERSION == "_") {
+	$dest = ['valerie.montusclat@btlecest.leclerc'];
+	$cc = [];
+} else {
 
-	$dest=['jonathan.domange@btlecest.leclerc'];
-	$cc=['valerie.montusclat@btlecest.leclerc', 'nathalie.pazik@btlecest.leclerc', 'christelle.trousset@btlecest.leclerc'];
+	$dest = ['jonathan.domange@btlecest.leclerc'];
+	$cc = ['valerie.montusclat@btlecest.leclerc', 'nathalie.pazik@btlecest.leclerc', 'christelle.trousset@btlecest.leclerc'];
 }
 
 
@@ -86,19 +92,19 @@ $transport = (new Swift_SmtpTransport(SMTP_ADDRESS, 25));
 $mailer = new Swift_Mailer($transport);
 
 $htmlMail = file_get_contents('occasion-excel-casse.html');
-$htmlMail=str_replace('{PROD}',$prod,$htmlMail);
-$subject='Portail BTLec Est - fichier excel casse/occasion';
+$htmlMail = str_replace('{PROD}', $prod, $htmlMail);
+$subject = 'Portail BTLec Est - fichier excel casse/occasion - expédition '.$_GET['id'];
 $message = (new Swift_Message($subject))
-->setBody($htmlMail, 'text/html')
-->setFrom(EMAIL_NEPASREPONDRE)
-->setTo($dest)
-->setCc($cc)
-->attach(Swift_Attachment::fromPath($xlPath));
-if (!$mailer->send($message, $failures)){
+	->setBody($htmlMail, 'text/html')
+	->setFrom(EMAIL_NEPASREPONDRE)
+	->setTo($dest)
+	->setCc($cc)
+	->attach(Swift_Attachment::fromPath($xlPath));
+if (!$mailer->send($message, $failures)) {
 	print_r($failures);
-}else{
+} else {
 	$trtDao->insertTrtHisto($_GET['id'], $_GET['id_trt']);
-	header('Location:casse-dashboard.php?#exp-'.$_GET['id']);
+	header('Location:casse-dashboard.php?#exp-' . $_GET['id']);
 }
 
 
