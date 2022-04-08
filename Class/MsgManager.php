3401db93bb5feff0b074f4a7c8e3931ff6eba60e
montusclat <vmontusclat@gmail.com>
2022-04-08 09:36:41 +0200

    suppression table service et table contact_service

--- Class/MsgManager.php
+++ Class/MsgManager.php
@@ -6,7 +6,7 @@ class MsgManager{
 
 	public function getDemande($pdoBt,$id){
 
-		$req=$pdoBt->prepare("SELECT *, msg.id as idMsg FROM msg LEFT JOIN services ON msg.id_service=services.id LEFT JOIN magasin.mag ON id_galec=magasin.mag.galec WHERE  msg.id= :id");
+		$req=$pdoBt->prepare("SELECT *, msg.id as idMsg FROM msg LEFT JOIN web_users.services ON msg.id_service=services.id LEFT JOIN magasin.mag ON id_galec=magasin.mag.galec WHERE  msg.id= :id");
 		$req->execute([
 			':id'		=>$id
 		]);
@@ -15,7 +15,7 @@ class MsgManager{
 
 	public function getListDemandeByGalec($pdoBt,$galec){
 
-		$req=$pdoBt->prepare("SELECT *, msg.id as idMsg FROM msg LEFT JOIN services ON msg.id_service=services.id LEFT JOIN magasin.mag ON id_galec=magasin.mag.galec WHERE  msg.id_galec= :galec ORDER BY msg.id DESC ");
+		$req=$pdoBt->prepare("SELECT *, msg.id as idMsg FROM msg LEFT JOIN web_users.services ON msg.id_service=services.id LEFT JOIN magasin.mag ON id_galec=magasin.mag.galec WHERE  msg.id_galec= :galec ORDER BY msg.id DESC ");
 		$req->execute([
 			':galec'		=>$galec
 		]);
@@ -66,7 +66,7 @@ class MsgManager{
 
 	public function  getListDdeClos($pdoBt)
 	{
-		$req=$pdoBt->prepare("SELECT * FROM msg LEFT JOIN services ON msg.id_service=services.id  WHERE etat= :clos ORDER BY id_service, date_msg DESC");
+		$req=$pdoBt->prepare("SELECT * FROM msg LEFT JOIN web_users.services ON msg.id_service=services.id  WHERE etat= :clos ORDER BY id_service, date_msg DESC");
 		$req->execute(array(
 			':clos' =>'clos'
 		));
@@ -74,7 +74,7 @@ class MsgManager{
 	}
 
 	public function getListDdeClosService($pdoBt,$idservice){
-		$req=$pdoBt->prepare("SELECT  *,msg.id as idMsg FROM msg LEFT JOIN services ON msg.id_service=services.id  WHERE etat = :clos AND id_service= :id_service ORDER BY id_service, date_msg DESC");
+		$req=$pdoBt->prepare("SELECT  *,msg.id as idMsg FROM msg LEFT JOIN web_users.services ON msg.id_service=services.id  WHERE etat = :clos AND id_service= :id_service ORDER BY id_service, date_msg DESC");
 		$req->execute(array(
 			':clos' =>'clos',
 			':id_service' =>$idservice
